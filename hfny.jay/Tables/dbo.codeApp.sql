426ce3d037c40518abc62950b92a64d10a8686d6
Jay Robohn <jrobohn@albany.edu>
2011-11-09 19:23:13 +0000

    Expanded size of code description

--- hfny.jay/Tables/dbo.codeApp.sql
+++ hfny.jay/Tables/dbo.codeApp.sql
@@ -3,7 +3,7 @@ CREATE TABLE [dbo].[codeApp]
 [codeAppPK] [int] NOT NULL IDENTITY(1, 1),
 [AppCode] [char] (2) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [AppCodeGroup] [char] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[AppCodeText] [char] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[AppCodeText] [char] (120) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [AppCodeUsedWhere] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
