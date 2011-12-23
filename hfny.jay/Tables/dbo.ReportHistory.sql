d042c7bbc0c8bc2b55abe16d413568bf5f58b4b2
Jay Robohn <jrobohn@albany.edu>
2011-12-23 13:37:12 +0000

    Add ProgramFK to ReportHistory

--- hfny.jay/Tables/dbo.ReportHistory.sql
+++ hfny.jay/Tables/dbo.ReportHistory.sql
@@ -1,12 +1,15 @@
 CREATE TABLE [dbo].[ReportHistory]
 (
 [ReportHistoryPK] [int] NOT NULL IDENTITY(1, 1),
+[ProgramFK] [int] NOT NULL,
 [ReportCategory] [char] (2) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [ReportName] [char] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [ReportType] [char] (2) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [TimeRun] [datetime] NOT NULL,
 [UserFK] [char] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 ) ON [PRIMARY]
+ALTER TABLE [dbo].[ReportHistory] ADD
+CONSTRAINT [FK_ReportHistory_ProgramFK] FOREIGN KEY ([ProgramFK]) REFERENCES [dbo].[HVProgram] ([HVProgramPK])
 GO
 ALTER TABLE [dbo].[ReportHistory] ADD CONSTRAINT [PK__ReportHi__488810E06BE40491] PRIMARY KEY CLUSTERED  ([ReportHistoryPK]) ON [PRIMARY]
 GO
