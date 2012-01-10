3993d9fac89b4c7f48ba700971350fceae81f381
Devinder Singh Khalsa <dkhalsa@albany.edu>
2012-01-10 19:02:54 +0000

    Stops inserting duplicates Referral Source Name in a given program

--- hfny.jay/Stored Procedures/dbo.spAddlistReferralSource.sql
+++ hfny.jay/Stored Procedures/dbo.spAddlistReferralSource.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -7,18 +8,23 @@ CREATE PROCEDURE [dbo].[spAddlistReferralSource](@ProgramFK int=NULL,
 @RSIsActive bit=NULL,
 @listReferralSourcePK_old int=NULL)
 AS
-INSERT INTO listReferralSource(
-ProgramFK,
-ReferralSourceName,
-RSIsActive,
-listReferralSourcePK_old
-)
-VALUES(
-@ProgramFK,
-@ReferralSourceName,
-@RSIsActive,
-@listReferralSourcePK_old
-)
 
+-- Stops inserting duplicates Referral Source Name in a given program
+IF(NOT EXISTS(SELECT * FROM listReferralSource WHERE ProgramFK = @ProgramFK AND ReferralSourceName = @ReferralSourceName))
+BEGIN 
+
+	INSERT INTO listReferralSource(
+	ProgramFK,
+	ReferralSourceName,
+	RSIsActive,
+	listReferralSourcePK_old
+	)
+	VALUES(
+	@ProgramFK,
+	@ReferralSourceName,
+	@RSIsActive,
+	@listReferralSourcePK_old
+	)
+END
 SELECT SCOPE_IDENTITY() AS [SCOPE_IDENTITY]
 GO
