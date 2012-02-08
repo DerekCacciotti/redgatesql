b39a5bae7c6bf7ec87cd3361be9f01772e18ae69
Chun Dar Chen <cchen2@albany.edu>
2012-02-08 15:08:56 +0000

    add code to prevent duplicate agency name by programFK

--- hfny.jay/Stored Procedures/dbo.spAddlistServiceReferralAgency.sql
+++ hfny.jay/Stored Procedures/dbo.spAddlistServiceReferralAgency.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -6,6 +7,16 @@ CREATE PROCEDURE [dbo].[spAddlistServiceReferralAgency](@AgencyIsActive bit=NULL
 @AgencyName varchar(100)=NULL,
 @ProgramFK int=NULL)
 AS
+
+/*** check agency name duplication ***/
+DECLARE @err_message nvarchar(255)
+IF EXISTS (select AgencyName from listServiceReferralAgency 
+           where ProgramFK = @ProgramFK and AgencyName = @AgencyName) 
+	BEGIN	
+	  SET @err_message = 'Duplicate agency name'
+	  RAISERROR (@err_message, 11,1)
+	END
+
 INSERT INTO listServiceReferralAgency(
 AgencyIsActive,
 AgencyName,
