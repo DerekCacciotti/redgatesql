034336801038476c607b1d71948b35f7f210d81c
Jay Robohn <jrobohn@albany.edu>
2012-02-09 14:40:56 +0000

    Changes for new PC1ID List report

--- hfny.jay/Stored Procedures/dbo.rspPC1IDList.sql
+++ hfny.jay/Stored Procedures/dbo.rspPC1IDList.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -8,72 +9,99 @@ GO
 -- Description:	<report: Use of Creative Outreach - Detail>
 --				Moved from FamSys - 02/05/12 jrobohn
 -- =============================================
-CREATE PROCEDURE [dbo].[rspPC1IDList] (@programfk INT = 1)
-AS
+CREATE procedure [dbo].[rspPC1IDList]
+(
+    @programfk int = 1
+)
+as
 
-SELECT TOP 100 PERCENT 
-	programname,
-	hvcasepk,
-	pc1id, 
-	oldid,
-	LTRIM(RTRIM(fsw.firstname)) + ' ' + LTRIM(RTRIM(fsw.lastname)) as workername,
-	codelevelpk, 
-	levelname, 
-	currentleveldate, 
-	LTRIM(RTRIM(pc.pcfirstname))+ ' ' + LTRIM(RTRIM(pc.pclastname)) as pcname,
-	LTRIM(RTRIM(tcid.tcfirstname)) + ' ' + LTRIM(RTRIM(tcid.tclastname)) as tcname,
-	hvcase.tcdob, 
-	hvcase.edc,
-	hvcase.screendate,
-	HVCase.kempedate,
-	hvcase.intakedate,
-	pc2fk,
-    CASE WHEN pc2fk IS NULL OR pc2fk=0 THEN 0 ELSE 1 END AS pc2exists,
-	CASE WHEN pc2fk IS NOT NULL AND pc2fk>0 
-		 THEN (SELECT LTRIM(RTRIM(pcfirstname)) + ' ' + LTRIM(RTRIM(pclastname)) AS pc2name 
-				  FROM pc WHERE pcpk=pc2fk) 
-		 ELSE
-			''
-		 END AS pc2name,
-    obpfk,
-    CASE WHEN obpfk IS NULL OR obpfk=0 THEN 0 ELSE 1 END AS obpexists,
-	CASE WHEN obpfk IS NOT NULL AND obpfk>0 
-		 THEN (SELECT LTRIM(RTRIM(pcfirstname)) + ' ' + LTRIM(RTRIM(pclastname)) AS obpname 
-				  FROM pc WHERE pcpk=obpfk) 
-		 ELSE
-			''
-		 END AS obpname,
-    cpfk as ecfk,
-    CASE WHEN cpfk IS NULL OR cpfk=0 THEN 0 ELSE 1 END AS ecexists, 
-	CASE WHEN cpfk IS NOT NULL AND cpfk>0 
-		 THEN (SELECT LTRIM(RTRIM(pcfirstname)) + ' ' + LTRIM(RTRIM(pclastname)) AS cpname 
-				  FROM pc WHERE pcpk=cpfk) 
-		 ELSE
-			''
-		 END AS ecname
-FROM hvcase
-INNER JOIN caseprogram
-ON caseprogram.hvcasefk = hvcasepk
-INNER JOIN HVProgram
-on hvprogrampk=caseprogram.programfk
-INNER JOIN codelevel
-ON codelevelpk = currentlevelfk
-INNER JOIN pc
-ON pc.pcpk = pc1fk
-LEFT JOIN tcid
-ON tcid.hvcasefk = hvcasepk
-INNER JOIN worker fsw
-ON CurrentFSWFK = fsw.workerpk
-INNER JOIN workerprogram
-ON workerfk = fsw.workerpk
-WHERE caseprogram.programfk = @programfk
-AND dischargedate IS NULL
-AND kempedate IS NOT NULL
-AND casestartdate <= DATEADD(dd,1,DATEDIFF(dd,0,GETDATE()))
-ORDER BY workername,oldid,screendate
+	select top 100 percent
+						  programname
+						 ,hvcasepk
+						 ,pc1id
+						 ,oldid
+						 ,LTRIM(RTRIM(fsw.firstname))+' '+LTRIM(RTRIM(fsw.lastname)) as workername
+						 ,codelevelpk
+						 ,levelname
+						 ,currentleveldate
+						 ,LTRIM(RTRIM(pc.pcfirstname))+' '+LTRIM(RTRIM(pc.pclastname)) as pcname
+						 ,LTRIM(RTRIM(tcid.tcfirstname))+' '+LTRIM(RTRIM(tcid.tclastname)) as tcname
+						 ,hvcase.tcdob
+						 ,hvcase.edc
+						 ,hvcase.screendate
+						 ,HVCase.kempedate
+						 ,hvcase.intakedate
+						 ,pc2fk
+						 ,case
+							  when pc2fk is null or pc2fk = 0 then
+								  0
+							  else
+								  1
+						  end as pc2exists
+						 ,case
+							  when pc2fk is not null and pc2fk > 0
+								  then
+								  (select LTRIM(RTRIM(pcfirstname))+' '+LTRIM(RTRIM(pclastname)) as pc2name
+									   from pc
+									   where pcpk = pc2fk)
+							  else
+								  ''
+						  end as pc2name
+						 ,obpfk
+						 ,case
+							  when obpfk is null or obpfk = 0 then
+								  0
+							  else
+								  1
+						  end as obpexists
+						 ,case
+							  when obpfk is not null and obpfk > 0
+								  then
+								  (select LTRIM(RTRIM(pcfirstname))+' '+LTRIM(RTRIM(pclastname)) as obpname
+									   from pc
+									   where pcpk = obpfk)
+							  else
+								  ''
+						  end as obpname
+						 ,cpfk as ecfk
+						 ,case
+							  when cpfk is null or cpfk = 0 then
+								  0
+							  else
+								  1
+						  end as ecexists
+						 ,case
+							  when cpfk is not null and cpfk > 0
+								  then
+								  (select LTRIM(RTRIM(pcfirstname))+' '+LTRIM(RTRIM(pclastname)) as cpname
+									   from pc
+									   where pcpk = cpfk)
+							  else
+								  ''
+						  end as ecname
+		from hvcase
+			inner join caseprogram
+					  on caseprogram.hvcasefk = hvcasepk
+			inner join HVProgram
+					  on hvprogrampk = caseprogram.programfk
+			inner join codelevel
+					  on codelevelpk = currentlevelfk
+			inner join pc
+					  on pc.pcpk = pc1fk
+			left join tcid
+					 on tcid.hvcasefk = hvcasepk
+			inner join worker fsw
+					  on CurrentFSWFK = fsw.workerpk
+			inner join workerprogram
+					  on workerfk = fsw.workerpk
+		where caseprogram.programfk = @programfk
+			 and dischargedate is null
+			 and kempedate is not null
+			 and casestartdate <= dateadd(dd,1,datediff(dd,0,GETDATE()))
+		order by oldid
+				,workername
+				,screendate
 
-/* SET NOCOUNT ON */
-RETURN
-
-
+	/* SET NOCOUNT ON */
+	return
 GO
