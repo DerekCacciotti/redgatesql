143e39e9c5671a3ffe6e44f191d811b05ab4e512
Chris Papas <cpapas@albany.edu>
2012-05-24 19:00:25 +0000

    Transfer case

---
+++ hfny.jay/Stored Procedures/dbo.spGetCasesEligibleForTransfer.sql
@@ -0,0 +1,36 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spGetCasesEligibleForTransfer] (@ProgramFK int)
+
+AS
+BEGIN
+  SET NOCOUNT ON;
+
+SELECT  cp.PC1ID, cp.DischargeDate, PC.PCDOB, PC.PCFirstName, PC.PCLastName, 
+		cp.HVCaseFK, cp.ProgramFK, cp.CaseProgramPK
+FROM    CaseProgram cp
+        INNER JOIN HVCase c ON cp.HVCaseFK = c.HVCasePK 
+        INNER JOIN PC ON c.PC1FK = PC.PCPK
+WHERE   (cp.DischargeReason = '37') AND 
+        (cp.DischargeDate IS NOT NULL) AND 
+        cp.TransferredToProgramFK = @ProgramFK AND 
+		cp.HVCaseFK not in 
+		(select cp2.HVCaseFK 
+		 from   CaseProgram cp2 
+		 where  cp2.HVCaseFK= cp.HVCaseFK and 
+				cp2.TransferredToProgramFK is null)
+ORDER BY DischargeDate 
+
+/* AND CaseProgram.HVCaseFK NOT IN */
+/* (SELECT HVCaseFK FROM CaseProgram WHERE ProgramFK = @ProgramFK) */
+/* SET NOCOUNT ON */ 
+
+END
+GO
