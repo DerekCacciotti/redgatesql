096fd3d612dac47b2653efd12bea060bd905561d
Chun Dar Chen <cchen2@albany.edu>
2012-08-21 13:24:54 +0000

    FAW monthly report includes No Preassessment cases

--- hfny.jay/Stored Procedures/dbo.rspFAWMonthlyReport_Part5.sql
+++ hfny.jay/Stored Procedures/dbo.rspFAWMonthlyReport_Part5.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -17,6 +18,59 @@ AS
 --DECLARE @StartDt DATE = '01/01/2011'
 --DECLARE @EndDt DATE = '01/31/2011'
 --DECLARE @programfk INT = 6
+
+DECLARE @NoAnyPreAssessment TABLE (
+    [PreassessmentPK] [int] NULL,
+	[CaseStatus] [char](2) NOT NULL,
+	[DischargeReason] [char](2) NULL,
+	[DischargeReasonSpecify] [varchar](500) NULL,
+	[DischargeSafetyReason] [char](2) NULL,
+	[DischargeSafetyReasonDV] [bit] NULL,
+	[DischargeSafetyReasonMH] [bit] NULL,
+	[DischargeSafetyReasonOther] [bit] NULL,
+	[DischargeSafetyReasonSA] [bit] NULL,
+	[DischargeSafetyReasonSpecify] [varchar](500) NULL,
+	[FSWAssignDate] [datetime] NULL,
+	[HVCaseFK] [int] NOT NULL,
+	[KempeDate] [datetime] NULL,
+	[KempeResult] [bit] NULL,
+	[PAActivitySpecify] [varchar](500) NULL,
+	[PACall2Parent] [int] NULL,
+	[PACallFromParent] [int] NULL,
+	[PACaseReview] [int] NULL,
+	[PACreateDate] [datetime] NULL,
+	[PACreator] [char](10) NULL,
+	[PADate] [datetime] NULL,
+	[PAEditDate] [datetime] NULL,
+	[PAEditor] [char](10) NULL,
+	[PAFAWFK] [int] NULL,
+	[PAFSWFK] [int] NULL,
+	[PAGift] [int] NULL,
+	[PAOtherActivity] [int] NULL,
+	[PAOtherHVProgram] [int] NULL,
+	[PAParent2Office] [int] NULL,
+	[PAParentLetter] [int] NULL,
+	[PAProgramMaterial] [int] NULL,
+	[PAVisitAttempt] [int] NULL,
+	[PAVisitMade] [int] NULL,
+	[ProgramFK] [int] NOT NULL,
+	[TransferredtoProgram] [int] NULL )
+
+INSERT INTO @NoAnyPreAssessment ([HVCaseFK], [CaseStatus], [PAFAWFK], [ProgramFK])
+SELECT ppp.HVCaseFK, '99', ppp.FAWFK, @programfk
+FROM 
+(SELECT a.*
+FROM (
+SELECT HVCaseFK, FAWFK
+FROM HVScreen WHERE ProgramFK = @programfk
+AND ScreenDate < @StartDt 
+AND ScreenResult = 1 AND ReferralMade = 1
+) AS a
+JOIN CaseProgram AS c ON a.HVCaseFK = c.HVCaseFK
+LEFT OUTER JOIN Preassessment AS b ON a.HVCaseFK = b.HVCaseFK AND  b.PADate <= @EndDt
+WHERE b.HVCaseFK IS NULL AND (c.DischargeDate IS NULL OR c.DischargeDate > @StartDt)
+) as ppp
+;
 
 -- no status pre-assessment cases (no form this month)
 ;WITH zzz AS (
@@ -55,41 +109,42 @@ SELECT c.PC1ID [Participant]
   WHEN a.CaseStatus = '03' THEN rtrim(str(datediff(dd, b.ScreenDate , a.PADate),10))
   WHEN a.CaseStatus = '01' THEN rtrim(str(datediff(dd, b.ScreenDate, @EndDt),10))
   WHEN a.CaseStatus = '02' THEN rtrim(str(datediff(dd, b.ScreenDate, a.PADate),10))
-  ELSE '0 days' END [DaysInPreassess]
-  
+  WHEN a.CaseStatus = '99' THEN rtrim(str(datediff(dd, b.ScreenDate, @EndDt),10))
+  ELSE '0' END [DaysInPreassess]
   
 , CASE WHEN a.PADate < @StartDt THEN 'No Status'
   WHEN a.CaseStatus = '03' THEN rtrim(x.ReportDischargeText) + ' ' + convert(VARCHAR(12), a.PADate, 101) 
-  WHEN a.CaseStatus = '01' THEN 'Engagement Continue ' + convert(VARCHAR(12), @EndDt, 101) 
-  WHEN a.CaseStatus = '02' THEN 'Enrolled ' + convert(VARCHAR(12), b.KempeDate, 101) 
+  WHEN a.CaseStatus = '01' THEN 'Continue ' + convert(VARCHAR(12), @EndDt, 101) 
+  WHEN a.CaseStatus = '02' THEN 'Enrolled ' + convert(VARCHAR(12), b.KempeDate, 101)
+  WHEN a.CaseStatus = '99' THEN 'No Preassessment'
   ELSE '' END [CurrentStatus]
-, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(PAParentLetter, 0) END [Letters]
-, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(PACall2Parent, 0) END [Call2Parent]
-, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(PACallFromParent, 0) END [CallFromParent]
-, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(PAVisitAttempt, 0) END [VisitAttempted]
-, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(PAVisitMade, 0) END [VisitConducted]
-, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(PAOtherHVProgram, 0) END [Referrals]
-, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(PAParent2Office, 0) END [Parent2Office]
-, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(PAProgramMaterial, 0) END [ProgramMaterial]
-, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(PAGift, 0) END [Gift]
-, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(PACaseReview, 0) END [CaseReview]
-, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(PAOtherActivity, 0) END [OtherActivity]
+, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(a.PAParentLetter, 0) END [Letters]
+, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(a.PACall2Parent, 0) END [Call2Parent]
+, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(a.PACallFromParent, 0) END [CallFromParent]
+, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(a.PAVisitAttempt, 0) END [VisitAttempted]
+, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(a.PAVisitMade, 0) END [VisitConducted]
+, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(a.PAOtherHVProgram, 0) END [Referrals]
+, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(a.PAParent2Office, 0) END [Parent2Office]
+, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(a.PAProgramMaterial, 0) END [ProgramMaterial]
+, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(a.PAGift, 0) END [Gift]
+, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(a.PACaseReview, 0) END [CaseReview]
+, CASE WHEN a.PADate < @StartDt THEN 0 ELSE ISNULL(a.PAOtherActivity, 0) END [OtherActivity]
 -- support fields
 --, b.ScreenDate , a.PADate 
 --, CASE WHEN c.DischargeDate > @EndDt THEN NULL ELSE c.DischargeDate END [xDischargeDate] 
 --,a.HVCaseFK, b.KempeDate, a.CaseStatus, a.KempeResult, a.DischargeReason, x.ReportDischargeText
 
-FROM AllPreAssessment AS a
+FROM 
+(SELECT * FROM AllPreAssessment 
+UNION
+SELECT * FROM @NoAnyPreAssessment
+) AS a
 JOIN CaseProgram AS c ON c.HVCaseFK = a.HVCaseFK
 JOIN HVCase AS b ON b.HVCasePK = a.HVCaseFK
 LEFT OUTER JOIN codeDischarge x ON x.DischargeCode = a.DischargeReason 
 AND x.DischargeUsedWhere LIKE '%PA%'
 LEFT OUTER JOIN Worker AS w ON w.WorkerPK = a.PAFAWFK
---WHERE a.ProgramFK = @programfk AND a.PADate BETWEEN @StartDt AND @EndDt
 ORDER BY [Participant]
-
-
-
 
 
 
