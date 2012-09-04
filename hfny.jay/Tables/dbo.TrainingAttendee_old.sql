425154a03242977ca5809bfc0b1db425e5e06341
Jay Robohn <jrobohn@albany.edu>
2012-09-04 19:38:13 +0000

    misc. changes

--- hfny.jay/Tables/dbo.TrainingAttendee_old.sql
+++ hfny.jay/Tables/dbo.TrainingAttendee_old.sql
@@ -7,7 +7,7 @@ CREATE TABLE [dbo].[TrainingAttendee_old]
 [ProgramFK] [int] NULL,
 [SubtopicFK] [int] NULL,
 [TopicFK] [int] NULL,
-[TrainingAttendeeCreateDate] [datetime] NOT NULL CONSTRAINT [DF_TrainingAttendee_TrainingAttendeeCreateDate] DEFAULT (getdate()),
+[TrainingAttendeeCreateDate] [datetime] NOT NULL CONSTRAINT [DF_TrainingAttendee_old_TrainingAttendeeCreateDate] DEFAULT (getdate()),
 [TrainingAttendeeCreator] [char] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [TrainingAttendeeEditDate] [datetime] NULL,
 [TrainingAttendeeEditor] [char] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
@@ -16,18 +16,6 @@ CREATE TABLE [dbo].[TrainingAttendee_old]
 [WorkerFK] [int] NOT NULL
 ) ON [PRIMARY]
 ALTER TABLE [dbo].[TrainingAttendee_old] ADD 
-CONSTRAINT [PK__Training__C0BC3C361B9317B3] PRIMARY KEY CLUSTERED  ([TrainingAttendee_oldPK]) ON [PRIMARY]
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-SET ANSI_NULLS ON
-GO
--- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- create trigger TR_TrainingAttendeeEditDate ON TrainingAttendee
--- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-CREATE TRIGGER [dbo].[TR_TrainingAttendeeEditDate] ON [dbo].[TrainingAttendee_old]
-For Update 
-AS
-Update TrainingAttendee Set TrainingAttendee.TrainingAttendeeEditDate= getdate()
-From [TrainingAttendee] INNER JOIN Inserted ON [TrainingAttendee].[TrainingAttendeePK]= Inserted.[TrainingAttendeePK]
+CONSTRAINT [PK__TrainingAttendee_old__C0BC3C361B9317B3] PRIMARY KEY CLUSTERED  ([TrainingAttendee_oldPK]) ON [PRIMARY]
+
 GO
