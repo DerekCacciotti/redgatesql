aecf3ab378ca421b64e8dc2a8483cdb6cef77c31
Jay Robohn <jrobohn@albany.edu>
2012-10-01 18:44:49 +0000

    Make report category larger and nullable

--- hfny.jay/Tables/dbo.ReportHistory.sql
+++ hfny.jay/Tables/dbo.ReportHistory.sql
@@ -2,17 +2,15 @@ CREATE TABLE [dbo].[ReportHistory]
 (
 [ReportHistoryPK] [int] NOT NULL IDENTITY(1, 1),
 [ProgramFK] [int] NOT NULL,
-[ReportCategory] [char] (2) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[ReportFK] [int] NULL,
+[ReportCategory] [char] (2) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ReportFK] [int] NOT NULL,
 [ReportName] [char] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [ReportType] [char] (2) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [TimeRun] [datetime] NOT NULL,
 [UserFK] [char] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 ) ON [PRIMARY]
-ALTER TABLE [dbo].[ReportHistory] WITH NOCHECK ADD
-CONSTRAINT [FK_ReportHistory_ProgramFK] FOREIGN KEY ([ProgramFK]) REFERENCES [dbo].[HVProgram] ([HVProgramPK])
-ALTER TABLE [dbo].[ReportHistory] WITH NOCHECK ADD
-CONSTRAINT [FK_ReportHistory_ReportFK] FOREIGN KEY ([ReportFK]) REFERENCES [dbo].[codeReportCatalog] ([codeReportCatalogPK])
+
+
 
 
 GO
