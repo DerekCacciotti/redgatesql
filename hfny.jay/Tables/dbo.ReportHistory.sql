daca0df4fee46cce464ff404b6d649ba3eec44a5
Jay Robohn <jrobohn@albany.edu>
2012-10-01 18:50:12 +0000

    Make report category larger and nullable

--- hfny.jay/Tables/dbo.ReportHistory.sql
+++ hfny.jay/Tables/dbo.ReportHistory.sql
@@ -2,12 +2,13 @@ CREATE TABLE [dbo].[ReportHistory]
 (
 [ReportHistoryPK] [int] NOT NULL IDENTITY(1, 1),
 [ProgramFK] [int] NOT NULL,
-[ReportCategory] [char] (2) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ReportCategory] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [ReportFK] [int] NOT NULL,
 [ReportName] [char] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[ReportType] [char] (2) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[ReportType] [char] (2) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [TimeRun] [datetime] NOT NULL,
-[UserFK] [char] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+[UserFK] [char] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[ReportFK_old] [int] NULL
 ) ON [PRIMARY]
 
 
