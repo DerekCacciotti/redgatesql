309d25aded70ea5e0336f30cc8527e25a064ee86
Jay Robohn <jrobohn@albany.edu>
2012-10-30 20:21:37 +0000

    Small tweak to getFatherFigures

--- hfny.jay/Stored Procedures/dbo.spGetFatherFiguresByHVCaseFK.sql
+++ hfny.jay/Stored Procedures/dbo.spGetFatherFiguresByHVCaseFK.sql
@@ -12,6 +12,7 @@ CREATE procedure [dbo].[spGetFatherFiguresByHVCaseFK]
 as
 	set nocount on;
 
+	--spGetFatherFiguresByHVCaseFK 5938,1
 	--declare @IsApproved bit
 	--exec spGetApprovalStatus @FormType = 'FF' --varchar(2)
 	--							,@FormFK = 0 --int
@@ -19,7 +20,9 @@ as
 	--							,@ProgramFK = 0 --int
 	--							,@isApproved = 0 --bit
 
-	select FatherFigurePK
+	select isnull(IsApproved,0) as IsApproved
+		  ,0 as IsReviewRequired
+		  ,FatherFigurePK
 		  ,DateAcceptService
 		  ,DateInactive
 		  ,FatherAdvocateFK
@@ -43,8 +46,6 @@ as
 		  ,case when FF.IsOBP=1 then 'Yes' else 'No' end as OBPOfCase
 		  ,case when FF.IsPC2=1 then 'Yes' else 'No' end as PC2OfCase
 		  ,case when FF.LiveInPC1Home=1 then 'Yes' else 'No' end as InPC1Home
-		  ,isnull(IsApproved,0) as IsApproved
-		  ,0 as IsReviewRequired
 		from FatherFigure FF
 		inner join PC P on P.PCPK = FF.PCFK
 		left outer join dbo.FormReviewedTableList('FF',@ProgramFK) frtl on FormFK = FatherFigurePK
