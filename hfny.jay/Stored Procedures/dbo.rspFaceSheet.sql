d1d7263bea9e4ea619978e9c5fe9f5854bc95edc
Chun Dar Chen <cchen2@albany.edu>
2012-11-27 18:05:22 +0000

    HW434-HW436  Face Sheet - missing TC info and others Fixed

--- hfny.jay/Stored Procedures/dbo.rspFaceSheet.sql
+++ hfny.jay/Stored Procedures/dbo.rspFaceSheet.sql
@@ -4,20 +4,28 @@ GO
 SET ANSI_NULLS ON
 GO
 -- =============================================
--- Author:		<Jay Robohn>
+-- Author:		<Jay Robohn> <dar chen>
 -- Create date: <Jan 25, 2012>
 -- Description:	<Face Sheet report>
 -- =============================================
 CREATE procedure [dbo].[rspFaceSheet]
 (
-    @ProgramFK varchar(max)    = null,
-    @PC1ID char(13),
-    @WorkerFK int,
-    @FAWFK int, 
-    @SupervisorFK int
+    @ProgramFK varchar(max) = null,
+    @PC1ID char(13) = null,
+    @WorkerFK INT = null,
+    @FAWFK INT = null, 
+    @SupervisorFK INT = null
 )
 as
 begin
+
+--DECLARE @ProgramFK VARCHAR(MAX) = N'1'
+--DECLARE @PC1ID CHAR(13) = N'AB91010007555'
+--DECLARE @WorkerFK INT = NULL
+--DECLARE	@FAWFK INT = NULL
+--DECLARE @SupervisorFK INT = NULL
+
+
 	if @ProgramFK is null
 	begin
 		select @ProgramFK = substring((select ','+LTRIM(RTRIM(STR(HVProgramPK)))
@@ -57,9 +65,10 @@ begin
 			  ,CaseProgress
 			  ,isnull(PC1FK,0) as PC1FK
 			  ,isnull(OBPFK,0) as OBPFK
-			  ,OBPinHomeIntake
+			  ,CASE WHEN OBPinHomeIntake = 1 THEN 'Yes' ELSE '' END [OBPinHomeIntake]
 			  ,isnull(PC2FK,0) as PC2FK
-			  ,PC2inHomeIntake
+			  ,CASE WHEN PC2inHomeIntake = 1 THEN 'Yes' ELSE '' END [PC2inHomeIntake]
+			  ,c.PC2Relation2TC
 			  ,isnull(CPFK,0) as CPFK
 			  ,rtrim(PC1.PCFirstName)+' '+rtrim(PC1.PCLastName) as PC1FullName
 			  ,rtrim(PC1.PCStreet)+case
@@ -91,10 +100,12 @@ begin
 			  ,k.FAWFK
 			  ,rtrim(PCOBP.PCFirstName)+' '+rtrim(PCOBP.PCLastName) as OBPFullName
 			  ,PCOBP.PCDOB as OBPDOB
-			  ,PCOBP.Gender as OBPGender
+			  ,CASE WHEN PCOBP.Gender = '01' THEN 'Female' 
+			  WHEN PCOBP.Gender = '02' THEN 'Male' ELSE '' END  as OBPGender
 			  ,rtrim(PCPC2.PCFirstName)+' '+rtrim(PCPC2.PCLastName) as PC2FullName
 			  ,PCPC2.PCDOB as PC2DOB
-			  ,PCPC2.Gender as PC2Gender
+	    	   ,CASE WHEN PCPC2.Gender = '01' THEN 'Female' 
+			  WHEN PCPC2.Gender = '02' THEN 'Male' ELSE '' END  as PC2Gender
 			  ,rtrim(PCCP.PCFirstName)+' '+rtrim(PCCP.PCLastName) as CPFullName
 			  ,rtrim(PCCP.PCStreet)+case
 										when PCCP.PCApt is null or PCCP.PCApt = '' then
@@ -109,9 +120,9 @@ begin
 			  ,rtrim(TCFirstName)+' '+rtrim(TCLastName) as TCFullName
 			  ,t.TCDOB
 			  ,case
-				   when TCGender = '1' then
+				   when TCGender = '01' then
 					   'Female'
-				   when TCGender = '2' then
+				   when TCGender = '02' then
 					   'Male'
 			   end as TCGender
 			  ,(datediff(day,t.TCDOB,getdate()))/30.44 as TCChronologicalAge
@@ -140,11 +151,13 @@ begin
 				left outer join TCID t on t.HVCaseFK = c.HVCasePK
 				left outer join codeLevel cl on cl.codeLevelPK = cp.CurrentLevelFK
 				left outer join codeDischarge cd on cd.DischargeCode = cp.DischargeReason
+	
 			where PC1ID = isnull(@PC1ID,PC1ID)
 				 and CurrentFSWFK = isnull(@WorkerFK,CurrentFSWFK)
 				 and CurrentFAWFK = isnull(@FAWFK,CurrentFAWFK)
 				 and sup.WorkerPK = isnull(@SupervisorFK,sup.WorkerPK)
 				 and caseprogress >= 6
+				 
 	-- and PC1ID='SP80040113929'
 	),
 	cteFSWAssignDate
@@ -154,273 +167,217 @@ begin
 				inner join cteMain on HVCaseFK = HVCasePK
 			where WorkerFK = CurrentFSWFK
 			group by HVCaseFK
-	)
-	--,
-	--cteLevelChanges
-	--as
-	--(select casefk
-	--	   ,count(casefk)-1 as LevelChanges
-	--	 from cteHVRecords
-	--	 group by casefk
-	--)
-	--,
-	--cteSummary
-	--as
-	--(select distinct workername
-	--				,workerfk
-	--				,pc1id
-	--				,casecount
-	--				,sum(visitlengthminute) over (partition by pc1wrkfk) as 'Minutes'
-	--				,sum(expvisitcount) over (partition by pc1wrkfk) as expvisitcount
-	--				,min(startdate) over (partition by pc1wrkfk) as 'startdate'
-	--				,max(enddate) over (partition by pc1wrkfk) as 'enddate'
-	--				,levelname
-	--				,max(levelstart) over (partition by pc1wrkfk) as 'levelstart'
-	--				,sum(actvisitcount) over (partition by pc1wrkfk) as actvisitcount
-	--				,sum(inhomevisitcount) over (partition by pc1wrkfk) as inhomevisitcount
-	--				,sum(attvisitcount) over (partition by pc1wrkfk) as attvisitcount
-	--				,max(dischargedate) over (partition by pc1wrkfk) as 'dischargedate'
-	--				,LevelChanges
-	--	 from cteMain
-	--		 inner join cteLevelChanges on cteLevelChanges.casefk = cteHVRecords.casefk
-	--)
-	select Main.HVCasePK
-		  ,Main.PC1ID
-		  ,Main.CurrentFSWFK
-		  ,Main.CurrentLevelFK
-		  ,Main.LevelName
-		  ,Main.CurrentLevelDate
-		  ,Main.DaysOnCurrentLevel
-		  ,Main.MonthsInProgram
-		  ,Main.ScreenDate
-		  ,Main.KempeDate
-		  ,Main.IntakeDate
-		  ,Main.DischargeDate
-		  ,Main.DischargeReasonName
-		  ,Main.CaseProgress
-		  ,Main.PC1FK
-		  ,Main.OBPFK
-		  ,Main.OBPinHomeIntake
-		  ,Main.PC2FK
-		  ,Main.PC2inHomeIntake
-		  ,Main.CPFK
-		  ,Main.PC1FullName
-		  ,Main.PC1Street
-		  ,Main.PC1CSZ
-		  ,Main.PCPhone
-		  ,Main.PCCellPhone
-		  ,Main.SocialSecurityNumberOnFile
-		  ,Main.PCDOB
-		  ,Main.CurrentAge
-		  ,Main.AgeAtIntake
-		  ,Main.WorkerName
-		  ,Main.WorkerNameLast
-		  ,Main.FirstName
-		  ,Main.LastName
-		  ,Main.FAWName
-		  ,Main.SupervisorName
-		  ,Main.MomScore
-		  ,Main.DadScore
-		  ,Main.FAWFK
-		  ,Main.OBPFullName
-		  ,Main.OBPDOB
-		  ,Main.OBPGender
-		  ,Main.PC2FullName
-		  ,Main.PC2DOB
-		  ,Main.PC2Gender
-		  ,Main.CPFullName
-		  ,Main.CPStreet
-		  ,Main.CPCSZ
-		  ,Main.CPDOB
-		  ,Main.CPPhone
-		  ,Main.TCIDPK
-		  ,Main.TCFullName
-		  ,Main.TCDOB
-		  ,Main.TCGender
-		  ,Main.TCChronologicalAge
-		  ,Main.GestationalAge
-		  ,Main.TCDevelopmentalAge
-		  ,Main.NumberofChildren
-		  ,Main.TCDeceased
-		  ,FSWAssignDate
-		from cteMain Main
-			inner join cteFSWAssignDate on HVCaseFK = HVCasePK
-		order by LastName
-				,FirstName
-				,PC1ID
---order by WorkerName
---		,pc1id
+	), 
+	
+	
+-- PC1 Medical Insurance, Benefits, TANF service eligible (xx1)
+pc1MedicalInsurance
+AS (
+SELECT hh.HVCaseFK
+, convert(VARCHAR(MAX), ca.FormDate, 101) [PC1FormDate]
+, CASE WHEN ca.PC1ReceivingMedicaid = 1 THEN 'MA' 
+WHEN ca.HIFamilyChildHealthPlus = 1 THEN 'HealthPlus'
+WHEN ca.HIPrivate = 1 THEN 'Private'
+WHEN ca.HIUninsured = 1 THEN 'Uninsured'
+WHEN ca.HIUnknown = 1 THEN 'Other'
+WHEN ca.HIOther = 1 THEN 'Other'
+ELSE '' END [PC1MedicalInsurance]
+, CASE WHEN ca.PC1ReceivingMedicaid = 1 THEN 'On File' ELSE '' END [PC1MAOnFile]
+, CASE WHEN ca.PBEmergencyAssistance = 1 THEN 'EA ' ELSE '' END +
+CASE WHEN ca.PBFoodStamps = 1 THEN 'FS ' ELSE '' END +
+CASE WHEN ca.PBSSI = 1 THEN 'SSI ' ELSE '' END +
+Case WHEN ca.PBTANF = 1 THEN 'TANF ' ELSE '' END +
+Case WHEN ca.PBWIC = 1 THEN 'WIC' ELSE '' END [PC1Benefits]
+, CASE WHEN  ca.TANFServices = 1 THEN 'Yes' ELSE '' END [TANFServiceEligible]
+FROM(
+SELECT HVCaseFK, cast(substring(maxkey, 9, 10) AS INT) CommonAttributesPK
+FROM (SELECT HVCaseFK,
+max(convert(VARCHAR(max), FormDate, 112) + cast(CommonAttributesPK AS VARCHAR(max))) [maxkey]
+FROM CommonAttributes
+WHERE FormType IN ('IN', 'FU-PC1')
+GROUP BY HVCaseFK) AS xyz) AS hh
+LEFT OUTER JOIN CommonAttributes ca ON hh.CommonAttributesPK = ca.CommonAttributesPK 
+),
 
-end
+pc1DoctorClinic
+AS (
+-- PC1 Doctor name/phone and Facility name/phone (xx2)
+SELECT hh.HVCaseFK --, ca.PC1MedicalFacilityFK, ca.PC1MedicalProviderFK
+,rtrim(lmf.MFName) [PC1Cllinic]
+,rtrim(lmp.MPFirstName) + ' ' + rtrim(lmp.MPLastName) [PC1DoctorName]
+,lmp.MPPhone [PC1DoctorPhone]
+FROM(
+SELECT HVCaseFK, cast(substring(maxkey, 9, 10) AS INT) CommonAttributesPK
+FROM (SELECT HVCaseFK,
+max(convert(VARCHAR(max), FormDate, 112) + cast(CommonAttributesPK AS VARCHAR(max))) [maxkey]
+FROM CommonAttributes
+WHERE FormType IN ('IN', 'CH')
+GROUP BY HVCaseFK) AS xyz) AS hh --ON	hh.HVCaseFK = c.HVCasePK
+LEFT OUTER JOIN CommonAttributes ca ON hh.CommonAttributesPK = ca.CommonAttributesPK 
+LEFT OUTER JOIN listMedicalFacility lmf ON ca.PC1MedicalFacilityFK = lmf.listMedicalFacilityPK
+LEFT OUTER JOIN listMedicalProvider lmp ON ca.PC1MedicalProviderFK = lmp.listMedicalProviderPK
+),
 
+TCDoctorClinic
+AS (
+-- TC Doctor name/phone and Facility name/phone (xx3)
+SELECT hh.HVCaseFK --ca.TCMedicalFacilityFK, ca.TCMedicalProviderFK
+,rtrim(lmf.MFName) [TCClinic],
+rtrim(lmp.MPFirstName) + ' ' + rtrim(lmp.MPLastName) [TCDoctorName]
+,lmp.MPPhone [TCDoctorPhone]
+, CASE WHEN ca.TCReceivingMedicaid = 1 THEN 'MA' 
+WHEN ca.TCHIFamilyChildHealthPlus = 1 THEN 'HealthPlus'
+WHEN ca.TCHIPrivateInsurance = 1 THEN 'Private'
+WHEN ca.TCHIUninsured = 1 THEN 'Uninsured'
+WHEN ca.TCHIUnknown = 1 THEN 'Other'
+WHEN ca.TCHIOther = 1 THEN 'Other'
+ELSE '' END [TCMedicalInsurance]
+, CASE WHEN ca.TCReceivingMedicaid = 1 THEN 'On File'  ELSE '' END [TCMAOnFile]
+FROM(
+SELECT HVCaseFK, cast(substring(maxkey, 9, 10) AS INT) CommonAttributesPK
+FROM (SELECT HVCaseFK,
+max(convert(VARCHAR(max), FormDate, 112) + cast(CommonAttributesPK AS VARCHAR(max))) [maxkey]
+FROM CommonAttributes
+WHERE FormType IN ('FU', 'TC')
+GROUP BY HVCaseFK) AS xyz) AS hh
+LEFT OUTER JOIN CommonAttributes ca ON hh.CommonAttributesPK = ca.CommonAttributesPK 
+LEFT OUTER JOIN listMedicalFacility lmf ON ca.TCMedicalFacilityFK = lmf.listMedicalFacilityPK
+LEFT OUTER JOIN listMedicalProvider lmp ON ca.TCMedicalProviderFK = lmp.listMedicalProviderPK
+),
 
---hvcase5.pc1_fk=pc5.pc_pk AND ("Level" $ cur_level OR "Preintake" $ cur_level);		
+TCASQ
+AS (
+-- TC ASQ (xx4)
+SELECT hh.HVCaseFK
+, cast(cast(ASQ.TCAge AS INT) AS VARCHAR(5)) + ' months' [TCASQMonths]
+, convert(VARCHAR(max), ASQ.DateCompleted, 101) [TCASQDate]
+, CASE WHEN UnderCommunication = 1 THEN 'COM ' ELSE '' END +
+CASE WHEN UnderFineMotor = 1 THEN 'FM ' ELSE '' END +
+CASE WHEN UnderGrossMotor = 1 THEN 'GM ' ELSE '' END +
+CASE WHEN UnderPersonalSocial = 1 THEN 'PS ' ELSE '' END +
+CASE WHEN UnderProblemSolving = 1 THEN 'PBS' ELSE '' END [TCASQAREA]
+FROM (SELECT HVCaseFK, cast(substring(maxkey, 9, 10) AS INT) ASQPK
+FROM (SELECT HVCaseFK
+,max(convert(VARCHAR(max), DateCompleted, 112) + cast(ASQPK AS VARCHAR(max))) [maxkey]
+FROM ASQ
+WHERE UnderCommunication = 1 or UnderFineMotor = 1 OR
+UnderGrossMotor = 1 OR UnderPersonalSocial = 1 OR
+UnderProblemSolving = 1
+GROUP BY HVCaseFK) xyz ) hh
+JOIN ASQ ON ASQ.ASQPK = hh.ASQPK
+),
 
---Medical Provider FK (PC1)
---Medical Provider FK (TC)
---,mppc1_fk,mptc_fk,;
+pc1pc2InHome
+AS (
+-- PC1 and PC2 InHome from FollowUp table (xx5)
+SELECT hh.HVCaseFK
+, CASE WHEN fu.PC1InHome = 1 THEN 'Yes' ELSE '' END [PC1InHome]
+, CASE WHEN fu.PC2InHome = 1 THEN 'Yes' ELSE '' END [PC2InHome]
+FROM (SELECT HVCaseFK, cast(substring(maxkey, 9, 10) AS INT) FollowUpPK
+FROM (SELECT HVCaseFK,
+max(convert(VARCHAR(max), FollowUpDate, 112) + cast(FollowUpPK AS VARCHAR(max))) [maxkey]
+FROM FollowUp 
+GROUP BY HVCaseFK) xyz) hh
+JOIN FollowUp fu ON fu.FollowUpPK = hh.FollowUpPK
+)
 
-/*
-  IIF(PC1MA='1','MA','  ') as ma,;
-  IIF(CA_PC1PCAP='1','PCAP','    ') as pcap,;
-  IIF(CA_PC1CHIH='1','CHP',SPACE(3)) as chp,;
-  IIF(ca_pc1priv='1','Priv',SPACE(4)) as priv,;
-  IIF(ca_pc1hiot='1','Other',SPACE(5)) as other,;
-  IIF(ca_pc1none='1',"None",SPACE(4)) as none,;
-  IIF(pc1ma='1',IIF(!EMPTY(cpc1macase),"On file   ","Not on file"),SPACE(11)) as ma_of,;
-  IIF(ca_pc1pcap='1',IIF(!EMPTY(pc1pcapno),"On file    ","Not on file"),SPACE(11)) as pcap_of,;
-  IIF(tcMA='1','MA','  ') as Tcma,;
-  IIF(CA_tcCHIH='1','CHP',SPACE(3)) as tcchp,;
-  IIF(ca_tcpriv='1','Priv',SPACE(4)) as tcpriv,;
-  IIF(ca_tcother='1','Other',SPACE(5)) as tcother,;
-  IIF(ca_tcnone='1',"None",SPACE(4)) as tcnone,;
-  IIF(tcma='1',IIF(!EMPTY(ctcmacase),"On file   ","Not on file"),SPACE(11)) as tcma_of,;
-  pc1_inhome, pc2_inhome, cur_pc2ih, ;
-
-INTO CURSOR fs1    
-
-**Add preasses info (date worker assigned)
-SELECT fs4.*, datefswass FROM fs4 LEFT OUTER JOIN preasse5 ;
-   ON fs4.case_pk=preasse5.case_fk AND pa_castat='2' INTO CURSOR FS5 
-  
-***Add the closed reason
-SELECT fs5.*, reason FROM fs5 ;
-  LEFT OUTER JOIN clsdcode5 ON why_notc=cl_code INTO CURSOR fs6
-  
-***Add pc2 info
-SELECT fs6.*, ALLTRIM(pcfname)+SPACE(1)+ALLTRIM(pclname) as pc2name,;
-pc5.pcdob as pc2dob, pcsex as pc2sex  FROM fs6 LEFT OUTER JOIN pc5 ON ;
-  fs6.pc2_fk=pc5.pc_pk INTO CURSOR FS7
-
-***Add cp info
-SELECT fs7.*, ALLTRIM(pcfname)+SPACE(1)+ALLTRIM(pclname) as cpname,;
-  ALLTRIM(PCSTREET)+ IIF(!EMPTY(PCAPT),", "," ")+ALLTRIM(PCAPT) AS cp_STREET,;
-  ALLTRIM(pccity)+IIF(!EMPTY(pccity),", NY ","NY ")+ LEFT(ALLTRIM(pczipcod),5) as cp_city,;
-  pcphone as cp_phone,pc5.pcdob as cp_dob ;
-FROM fs7 LEFT OUTER JOIN pc5 ON fs7.cp_fk=pc5.pc_pk INTO CURSOR FS8
-
-***add medprov5
-SELECT FS9.*,;
-   "Dr. "+alltrim(mpa.mpfname)+space(1)+alltrim(mpa.mplname)+iif(!empty(mpa.mplname),';','')+alltrim(mpa.mpfacility) as pc1dr,;
-   mpa.mpphone as pc1mpphone;  
-FROM fs9 LEFT OUTER JOIN medprov5 mpa  ON ;
-mpa.mp_pk=fs9.mppc1_fk into cursor fs9a
-
-SELECT FS9a.*,;
-   "Dr. "+alltrim(mpb.mpfname)+space(1)+alltrim(mpb.mplname)+iif(!empty(mpb.mplname),';','')+alltrim(mpb.mpfacility) as tcdr,;
-   mpb.mpphone as tcmpphone ;   
-FROM fs9a LEFT OUTER JOIN medprov5 mpb ON ;
-mpb.mp_pk=fs9a.mptc_fk into cursor fs10
-
-***make cursor with asq's under cutoff
-SELECT tc_age, iif(fmot_under,"FM","  ") as fmot,iif(gmot_under,"GM","  ") as gmot,;
-IIF(COMM_UNDER,"COM","   ") AS com, IIF(psolv_unde,"PROB","   ") AS psolv,;
-IIF(psoc_under,"SOC","   ") as psoc, ;
-max(aq_datcomp) as asq_datcomp, aq_tc_fk from hvasq5 ;
-where fmot_under or gmot_under;
-    OR comm_under OR psolv_unde;
-    OR psoc_under ;
-group by aq_tc_fk ;
-INTO CURSOR fs11
-
-***add asq's to file
-
-SELECT fs10.*, tc_age, fs11.asq_datcomp,;
-  alltrim(com)+iif(!emptY(com)," ","")+;
-  alltrim(gmot)+iif(!empty(gmot)," ","")+;
-  ALLTRIM(fmot)+IIF(!EMPTY(fmot)," ","")+;
-  ALLTRIM(psolv)+IIF(!EMPTY(psolv)," ","")+;
-  ALLTRIM(psoc) as last_uasq;
-FROM fs10 LEFT OUTER JOIN fs11 ON fs10.tcid_pk=fs11.aq_tc_fk;
-INTO CURSOR fs12
-
-sele * from all_code5 where a_group="Tc_age" into cursor ages
-
-sele fs12.*,alltrim(code_txt) as tcage from fs12 LEFT OUTER JOIN ages ;
-  ON VAL(fs12.tc_age)=VAL(ages.a_code);
-  INTO CURSOR fs13
-
-****add the intake or latest fup
-sele fs13.*, IIF(xtra_fld6='3' or xtra_fld6='4','Intake'+SPACE(14), ;
- IIF(VAL(xtra_fld6)>4,all_code5.code_txt,SPACE(20))) as info_from from fs13;
-LEFT OUTER JOIN all_code5 ON VAL(fs13.xtra_fld6)= VAL(all_code5.a_code) ;
-WHERE a_group="Xtra_fld6" INTO CURSOR fs14
-
-***add the benefits
-***make cursor with intake info
-
-SELECT iif(CR_AFDC_HR,"TANF","    ") AS TANF,;
-       IIF(cr_foodstm,"FS","  ") as fs,;
-       IIF(cr_emer_as,"EA","  ") as ea,;
-       IIF(cr_wic,"WIC","   ") as wic,;
-       IIF(cr_ssi_ssd,"SSI/SSD","       ") AS ssi,;
-       meettanf,;
-       case_fk,in_dointak FROM intake25 INTO CURSOR fs15
-
-*ADD INTAKE INFO TO TABLE
-SELECT fs14.*,;
-  IIF(info_from="Intake", ;
-  ALLTRIM(tanf)+IIF(!EMPTY(tanf)," ","") + ;
-  ALLTRIM(fs)+ IIF(!EMPTY(fs)," ","") + ;
-  ALLTRIM(ea)+ IIF(!EMPTY(ea)," ","") + ;
-  ALLTRIM(wic)+ IIF(!EMPTY(wic)," ","") + ;
-  ALLTRIM(ssi), space(18)) as BENES, in_dointak as from_date,;
-   IIF(EMPTY(meettanf),"   ",IIF(meettanf=.t.,"Yes",IIF(meettanf=.f.,"No","   "))) as meettanf ;
-FROM fs14 LEFT OUTER JOIN fs15 ON FS14.case_pk=fs15.case_fk ;
-INTO CURSOR FS16 readwrite
-
-***make the fup benefits table &&& group by cursor corrected 11/20/03
-
-SELECT MAX(f2_datcomp) as from_date,case_fk FROM hvfup25 GROUP BY case_fk INTO CURSOR fup25s
-
-SELECT iif(AFDC_HR='1',"TANF","    ") AS TANF,;
-       IIF(fOOD_STMP='1',"FS","  ") as fs,;
-       IIF(emer_asS='1',"EA","  ") as ea,;
-       IIF(wic='1',"WIC","   ") as wic,;
-       IIF(ssi_ssd='1',"SSI/SSD","       ") AS ssi,;
-       case_fk, meet_tanf, f2_datcomp FROM HVFUP25 INTO CURSOR fs17;
-       where STR(CASE_FK)+TRANSFORM(f2_datcomp) in ;
-       (select STR(CASE_FK)+TRANSFORM(from_date) FROM fup25s) 
-       
-SELECT fs17
-SCAN
-	SELECT fs16
-	REPLACE benes with ALLTRIM(fs17.tanf)+IIF(!EMPTY(fs17.tanf)," ","") + ;
-  				       ALLTRIM(fs17.fs)+ IIF(!EMPTY(fs17.fs)," ","") + ;
-                       ALLTRIM(fs17.ea)+ IIF(!EMPTY(fs17.ea)," ","") + ;
-				  	   ALLTRIM(fs17.wic)+ IIF(!EMPTY(fs17.wic)," ","") + ;
-				       ALLTRIM(fs17.ssi), from_date with fs17.f2_datcomp, ;
-				MEETTANF WITH IIF(EMPTY(fs17.meet_tanf),"   ",IIF(fs17.meet_tanf=.t.,"Yes","No")) ;
-   FOR case_pk=fs17.case_fk
-ENDSCAN       
-***ADD FAW NAME    
-SELECT fs16.*, ALLTRIM(HV_FNAME)+SPACE(1)+ALLTRIM(hv_lname) as faw_name ;
-FROM fs16 LEFT OUTER JOIN hvwrkr5 ON k_faw_fk=wrkr_pk INTO CURSOR FS18
-
-***GET ALL PC2 INTAKE RECORDS
-SELECT * FROM intake5 WHERE in_ptype="PC2" INTO CURSOR pc2rel
-***GET ALL_CODE RECORDS FOR REL_TC
-SELECT code_Txt,a_code FROM all_code5 WHERE a_group="rel_tc" INTO CURSOR code1
-***ADD REL_TC TXT TO THE INTAKE RECORDS (PC2)        
-SELECT pc2rel.*, code_txt as pc2reltc FROM pc2rel LEFT OUTER JOIN code1;
-     ON pc2rel.pcreltc=code1.a_code INTO CURSOR FS21
-
-SELECT FS18.*,FS21.pc2reltc FROM fs18 LEFT OUTER JOIN FS21 ON fs18.case_pk=fs21.case_fk;
-INTO CURSOR FS19
-
-sele *, ;
- ALLTRIM(MA)+IIF(EMPTY(MA),""," ") +;
- ALLTRIM(pcap)+IIF(EMPTY(pcap),""," ") +;
- ALLTRIM(chp)+IIF(EMPTY(chp),""," ") +;
- ALLTRIM(priv)+IIF(EMPTY(priv),""," ") +;
- ALLTRIM(other)+IIF(EMPTY(other),""," ") +;
- ALLTRIM(none) as pc1hi, ;
- ALLTRIM(tcma)+IIF(EMPTY(tcma),""," ") +;
- ALLTRIM(tcchp)+IIF(EMPTY(tcchp),""," ") +;
- ALLTRIM(tcpriv)+IIF(EMPTY(tcpriv),""," ") +;
- ALLTRIM(tcother)+IIF(EMPTY(tcother),""," ") +;
- ALLTRIM(tcnone) as tchi ;
-FROM fs19 ORDER BY sup_fk, cur_wkr_fk,ca_pc1_id INTO CURSOR facesheet      
-       
-*/
+select Main.HVCasePK
+	  ,Main.PC1ID
+	  ,Main.CurrentFSWFK
+	  ,Main.CurrentLevelFK
+	  ,Main.LevelName
+	  ,Main.CurrentLevelDate
+	  ,Main.DaysOnCurrentLevel
+	  ,Main.MonthsInProgram
+	  ,Main.ScreenDate
+	  ,Main.KempeDate
+	  ,Main.IntakeDate
+	  ,Main.DischargeDate
+	  ,Main.DischargeReasonName
+	  ,Main.CaseProgress
+	  ,Main.PC1FK
+	  ,Main.OBPFK
+	  ,Main.OBPinHomeIntake
+	  ,Main.PC2FK
+	  ,Main.PC2inHomeIntake
+	  ,a1.AppCodeText AS PC2Relation2TC
+	  ,Main.CPFK
+	  ,Main.PC1FullName
+	  ,Main.PC1Street
+	  ,Main.PC1CSZ
+	  ,Main.PCPhone
+	  ,Main.PCCellPhone
+	  ,Main.SocialSecurityNumberOnFile
+	  ,Main.PCDOB
+	  ,Main.CurrentAge
+	  ,Main.AgeAtIntake
+	  ,Main.WorkerName
+	  ,Main.WorkerNameLast
+	  ,Main.FirstName
+	  ,Main.LastName
+	  ,Main.FAWName
+	  ,Main.SupervisorName
+	  ,Main.MomScore
+	  ,Main.DadScore
+	  ,Main.FAWFK
+	  ,Main.OBPFullName
+	  ,Main.OBPDOB
+	  ,Main.OBPGender
+	  ,Main.PC2FullName
+	  ,Main.PC2DOB
+	  ,Main.PC2Gender
+	  ,Main.CPFullName
+	  ,Main.CPStreet
+	  ,Main.CPCSZ
+	  ,Main.CPDOB
+	  ,Main.CPPhone
+	  ,Main.TCIDPK
+	  ,Main.TCFullName
+	  ,Main.TCDOB
+	  ,Main.TCGender
+	  ,Main.TCChronologicalAge
+	  ,Main.GestationalAge
+	  ,Main.TCDevelopmentalAge
+	  ,Main.NumberofChildren
+	  ,Main.TCDeceased
+	  ,FSWAssignDate
+	  
+	  ,xx1.[PC1FormDate]
+	  ,xx1.[PC1MedicalInsurance]
+	  ,xx1.[PC1MAOnFile]
+	  ,xx1.[PC1Benefits]
+	  ,xx1.[TANFServiceEligible]
+	  
+	  ,xx2.[PC1Cllinic]
+	  ,xx2.[PC1DoctorName]
+	  ,xx2.[PC1DoctorPhone]
+	  
+	  ,xx3.[TCClinic]
+	  ,xx3.[TCDoctorName]
+	  ,xx3.[TCDoctorPhone]
+	  ,xx3.[TCMedicalInsurance]
+	  ,xx3.[TCMAOnFile]
+	  
+	  ,xx4.[TCASQMonths]
+	  ,xx4.[TCASQDate]
+	  ,xx4.[TCASQAREA]
+	
+	  ,xx5.[PC1InHome]
+	  ,xx5.[PC2InHome]
+	  
+	from cteMain Main
+		inner join cteFSWAssignDate on HVCaseFK = HVCasePK
+		LEFT OUTER JOIN codeApp a1 ON a1.AppCode = Main.PC2Relation2TC AND a1.AppCodeGroup = 'Relation2PC1'
+		
+		LEFT OUTER JOIN pc1MedicalInsurance xx1 ON xx1.HVCaseFK = Main.HVCasePK
+		LEFT OUTER JOIN pc1DoctorClinic xx2 ON xx2.HVCaseFK = Main.HVCasePK
+		LEFT OUTER JOIN TCDoctorClinic xx3 ON xx3.HVCaseFK = Main.HVCasePK
+		LEFT OUTER JOIN TCASQ xx4 ON xx4.HVCaseFK = Main.HVCasePK
+		LEFT OUTER JOIN pc1pc2InHome xx5 ON xx5.HVCaseFK = Main.HVCasePK
+		
+		
+	order by LastName
+			,FirstName
+			,PC1ID
+END;
 GO
