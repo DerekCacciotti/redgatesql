35bb1ebdf8fcbb89b2ab9a04289c3db38f1c0b3b
Devinder Singh Khalsa <dkhalsa@albany.edu>
2012-12-06 12:42:59 +0000

    implementing new report template

--- hfny.jay/Stored Procedures/dbo.rspListMedicalFacility.sql
+++ hfny.jay/Stored Procedures/dbo.rspListMedicalFacility.sql
@@ -3,15 +3,21 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
+
 -- =============================================
 -- Author:		Devinder S. Khalsa
 -- Create date: 06/20/2012
+-- MC = MEDICAL Facilities, CSZ = CITY STATE ZIP, FNLN = FIRST NAME LAST NAME
+-- if bIncludeInactive = TRUE (means include inactive to active list) then we get ALL ACTIVE AND INACTIVE
 -- Description:	Report containing  List Of Medical Facilities
--- Usage: rspListMedicalFacility 1,1
+-- Usage: rspListMedicalFacility 1,1  -- INCLUDE ALL ACTIVE AND INACTIVE
+-- Usage: rspListMedicalFacility 1,0  -- INCLUDE ONLY ACTIVE
+
 -- =============================================
+
 CREATE PROCEDURE [dbo].[rspListMedicalFacility]
 ( @programfk INT,
-@bMFIsActive      bit             = null
+@bIncludeInactive      bit             = FALSE 
 )
 AS
 BEGIN
@@ -19,13 +25,35 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;  
   
+WITH cteMain AS
+(  
+  
 SELECT 
 isnull(rtrim(MFName),'') as MFNAME
+, MFIsActive
 ,isnull(rtrim(MFAddress),'') as MFADDRESS
 ,isnull(rtrim(MFCity) + ',','')+'  '+isnull(rtrim(MFState) + ',','')+'  '+isnull(rtrim(MFZip),'') as MFCSZ
 ,isnull(rtrim(MFPhone),'') as MFPHONE    
   FROM [listMedicalFacility]
-  WHERE ProgramFK = @programfk AND MFIsActive = @bMFIsActive
+  WHERE ProgramFK = @programfk AND 
+  MFIsActive = CASE WHEN NOT @bIncludeInactive = 1 THEN 1 ELSE MFIsActive END     
+  
+ ) 
+  
+  SELECT 
+		CASE WHEN MFIsActive = 1 THEN isnull(MFNAME,'') ELSE 
+		
+		 CASE WHEN MFNAME IS NULL THEN '' ELSE MFNAME + ' ( **Inactive )' END
+		 
+		
+		 END AS MFNAME  
+	   , MFIsActive
+	   , MFADDRESS
+	   , MFCSZ
+	   , MFPHONE FROM cteMain 
+  WHERE MFNAME <> ''
+  --OR  MFADDRESS IS NOT NULL
+  --OR  MFCSZ IS NOT NULL
   ORDER BY MFNAME
 
   
--- hfny.jay/Stored Procedures/dbo.rspListMedicalProvider.sql
+++ hfny.jay/Stored Procedures/dbo.rspListMedicalProvider.sql
@@ -7,12 +7,15 @@ GO
 -- Author:		Devinder S. Khalsa
 -- Create date: 06/20/2012
 -- MP = MEDICAL PROVIDER, CSZ = CITY STATE ZIP, FNLN = FIRST NAME LAST NAME
+-- if bIncludeInactive = TRUE (means include inactive to active list) then we get ALL ACTIVE AND INACTIVE
 -- Description:	Report containing  List Of Medical Providers
--- Usage: rspListMedicalProvider 1,1
+-- Usage: rspListMedicalProvider 1,1  -- INCLUDE ALL ACTIVE AND INACTIVE
+-- Usage: rspListMedicalProvider 1,0  -- INCLUDE ONLY ACTIVE
+
 -- =============================================
 CREATE PROCEDURE [dbo].[rspListMedicalProvider]
 ( @programfk INT,
-@bMPIsActive      bit             = null
+@bIncludeInactive      bit             = FALSE 
 )
 
 
@@ -22,6 +25,8 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;  
 
+WITH cteMain AS
+(  
   
 SELECT 
 isnull(rtrim(MPLastName),'')+
@@ -30,16 +35,30 @@ CASE WHEN len(isnull(rtrim(MPFirstName),'')) > 0 -- Add comma (,) if there is a 
  ELSE
   ''
   end as MPFNLN
+  ,MPIsActive
 ,isnull(rtrim(MPAddress),'') as MPADDRESS
 ,isnull(rtrim(MPCity) + ',','')+'  '+isnull(rtrim(MPState) + ',','')+'  '+isnull(rtrim(MPZip),'') as MPCSZ
 ,isnull(rtrim(MPPhone),'') as MPPHONE    
   FROM [listMedicalProvider]
-  WHERE ProgramFK = @programfk AND MPIsActive = @bMPIsActive
-  ORDER BY MPFNLN
+  WHERE ProgramFK = @programfk AND 
+  MPIsActive = CASE WHEN NOT @bIncludeInactive = 1 THEN 1 ELSE MPIsActive END 
+)  
+  
+  SELECT 
+		CASE WHEN MPIsActive = 1 THEN isnull(MPFNLN,'') ELSE 
+		
+		 CASE WHEN MPFNLN IS NULL THEN '' ELSE MPFNLN + ' ( **Inactive )' END
+		 
+		
+		 END AS MPFNLN      
  
-  
-  
-  
+	   , MPIsActive
+	   , MPADDRESS
+	   , MPCSZ
+	   , MPPHONE FROM cteMain 
+    
+  ORDER BY MPFNLN
+   
   
   END
 GO
