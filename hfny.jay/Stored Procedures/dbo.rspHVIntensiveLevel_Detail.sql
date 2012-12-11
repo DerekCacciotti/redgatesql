3b0a6209cd6bbf318f19b941c04fbb9a83798401
Chun Dar Chen <cchen2@albany.edu>
2012-12-11 18:40:39 +0000

    fix hvIntensiveLevel report

--- hfny.jay/Stored Procedures/dbo.rspHVIntensiveLevel_Detail.sql
+++ hfny.jay/Stored Procedures/dbo.rspHVIntensiveLevel_Detail.sql
@@ -4,8 +4,8 @@ GO
 SET ANSI_NULLS ON
 GO
 -- =============================================
--- Author:    <Jay Robohn>
--- Create date: <Feb 20, 2012>
+-- Author:    <Dar Chen>
+-- Create date: <Dec 11, 2012>
 -- Description: <4-1B. Intensive Home Visitation Level after Target Child is Born>
 -- =============================================
 CREATE procedure [dbo].[rspHVIntensiveLevel_Detail]
@@ -18,135 +18,95 @@ CREATE procedure [dbo].[rspHVIntensiveLevel_Detail]
     @negclause	 varchar(200)
 )
 as
-	if @programfk is null
-	begin
-		select @programfk = substring((select ','+LTRIM(RTRIM(STR(HVProgramPK)))
-										   from HVProgram
-										   for xml path ('')),2,8000)
-	end
 
-	set @programfk = REPLACE(@programfk,'"','')
-	set @sitefk = case when dbo.IsNullOrEmpty(@sitefk) = 1 then 0 else @sitefk end
-	set @posclause = case when @posclause = '' then null else @posclause end
+--declare    @programfk varchar(max)    = '1'
+--declare    @sdate     DATETIME = '01/01/2010'
+--declare    @edate     DATETIME = '01/01/2011'
+--declare    @sitefk		 int			 = null
+--declare    @posclause	 varchar(200) = null
+--declare    @negclause	 varchar(200) = null
 
-	select 2 [level1_less_183]
-		  ,b.PC1ID
-		  ,RTRIM(c.PCFirstName)+' '+c.PCLastName [Name]
-		  ,isnull(a.tcdob,a.edc) [edc_dob]
-		  ,a.IntakeDate
-		  ,yy.days_length_total
-		  ,rtrim(w.FirstName)+' '+w.LastName [WorkerName]
-		from hvcase as a
-			inner join caseprogram as b on b.hvcasefk = a.hvcasepk
-			inner join dbo.SplitString(@programfk,',') on b.programfk = listitem
-			inner join pc as c on c.PCPK = a.PC1FK
-			inner join Worker w on b.CurrentFSWFK = w.WorkerPK
-			join (select distinct x.hvcasefk [hvcasefk]
-								 ,[days_length_total]
-					  from (select q.hvcasefk
-								  ,sum(q.[days_length]) [days_length_total]
-								from (select p.hvcasefk
-											,p.Levelfk
-											,p.levelname
-											,p.StartLevelDate
-											,p.EndLevelDate
-											,p.FinalEndLevelDate
-											,(datediff(dd,p.StartLevelDate,p.FinalEndLevelDate)+1) [days_length]
-										  from (select d.hvcasefk
-													  ,d.Levelfk
-													  ,d.levelname
-													  ,d.StartLevelDate
-													  ,d.EndLevelDate
-													  ,case
-														   when d.EndLevelDate is null then
-															   @edate
-														   when d.EndLevelDate > @edate then
-															   @edate
-														   else
-															   d.EndLevelDate
-													   end [FinalEndLevelDate]
-													from hvcase as a
-														inner join caseprogram as b on b.hvcasefk = a.hvcasepk
-														inner join dbo.SplitString(@programfk,',') on b.programfk = listitem
-														join HVLevelDetail as d on a.hvcasepk = d.hvcasefk and b.programfk = d.programfk and d.StartLevelDate <= @edate
-														inner join dbo.udfCaseFilters(@posclause, @negclause, @programfk) cf on cf.HVCaseFK = a.HVCasePK
-														inner join WorkerProgram wp on wp.WorkerFK = CurrentFSWFK
-													where a.caseprogress >= 9
-														 and a.intakedate <= @edate
-														 and (b.dischargedate is null
-														 or b.dischargedate >= @sdate)
-														 and (case when @SiteFK = 0 then 1 when wp.SiteFK = @SiteFK then 1 else 0 end = 1)
-												) as p
-									) as q
-								where q.Levelfk in (14,27,24)
-								group by q.hvcasefk) as x
-					  where x.[days_length_total] >= 183) as yy on yy.hvcasefk = a.HVCasePK
+if @programfk is null
+begin
+	select @programfk = substring((select ','+LTRIM(RTRIM(STR(HVProgramPK)))
+									   from HVProgram
+									   for xml path ('')),2,8000)
+end
 
-	union
-	select 1 [level1_less_183]
-		  ,b.PC1ID
-		  ,RTRIM(c.PCFirstName)+' '+c.PCLastName [Name]
-		  ,isnull(a.tcdob,a.edc) [edc_dob]
-		  ,a.IntakeDate
-		  ,yy.days_length_total
-		  ,rtrim(w.FirstName)+' '+w.LastName [WorkerName]
-		from hvcase as a
-			inner join caseprogram as b on b.hvcasefk = a.hvcasepk
-			inner join dbo.SplitString(@programfk,',') on b.programfk = listitem
-			inner join pc as c on c.PCPK = a.PC1FK
-			inner join Worker w on b.CurrentFSWFK = w.WorkerPK
-			inner join WorkerProgram wp on CurrentFSWFK = WorkerFK
-			join (select distinct x.hvcasefk [hvcasefk]
-								 ,[days_length_total]
-					  from (select q.hvcasefk
-								  ,sum(q.[days_length]) [days_length_total]
-								from (select p.hvcasefk
-											,p.Levelfk
-											,p.levelname
-											,p.StartLevelDate
-											,p.EndLevelDate
-											,p.FinalEndLevelDate
-											,(datediff(dd,p.StartLevelDate,p.FinalEndLevelDate)+1) [days_length]
-										  from (select d.hvcasefk
-													  ,d.Levelfk
-													  ,d.levelname
-													  ,d.StartLevelDate
-													  ,d.EndLevelDate
-													  ,case
-														   when d.EndLevelDate is null then
-															   @edate
-														   when d.EndLevelDate > @edate then
-															   @edate
-														   else
-															   d.EndLevelDate
-													   end [FinalEndLevelDate]
-													from hvcase as a
-														inner join caseprogram as b on b.hvcasefk = a.hvcasepk
-														inner join dbo.SplitString(@programfk,',') on b.programfk = listitem
-														join HVLevelDetail as d on a.hvcasepk = d.hvcasefk and b.programfk = d.programfk and d.StartLevelDate <= @edate
-														inner join dbo.udfCaseFilters(@posclause, @negclause, @programfk) cf on cf.HVCaseFK = a.HVCasePK
-														inner join WorkerProgram wp on wp.WorkerFK = CurrentFSWFK
-													where a.caseprogress >= 9
-														 and a.intakedate <= @edate
-														 and (b.dischargedate is null
-														 or b.dischargedate >= @sdate)
-														 and (case when @SiteFK = 0 then 1 when wp.SiteFK = @SiteFK then 1 else 0 end = 1)
-												) as p
-									) as q
-								where q.Levelfk in (14,27,24)
-								group by q.hvcasefk) as x
-					  where x.[days_length_total] < 183) as yy on yy.hvcasefk = a.HVCasePK
-		where a.HVCasePK in (select distinct d.hvcasefk
-								 from hvcase as a
-									 inner join caseprogram as b on b.hvcasefk = a.hvcasepk
-									 inner join dbo.SplitString(@programfk,',') on b.programfk = listitem
-									 join HVLevelDetail as d on a.hvcasepk = d.hvcasefk
-										 and b.programfk = d.programfk
-										 and d.StartLevelDate <= @edate
-								 where a.caseprogress >= 9
-									  and a.intakedate <= @edate
-									  and (b.dischargedate is null
-									  or b.dischargedate >= @sdate)
-									  and d.Levelfk in (16,18,20))
+set @programfk = REPLACE(@programfk,'"','')
+set @sitefk = case when dbo.IsNullOrEmpty(@sitefk) = 1 then 0 else @sitefk end
+set @posclause = case when @posclause = '' then null else @posclause end
 
+;WITH xxx AS (
+
+SELECT 
+	b.PC1ID
+	, rtrim(p.PCFirstName) + ' ' + rtrim(p.PCLastName) [Name]
+	, isnull(a.tcdob,a.edc) [edc_dob]
+	, a.IntakeDate
+	, sum((datediff(dd, c.StartLevelDate, CASE When c.EndLevelDate is null THEN @edate
+	When c.EndLevelDate > @edate THEN @edate
+	ELSE c.EndLevelDate END + 1))) [days_length_total]
+	, rtrim(w.FirstName) + ' ' + rtrim(w.LastName) [WorkerName]
+	, count(*) [n]
+	,b.HVCaseFK
+
+FROM HVCase  AS a
+JOIN CaseProgram AS b ON a.HVCasePK = b.HVCaseFK
+JOIN HVLevelDetail AS c ON c.hvcasefk = a.HVCasePK AND c.StartLevelDate <= @edate
+JOIN udfCaseFilters(@posclause, @negclause, @programfk) cf on cf.HVCaseFK = a.HVCasePK
+JOIN Worker AS w on w.WorkerPK = b.CurrentFSWFK
+JOIN WorkerProgram AS wp ON wp.WorkerFK = w.WorkerPK
+JOIN PC AS p ON p.PCPK = a.PC1FK			
+WHERE b.ProgramFK = @programfk 
+AND a.caseprogress >= 9
+AND a.IntakeDate <= @edate
+AND (b.dischargedate is NULL or b.dischargedate >= @sdate)
+AND (case when @SiteFK = 0 then 1 when wp.SiteFK = @SiteFK then 1 else 0 end = 1)
+AND c.Levelfk IN (14)
+GROUP BY
+b.HVCaseFK, b.PC1ID
+, rtrim(p.PCFirstName) + ' ' + rtrim(p.PCLastName)
+, isnull(a.tcdob,a.edc)
+, a.IntakeDate
+, rtrim(w.FirstName) + ' ' + rtrim(w.LastName)
+)
+,
+
+yyy AS (
+SELECT DISTINCT a.HVCasePK
+FROM HVCase  AS a
+JOIN CaseProgram AS b ON a.HVCasePK = b.HVCaseFK
+JOIN HVLevelDetail AS c ON c.hvcasefk = a.HVCasePK AND c.StartLevelDate <= @edate
+JOIN udfCaseFilters(@posclause, @negclause, @programfk) cf on cf.HVCaseFK = a.HVCasePK
+JOIN Worker AS w on w.WorkerPK = b.CurrentFSWFK
+JOIN WorkerProgram AS wp ON wp.WorkerFK = w.WorkerPK
+WHERE b.ProgramFK = @programfk 
+AND a.caseprogress >= 9
+AND a.IntakeDate <= @edate
+AND (b.dischargedate is NULL or b.dischargedate >= @sdate)
+AND (case when @SiteFK = 0 then 1 when wp.SiteFK = @SiteFK then 1 else 0 end = 1)
+AND c.Levelfk IN (16,18,20)
+),
+
+zzz AS (
+SELECT 
+sum(CASE WHEN a.days_length_total >= 183 THEN 1 ELSE 0 END) [more_than_6mo]
+, sum(CASE WHEN a.days_length_total >= 183 THEN 0 ELSE 1 END) [less_than_6mo]
+, count(*) [total_number]
+FROM xxx AS a
+LEFT OUTER JOIN yyy AS b ON a.HVCaseFK = b.HVCasePK
+WHERE b.HVCasePK IS NOT NULL OR a.[days_length_total] >= 183
+)
+
+SELECT 
+CASE WHEN a.days_length_total >= 183 THEN 2 ELSE 1 END [level1_less_183],
+a.*,
+c.*,
+CASE WHEN b.HVCasePK IS NOT NULL THEN 'Yes' ELSE 'No' END [Level2_3_4]
+FROM xxx AS a
+JOIN zzz AS c ON 1 = 1
+LEFT OUTER JOIN yyy AS b ON a.HVCaseFK = b.HVCasePK
+WHERE b.HVCasePK IS NOT NULL OR a.[days_length_total] >= 183
+ORDER BY level1_less_183, PC1ID
 GO
