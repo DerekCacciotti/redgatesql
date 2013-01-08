feb3663f36fd7d4d4c0e5a0455a94f74ece8c0f6
Chun Dar Chen <cchen2@albany.edu>
2013-01-08 18:23:03 +0000

    Grouped bySupervisor and worker - request by john

--- hfny.jay/Stored Procedures/dbo.rspLevelChangeHistory.sql
+++ hfny.jay/Stored Procedures/dbo.rspLevelChangeHistory.sql
@@ -36,7 +36,10 @@ begin
 		set @pc1id = nullif(@pc1id,'')
 	end;
 	
-	select PC1ID
+	select 
+	      LTRIM(RTRIM(supervisor.firstname))+' '+LTRIM(RTRIM(supervisor.lastname)) supervisor
+		  ,LTRIM(RTRIM(fsw.firstname))+' '+LTRIM(RTRIM(fsw.lastname)) worker
+	      ,PC1ID
 		  ,levelname
 		  ,StartLevelDate
 		  ,isnull(EndLevelDate,current_timestamp) as EndLevelDate
@@ -44,13 +47,19 @@ begin
 		  ,DischargeDate
 		from HVLevelDetail hld
 			inner join CaseProgram cp on cp.HVCaseFK = hld.hvcasefk
-			inner join WorkerProgram wp on wp.WorkerFK = cp.CurrentFSWFK
+			--inner join WorkerProgram wp on wp.WorkerFK = cp.CurrentFSWFK
 			inner join dbo.SplitString(@programfk,',') on hld.programfk = listitem
+			
+			--inner join CaseProgram d on d.HVCaseFK = a.HVCaseFK
+			inner join worker fsw on cp.CurrentFSWFK = fsw.workerpk
+			inner join workerprogram wp on wp.workerfk = fsw.workerpk
+			inner join worker supervisor on wp.supervisorfk = supervisor.workerpk
+			
 		where PC1ID = isnull(@PC1ID,pc1id)
 			  and CurrentFSWFK = isnull(@WorkerFK, CurrentFSWFK)
 			  and SupervisorFK = isnull(@SupervisorFK, SupervisorFK)
 		      AND (CASE WHEN (@pc1id IS NULL AND DischargeDate IS NOT NULL) THEN 0 ELSE 1 END = 1)
-		order by PC1ID, StartLevelDate desc
+		order by supervisor, worker, PC1ID, StartLevelDate desc
 
 end
 GO
