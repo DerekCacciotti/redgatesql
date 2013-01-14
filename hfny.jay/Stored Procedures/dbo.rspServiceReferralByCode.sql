2bf3ca0a4cc2e4f67db86e3c5ab9cc69c348cf2b
Chun Dar Chen <cchen2@albany.edu>
2013-01-14 20:02:57 +0000

    Count of Service Referrals by Code

--- hfny.jay/Stored Procedures/dbo.rspServiceReferralByCode.sql
+++ hfny.jay/Stored Procedures/dbo.rspServiceReferralByCode.sql
@@ -19,24 +19,28 @@ CREATE PROCEDURE [dbo].[rspServiceReferralByCode]
     @doPC1ID AS INT = 0
 AS
 
---DECLARE @programfk INT = 6 
+--DECLARE @programfk INT = 1
 --DECLARE @workerfk INT = NULL
 --DECLARE @StartDt DATETIME = '01/01/2012'
---DECLARE @EndDt DATETIME = '03/31/2012'
+--DECLARE @EndDt DATETIME = '12/31/2012'
 --DECLARE @pc1id AS VARCHAR(13)
 --DECLARE @doWorker AS INT = 0
 --DECLARE @doPC1ID AS INT = 0
 
-; WITH HVCaseInRange AS (
-SELECT b.PC1ID, b.HVCaseFK
-, CASE WHEN a.IntakeDate < @StartDt THEN @StartDt ELSE a.IntakeDate END [Start_Period]
-, CASE WHEN b.DischargeDate IS NULL THEN @EndDt
-  WHEN b.DischargeDate > @EndDt THEN @EndDt ELSE b.DischargeDate END [End_Period]
-FROM HVCase AS a JOIN CaseProgram AS b ON a.HVCasePK = b.HVCaseFK
-WHERE a.IntakeDate <= @EndDt AND a.IntakeDate IS NOT NULL AND
-(b.DischargeDate IS NULL OR b.DischargeDate > @StartDt) 
-AND b.PC1ID = ISNULL(@pc1id, b.PC1ID)
-)
+--; WITH HVCaseInRange AS (
+--SELECT b.PC1ID, b.HVCaseFK
+----, CASE WHEN a.IntakeDate < @StartDt THEN @StartDt ELSE a.IntakeDate END [Start_Period]
+----, CASE WHEN b.DischargeDate IS NULL THEN @EndDt
+----  WHEN b.DischargeDate > @EndDt THEN @EndDt ELSE b.DischargeDate END [End_Period]
+
+--, @StartDt [Start_Period]
+--, @EndDt  [End_Period]
+
+--FROM HVCase AS a JOIN CaseProgram AS b ON a.HVCasePK = b.HVCaseFK
+--WHERE a.IntakeDate <= @EndDt AND a.IntakeDate IS NOT NULL AND
+--(b.DischargeDate IS NULL OR b.DischargeDate > @StartDt) 
+--AND b.PC1ID = ISNULL(@pc1id, b.PC1ID)
+--)
 
 SELECT 
 CASE WHEN @doWorker = 1 THEN 
@@ -59,17 +63,20 @@ ltrim(rtrim(b.servicereferralcategory)) [CategoryCode],
 x.n
 FROM
 (SELECT 
-CASE WHEN @doWorker = 1 THEN a.FSWFK ELSE '' END [FSWFK], 
+CASE WHEN @doWorker = 1 THEN isnull(b.CurrentFSWFK, b.CurrentFAWFK) ELSE '' END [FSWFK], 
 CASE WHEN @doPC1ID = 1 THEN b.PC1ID ELSE '' END [PC1ID], 
 a.ServiceCode, count(*) [n]
 FROM ServiceReferral a
-JOIN HVCaseInRange AS b ON b.HVCaseFK = a.HVCaseFK
+JOIN CaseProgram AS b ON a.HVCaseFK = b.HVCaseFK
+--JOIN HVCaseInRange AS b ON b.HVCaseFK = a.HVCaseFK
 WHERE a.ProgramFK = @programfk 
-AND a.ReferralDate Between b.Start_Period AND b.End_Period
+AND a.ReferralDate Between @StartDt AND @EndDt
 --AND a.ServiceReceived = 1
-AND a.FSWFK = ISNULL(@workerfk, a.FSWFK)
+--AND a.FSWFK = ISNULL(@workerfk, a.FSWFK)
+AND isnull(b.CurrentFSWFK, b.CurrentFAWFK) = ISNULL(@workerfk, isnull(b.CurrentFSWFK, b.CurrentFAWFK))
+
 GROUP BY 
-CASE WHEN @doWorker = 1 THEN a.FSWFK ELSE '' END, 
+CASE WHEN @doWorker = 1 THEN isnull(b.CurrentFSWFK, b.CurrentFAWFK) ELSE '' END, 
 CASE WHEN @doPC1ID = 1 THEN b.PC1ID ELSE '' END, 
 a.ServiceCode) x
 LEFT OUTER JOIN worker fsw
