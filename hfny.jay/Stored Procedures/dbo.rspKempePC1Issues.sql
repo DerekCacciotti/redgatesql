95c58ad8afb4144d226b0a92d35ce23fb8d4a4a9
Jay Robohn <jrobohn@albany.edu>
2013-01-18 19:07:17 +0000

    Corrected report to do outer join on referrals

--- hfny.jay/Stored Procedures/dbo.rspKempePC1Issues.sql
+++ hfny.jay/Stored Procedures/dbo.rspKempePC1Issues.sql
@@ -8,7 +8,7 @@ GO
 -- Author:      <Dar Chen>
 -- Create date: <Jul 11, 2012>
 -- Description: 
--- exec rspKempePC1Issues 1, '20120701', '20120930', '', null
+-- exec rspKempePC1Issues 19, '20120701', '20120930', '', null
 -- =============================================
 CREATE procedure [dbo].[rspKempePC1Issues]
 (
@@ -33,33 +33,13 @@ begin
 
 	set @ProgramFK = REPLACE(@ProgramFK,'"','')
 	set @SiteFK = case when dbo.IsNullOrEmpty(@SiteFK) = 1 then 0 else @SiteFK end
-	set @casefilterspositive = case when @casefilterspositive = '' then null else @casefilterspositive end
+	set @casefilterspositive = case when @casefilterspositive = '' then null else @casefilterspositive end;
 
-	--DECLARE @StartDt DATE = '01/01/2011'
-	--DECLARE @EndDt DATE = '12/31/2011'
-	--DECLARE @programfk INT = 17
-
-	;
-	with inserviceReferral
+	with cteKempeIssues
 	as
 	(
-	select c.HVCasePK
-		  ,sum(case when sr.servicecode in ('49','50') and sr.FamilyCode = '01' then 1 else 0 end) MentalHealthServices
-		  ,sum(case when sr.servicecode = '51' and sr.FamilyCode = '01' then 1 else 0 end) DomesticViolenceServices
-		  ,sum(case when sr.servicecode = '52' and sr.FamilyCode = '01' then 1 else 0 end) SubstanceAbuseServices
-		from HVCase c
-			join ServiceReferral sr on sr.HVCaseFK = c.HVCasePK
-			inner join WorkerProgram wp on WorkerFK = FSWFK
-			inner join dbo.udfCaseFilters(@casefilterspositive,'', @programfk) cf on cf.HVCaseFK = c.HVCasePK
-		where c.IntakeDate between @StartDt and @EndDt
-			 and sr.ReferralDate-c.IntakeDate < 183
-			 and (case when @SiteFK = 0 then 1 when wp.SiteFK = @SiteFK then 1 else 0 end = 1)
-		group by c.HVCasePK
-	),
-
-	inPC1Issues
-	as (
 	select pc1i.HVCaseFK
+			, pc1id
 		  ,case when sum(case when SubstanceAbuse = 1 then 1 else 0 end) > 0 then 1 else 0 end SubstanceAbuse
 		  ,case when sum(case when MentalIllness = 1 then 1 else 0 end) > 0 then 1 else 0 end MentalIllness
 		  ,case when sum(case when DomesticViolence = 1 then 1 else 0 end) > 0 then 1 else 0 end DomesticViolence
@@ -67,13 +47,32 @@ begin
 		  ,case when sum(case when Depression = 1 then 1 else 0 end) > 0 then 1 else 0 end Depression
 		  ,case when sum(case when OtherIssue = 1 then 1 else 0 end) > 0 then 1 else 0 end OtherIssue
 		from PC1Issues pc1i
+		inner join caseprogram cp on cp.HVCaseFK = pc1i.HVCaseFK
 		inner join Kempe k on k.PC1IssuesFK = pc1i.PC1IssuesPK
 		inner join WorkerProgram wp on WorkerFK = FAWFK
 		inner join dbo.udfCaseFilters(@casefilterspositive,'', @programfk) cf on cf.HVCaseFK = pc1i.HVCaseFK
 		where PC1IssuesDate <= @EndDt
 				and Interval='1'
 				and (case when @SiteFK = 0 then 1 when wp.SiteFK = @SiteFK then 1 else 0 end = 1)
-		group by pc1i.HVCaseFK
+		group by pc1i.HVCaseFK, PC1ID
+	),
+	cteReferrals 
+	as
+	(
+	select c.HVCasePK
+		  ,sum(case when sr.servicecode in ('49','50') and sr.FamilyCode = '01' then 1 else 0 end) MentalHealthServices
+		  ,sum(case when sr.servicecode = '51' and sr.FamilyCode = '01' then 1 else 0 end) DomesticViolenceServices
+		  ,sum(case when sr.servicecode = '52' and sr.FamilyCode = '01' then 1 else 0 end) SubstanceAbuseServices
+			, pc1id
+		from HVCase c
+			join ServiceReferral sr on sr.HVCaseFK = c.HVCasePK
+			inner join caseprogram cp on cp.HVCaseFK = c.HVCasePK
+			inner join WorkerProgram wp on WorkerFK = FSWFK
+			inner join dbo.udfCaseFilters(@casefilterspositive,'', @programfk) cf on cf.HVCaseFK = c.HVCasePK
+		where c.IntakeDate between @StartDt and @EndDt
+			 and sr.ReferralDate-c.IntakeDate < 183
+			 and (case when @SiteFK = 0 then 1 when wp.SiteFK = @SiteFK then 1 else 0 end = 1)
+		group by c.HVCasePK, PC1ID
 	)
 
 	select distinct cp.PC1ID
@@ -92,10 +91,10 @@ begin
 				   ,ltrim(rtrim(fsw.firstname))+' '+ltrim(rtrim(fsw.lastname)) fswname
 				   ,ltrim(rtrim(sup.firstname))+' '+ltrim(rtrim(sup.lastname)) supervisor
 		from HVCase c
-			join inPC1Issues as pc1i on c.HVCasePK = pc1i.HVCaseFK
+			join cteKempeIssues as pc1i on c.HVCasePK = pc1i.HVCaseFK
 			join CaseProgram cp on cp.HVCaseFK = c.HVCasePK
 			join codeLevel l on cp.CurrentLevelFK = l.codeLevelPK
-			join inserviceReferral sr on sr.HVCasePK = c.HVCasePK
+			left outer join cteReferrals sr on sr.HVCasePK = c.HVCasePK
 			inner join worker fsw on fsw.workerpk = cp.currentfswfk
 			inner join workerprogram wp on wp.workerfk = fsw.workerpk
 			inner join worker sup on supervisorfk = sup.workerpk
@@ -112,6 +111,84 @@ begin
 			 and (case when @SiteFK = 0 then 1 when wp.SiteFK = @SiteFK then 1 else 0 end = 1)
 		order by supervisor
 				,cp.PC1ID
+
+	--DECLARE @StartDt DATE = '01/01/2011'
+	--DECLARE @EndDt DATE = '12/31/2011'
+	--DECLARE @programfk INT = 17
+
+	--;
+	--with inserviceReferral
+	--as
+	--(
+	--select c.HVCasePK
+	--	  ,sum(case when sr.servicecode in ('49','50') and sr.FamilyCode = '01' then 1 else 0 end) MentalHealthServices
+	--	  ,sum(case when sr.servicecode = '51' and sr.FamilyCode = '01' then 1 else 0 end) DomesticViolenceServices
+	--	  ,sum(case when sr.servicecode = '52' and sr.FamilyCode = '01' then 1 else 0 end) SubstanceAbuseServices
+	--	from HVCase c
+	--		join ServiceReferral sr on sr.HVCaseFK = c.HVCasePK
+	--		inner join WorkerProgram wp on WorkerFK = FSWFK
+	--		inner join dbo.udfCaseFilters(@casefilterspositive,'', @programfk) cf on cf.HVCaseFK = c.HVCasePK
+	--	where c.IntakeDate between @StartDt and @EndDt
+	--		 and sr.ReferralDate-c.IntakeDate < 183
+	--		 and (case when @SiteFK = 0 then 1 when wp.SiteFK = @SiteFK then 1 else 0 end = 1)
+	--	group by c.HVCasePK
+	--),
+
+	--inPC1Issues
+	--as (
+	--select pc1i.HVCaseFK
+	--	  ,case when sum(case when SubstanceAbuse = 1 then 1 else 0 end) > 0 then 1 else 0 end SubstanceAbuse
+	--	  ,case when sum(case when MentalIllness = 1 then 1 else 0 end) > 0 then 1 else 0 end MentalIllness
+	--	  ,case when sum(case when DomesticViolence = 1 then 1 else 0 end) > 0 then 1 else 0 end DomesticViolence
+	--	  ,case when sum(case when AlcoholAbuse = 1 then 1 else 0 end) > 0 then 1 else 0 end AlcoholAbuse
+	--	  ,case when sum(case when Depression = 1 then 1 else 0 end) > 0 then 1 else 0 end Depression
+	--	  ,case when sum(case when OtherIssue = 1 then 1 else 0 end) > 0 then 1 else 0 end OtherIssue
+	--	from PC1Issues pc1i
+	--	inner join Kempe k on k.PC1IssuesFK = pc1i.PC1IssuesPK
+	--	inner join WorkerProgram wp on WorkerFK = FAWFK
+	--	inner join dbo.udfCaseFilters(@casefilterspositive,'', @programfk) cf on cf.HVCaseFK = pc1i.HVCaseFK
+	--	where PC1IssuesDate <= @EndDt
+	--			and Interval='1'
+	--			and (case when @SiteFK = 0 then 1 when wp.SiteFK = @SiteFK then 1 else 0 end = 1)
+	--	group by pc1i.HVCaseFK
+	--)
+
+	--select distinct cp.PC1ID
+	--			   ,convert(varchar(12),c.KempeDate,101) KempDate
+	--			   ,convert(varchar(12),c.IntakeDate,101) IntakeDate
+	--			   ,l.LevelName
+	--			   ,case when (pc1i.SubstanceAbuse = 1 or pc1i.AlcoholAbuse = 1) then 'Yes' else '' end+
+	--				case when sr.SubstanceAbuseServices > 0 and (pc1i.SubstanceAbuse = 1 or pc1i.AlcoholAbuse = 1) then ' *' else '' end 
+	--					SubstanceAbuseServices
+	--			   ,case when (pc1i.MentalIllness = 1 or pc1i.Depression = 1) then 'Yes' else '' end+
+	--				case when sr.MentalHealthServices > 0 and (pc1i.MentalIllness = 1 or pc1i.Depression = 1) then ' *' else '' end 
+	--					MentalHealthServices
+	--			   ,case when pc1i.DomesticViolence = 1 then 'Yes' else '' end+
+	--				case when sr.DomesticViolenceServices > 0 and pc1i.DomesticViolence = 1 then ' *' else '' end 
+	--					DomesticViolenceServices
+	--			   ,ltrim(rtrim(fsw.firstname))+' '+ltrim(rtrim(fsw.lastname)) fswname
+	--			   ,ltrim(rtrim(sup.firstname))+' '+ltrim(rtrim(sup.lastname)) supervisor
+	--	from HVCase c
+	--		join inPC1Issues as pc1i on c.HVCasePK = pc1i.HVCaseFK
+	--		join CaseProgram cp on cp.HVCaseFK = c.HVCasePK
+	--		join codeLevel l on cp.CurrentLevelFK = l.codeLevelPK
+	--		join inserviceReferral sr on sr.HVCasePK = c.HVCasePK
+	--		inner join worker fsw on fsw.workerpk = cp.currentfswfk
+	--		inner join workerprogram wp on wp.workerfk = fsw.workerpk
+	--		inner join worker sup on supervisorfk = sup.workerpk
+	--		inner join dbo.udfCaseFilters(@casefilterspositive,'', @programfk) cf on cf.HVCaseFK = c.HVCasePK
+	--	where c.IntakeDate between @StartDt and @EndDt
+	--		 and cp.ProgramFK = @programfk
+	--		 and (cp.DischargeDate is null
+	--		 or cp.DischargeDate <= @EndDt)
+	--		 and (pc1i.SubstanceAbuse = 1
+	--		 or pc1i.AlcoholAbuse = 1
+	--		 or pc1i.MentalIllness = 1
+	--		 or pc1i.Depression = 1
+	--		 or pc1i.DomesticViolence = 1)
+	--		 and (case when @SiteFK = 0 then 1 when wp.SiteFK = @SiteFK then 1 else 0 end = 1)
+	--	order by supervisor
+	--			,cp.PC1ID
 
 end
 GO
