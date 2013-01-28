71c461adefdff34c71b32ca9bf3260aad567de29
Chun Dar Chen <cchen2@albany.edu>
2013-01-28 18:53:30 +0000

    Report - Count of Service Referrals by Code for Chemung County Fixed - number matched

--- hfny.jay/Stored Procedures/dbo.rspServiceReferralByCode.sql
+++ hfny.jay/Stored Procedures/dbo.rspServiceReferralByCode.sql
@@ -19,28 +19,28 @@ CREATE PROCEDURE [dbo].[rspServiceReferralByCode]
     @doPC1ID AS INT = 0
 AS
 
---DECLARE @programfk INT = 1
+--DECLARE @programfk INT = 5
 --DECLARE @workerfk INT = NULL
---DECLARE @StartDt DATETIME = '01/01/2012'
---DECLARE @EndDt DATETIME = '12/31/2012'
+--DECLARE @StartDt DATETIME = '04/01/2012'
+--DECLARE @EndDt DATETIME = '06/30/2012'
 --DECLARE @pc1id AS VARCHAR(13)
 --DECLARE @doWorker AS INT = 0
 --DECLARE @doPC1ID AS INT = 0
 
---; WITH HVCaseInRange AS (
---SELECT b.PC1ID, b.HVCaseFK
-----, CASE WHEN a.IntakeDate < @StartDt THEN @StartDt ELSE a.IntakeDate END [Start_Period]
-----, CASE WHEN b.DischargeDate IS NULL THEN @EndDt
-----  WHEN b.DischargeDate > @EndDt THEN @EndDt ELSE b.DischargeDate END [End_Period]
+; WITH HVCaseInRange AS (
+SELECT b.PC1ID, b.HVCaseFK
+--, CASE WHEN a.IntakeDate < @StartDt THEN @StartDt ELSE a.IntakeDate END [Start_Period]
+--, CASE WHEN b.DischargeDate IS NULL THEN @EndDt
+--  WHEN b.DischargeDate > @EndDt THEN @EndDt ELSE b.DischargeDate END [End_Period]
 
---, @StartDt [Start_Period]
---, @EndDt  [End_Period]
+, @StartDt [Start_Period]
+, @EndDt  [End_Period]
 
---FROM HVCase AS a JOIN CaseProgram AS b ON a.HVCasePK = b.HVCaseFK
---WHERE a.IntakeDate <= @EndDt AND a.IntakeDate IS NOT NULL AND
---(b.DischargeDate IS NULL OR b.DischargeDate > @StartDt) 
---AND b.PC1ID = ISNULL(@pc1id, b.PC1ID)
---)
+FROM HVCase AS a JOIN CaseProgram AS b ON a.HVCasePK = b.HVCaseFK
+WHERE a.IntakeDate <= @EndDt AND a.IntakeDate IS NOT NULL AND
+(b.DischargeDate IS NULL OR b.DischargeDate > @StartDt) 
+AND b.PC1ID = ISNULL(@pc1id, b.PC1ID)
+)
 
 SELECT 
 CASE WHEN @doWorker = 1 THEN 
@@ -68,7 +68,7 @@ CASE WHEN @doPC1ID = 1 THEN b.PC1ID ELSE '' END [PC1ID],
 a.ServiceCode, count(*) [n]
 FROM ServiceReferral a
 JOIN CaseProgram AS b ON a.HVCaseFK = b.HVCaseFK
---JOIN HVCaseInRange AS b ON b.HVCaseFK = a.HVCaseFK
+JOIN HVCaseInRange AS b1 ON b1.HVCaseFK = a.HVCaseFK
 WHERE a.ProgramFK = @programfk 
 AND a.ReferralDate Between @StartDt AND @EndDt
 --AND a.ServiceReceived = 1
