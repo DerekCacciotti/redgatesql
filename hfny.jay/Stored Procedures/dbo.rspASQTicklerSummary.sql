1c646b244d747c0b6ee4de71600744268d54f83b
Devinder Singh Khalsa <dkhalsa@albany.edu>
2013-02-06 19:19:00 +0000

    HW691 ASQ tickler LR89550163551

--- hfny.jay/Stored Procedures/dbo.rspASQTicklerSummary.sql
+++ hfny.jay/Stored Procedures/dbo.rspASQTicklerSummary.sql
@@ -1,4 +1,3 @@
-
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -8,10 +7,12 @@ GO
 
 
 -- =============================================
--- Author:		<Jay Robohn>
--- Create date: <Feb 5, 2012>
+-- Author:		Devinder Singh Khalsa
+-- Create date: 02/06/2013
 -- Description:	<report: ASQ Tickler Summary>
 --				Moved from FamSys - 02/05/12 jrobohn
+-- Modified: Exclude ASQs if case is receiving services for EI
+-- rspASQTicklerSummary 1, '12/12/2012'
 -- =============================================
 CREATE procedure [dbo].[rspASQTicklerSummary]
 (
@@ -31,7 +32,27 @@ as
 	set @programfk = replace(@programfk,'"','')
 
 	-- ASQ
-	select pc1id
+	
+	
+DECLARE @tblASQTicklerSummaryCohort TABLE(
+	[PC1ID] [char](13) NOT NULL,
+	[HVCasePK] [int] NOT NULL,
+	[TCDOB] [datetime] NULL,
+	[GestationalAge] INT,
+	[cdob] [datetime] NULL,
+	[EventDescription] varchar(50) NULL,
+	[DueDate] [datetime] NULL,
+	[TargetChild] VARCHAR(200),
+	[fswname] VARCHAR(200),
+	[supervisor] VARCHAR(200)			
+)		
+	
+
+	
+	INSERT INTO @tblASQTicklerSummaryCohort	
+	select  
+		  pc1id
+		  ,hvcase.HVCasePK
 		  ,hvcase.tcdob
 		  ,gestationalage
 		  ,(((40-gestationalage)*7)+hvcase.tcdob) cdob
@@ -44,7 +65,7 @@ as
 		   end DueDate
 		  ,rtrim(tcfirstname)+' '+rtrim(tclastname) TargetChild
 		  ,rtrim(fsw.firstname)+' '+rtrim(fsw.lastname) fswname
-		  ,ltrim(rtrim(supervisor.firstname))+' '+ltrim(rtrim(supervisor.lastname)) as supervisor
+		  ,ltrim(rtrim(supervisor.firstname))+' '+ltrim(rtrim(supervisor.lastname)) as supervisor		  
 		from caseprogram
 			inner join hvcase on hvcasepk = caseprogram.hvcasefk
 			inner join tcid on tcid.hvcasefk = hvcasepk and tcid.programfk = caseprogram.programfk
@@ -73,6 +94,69 @@ as
 						   else
 							   dateadd(dd,dueby,hvcase.tcdob)
 					   end) = month(@rdate)
-		order by fswname
-				,DueDate
+		order by hvcase.HVCasePK
+
+;
+
+
+WITH cteLastHighestASQ AS
+(
+SELECT 
+		asqt.HVCasePK			 
+	  , max(TCAge) AS Interval
+ 
+  FROM @tblASQTicklerSummaryCohort asqt  
+  LEFT JOIN ASQ A ON asqt.HVCasePK = a.HVCaseFK 
+ GROUP BY HVCasePK	
+ --ORDER BY HVCasePK 
+)
+
+,
+cteExcludeOptionalsIfCasesAreASQTCReceivingIsYes as
+(
+	-- get hvcasepk's that we will exclude because for them ASQTCReceiving = 1 and EventDescription contains optional
+SELECT DISTINCT 		
+	  cc.HVCasePK		 
+	  FROM @tblASQTicklerSummaryCohort cc
+INNER  JOIN cteLastHighestASQ cl ON cl.HVCasePK = cc.HVCasePK
+LEFT   JOIN ASQ asqq ON cl.HVCasePK = asqq.HVCaseFK -- OR asqq.HVCaseFK IS null
+WHERE  
+	(asqq.TCAge  = cl.Interval OR cl.Interval IS NULL)
+	AND
+	(asqq.ASQTCReceiving = 1 AND  EventDescription LIKE '%optional%')
+	
+)
+
+--SELECT * from cteCohort
+--SELECT * from cteExcludeOptionalsIfCasesAreASQTCReceivingIsYes
+
+SELECT DISTINCT 
+		PC1ID
+	 , cc.HVCasePK
+	 , TCDOB
+	 , GestationalAge
+	 , cdob
+	 , CASE WHEN asqq.ASQTCReceiving = 1 THEN 'Please contact EI program for update'
+		 ELSE 
+		EventDescription
+		END AS EventDescription
+	 , DueDate
+	 , TargetChild
+	 , fswname
+	 , supervisor	 
+	 --, cl.Interval
+	 --, asqq.ASQTCReceiving 
+	 
+	  FROM @tblASQTicklerSummaryCohort cc
+INNER  JOIN cteLastHighestASQ cl ON cl.HVCasePK = cc.HVCasePK
+LEFT   JOIN ASQ asqq ON cl.HVCasePK = asqq.HVCaseFK -- OR asqq.HVCaseFK IS null
+
+WHERE  
+	(asqq.TCAge  = cl.Interval OR cl.Interval IS NULL)
+	AND
+	cc.HVCasePK NOT IN (SELECT HVCasePK FROM cteExcludeOptionalsIfCasesAreASQTCReceivingIsYes)
+
+order by fswname ,DueDate
+
+
 GO
