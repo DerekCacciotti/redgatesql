2cbfc3418168171310205deef6b8333e8b5833b2
Jay Robohn <jrobohn@albany.edu>
2013-02-21 17:33:03 +0000

    Changes to migration. Not finished yet...

--- hfny.jay/Stored Procedures/dbo.pr_MigratePC2ToOBP.sql
+++ hfny.jay/Stored Procedures/dbo.pr_MigratePC2ToOBP.sql
@@ -1,9 +1,10 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
 
-create procedure [dbo].[pr_MigratePC2ToOBP]  (@TargetCaseFK int)
+CREATE procedure [dbo].[pr_MigratePC2ToOBP]  (@TargetCaseFK int)
 ---- Migrate a single cases' PC2 to OBP 
 ---- see bottom of function for original code and related queries
 as 
@@ -13,18 +14,21 @@ if exists(select * from tempdb.dbo.sysobjects where id=object_id(N'tempdb..#Case
 	-- where charindex('#',name)>0 order by name
 	drop table #CaseListToBeMigrated
 
+print 'Select HVCase row'
 select HVCasePK,PC2FK 
 into #CaseListToBeMigrated 
 from HVCase h
 inner join CaseProgram cp on cp.HVCaseFK = h.HVCasePK
 where HVCasePK=@TargetCaseFK
 
+print 'Update CommonAttributes - switch FU-PC2s to FU-OBP'
 update CommonAttributes
 set FormType='FU-OBP'
 from CommonAttributes ca
 inner join #CaseListToBeMigrated cltbm on ca.HVCaseFK=cltbm.HVCasePK
 where FormType='FU-PC2'
 
+print 'Update CommonAttributes - switch IN-PC2s to IN-OBP'
 update CommonAttributes
 set FormType='IN-OBP', 
 	OBPInHome='1'	
@@ -32,6 +36,7 @@ from CommonAttributes ca
 inner join #CaseListToBeMigrated cltbm on ca.HVCaseFK=cltbm.HVCasePK
 where FormType='IN-PC2'
 
+print 'Update FatherFigure - switch IsOBP and IsPC2 flags'
 update FatherFigure
 set IsOBP=IsPC2, 
 	IsPC2=0
@@ -39,29 +44,34 @@ from FatherFigure ff
 inner join #CaseListToBeMigrated cltbm on ff.HVCaseFK=cltbm.HVCasePK
 where ff.RelationToTargetChild='01'
 
+print 'Update CommonAttributes - switch PC2InHome to OBPInHome'
 update CommonAttributes
 set OBPInHome=PC2InHome 
 from CommonAttributes ca
 inner join #CaseListToBeMigrated cltbm on ca.HVCaseFK=cltbm.HVCasePK and ca.FormType='FU-OBP'
 inner join FollowUp fu on ca.FormFK=FollowUpPK
 
+print 'Update FollowUp - set PC2InHome to No'
 update FollowUp
 set PC2InHome='0'
 from FollowUp fu
 inner join #CaseListToBeMigrated cltbm on fu.HVCaseFK=cltbm.HVCasePK
 
+print 'Update HVLog - switch PC2Participated to OBPParticipated and set PC2Participated to no'
 update HVLog
 set OBPParticipated=PC2Participated,
 	PC2Participated=0
 from HVLog h
 inner join #CaseListToBeMigrated cltbm on h.HVCaseFK=cltbm.HVCasePK
 
+print 'Update PC - reverse OBP and PC2 flags'
 update PC
 set OBP=1,
 	PC2=0
 from PC p
 inner join #CaseListToBeMigrated cltbm on p.PCPK=cltbm.PC2FK
 
+print 'Update HVCase - switch all flags and values from PC2 to OBP and clear PC2 flags and values'
 update HVCase
 set OBPFK=h.PC2FK,
 	PC2FK=null,
@@ -74,17 +84,13 @@ set OBPFK=h.PC2FK,
 from HVCase h
 inner join #CaseListToBeMigrated cltbm on h.HVCasePK=cltbm.HVCasePK
 
-update HVCase
-set OBPInformationAvailable=0
-from HVCase h
-where h.HVCasePK not in (select HVCasePK from #CaseListToBeMigrated cltbm)
-		and h.CaseProgress >= 9
-		
+print 'Update Education - switch PC2 rows to OBP'
 update Education
 set PCType='OBP'
 from Education e
 where PCType='PC2' and e.HVCaseFK in (select HVCasePK from #CaseListToBeMigrated cltbm)
 
+print 'Update Employment - switch PC2 rows to OBP'
 update Employment
 set PCType='OBP'
 from Employment e
