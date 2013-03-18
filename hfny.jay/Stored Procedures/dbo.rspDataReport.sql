c75977943747b7bc969c4b1cfe6e594a13208bfa
Devinder Singh Khalsa <dkhalsa@albany.edu>
2013-03-18 12:06:46 +0000

    Data Report

---
+++ hfny.jay/Stored Procedures/dbo.rspDataReport.sql
@@ -0,0 +1,486 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+-- =============================================
+-- Author:		<Devinder Singh Khalsa>
+-- Create date: <Febu. 11, 2013>
+-- Description:	<This report gets you 'A. Data report '>
+-- rspDataReport 5, '06/01/2012', '09/30/2012'			
+
+
+-- =============================================
+
+CREATE procedure [dbo].[rspDataReport]
+(
+    @ProgramFKs				varchar(max)    = null,
+    @StartDate				datetime,
+    @EndDate				datetime,
+    @FSWFK					int             = null,
+    @SiteFK					int             = null,
+    @IncludeClosedCases		bit             = 0,
+    @CaseFiltersPositive	varchar(100)    = ''
+)
+as
+begin
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	set nocount on;
+
+	set @SiteFK = case when dbo.IsNullOrEmpty(@SiteFK) = 1 then 0 else @SiteFK end
+	set @CaseFiltersPositive = case when @CaseFiltersPositive = '' then null else @CaseFiltersPositive end
+
+	-- main report
+	declare @tbl4DataReport table(	
+		ReportTitle [varchar](500),
+		Total [varchar](10)
+	)
+
+
+-- SCREEN------1 @ BEGINNING OF MONTH
+
+	declare @tbl4DataReportRow1 table(
+	HVCasePK INT
+	)
+
+	INSERT INTO @tbl4DataReportRow1
+	(
+		HVCasePK
+
+	)
+	SELECT h.HVCasePK FROM HVCase h
+	INNER JOIN CaseProgram cp ON cp.HVCaseFK = h.HVCasePK
+	INNER JOIN HVScreen h1 ON h1.HVCaseFK = cp.HVCaseFK AND h1.ProgramFK = cp.ProgramFK
+	inner join dbo.udfCaseFilters(@CaseFiltersPositive,'',@ProgramFKs) cf on cf.HVCaseFK = h.HVCasePK	
+	
+	WHERE h1.ScreenDate < @StartDate
+	AND (h.KempeDate >= @StartDate OR h.KempeDate IS NULL)
+	AND (cp.DischargeDate >= @StartDate OR cp.DischargeDate IS NULL)
+
+	DECLARE @nposScreens INT 
+	SET @nposScreens = (SELECT count(HVCasePK) FROM @tbl4DataReportRow1)
+
+
+	-- Start of SCREEN-----2 @ NEW DURING MONTH  ----------
+	declare @tbl4DataReportRow2 table(
+	HVCasePK INT,
+	ReferralMade [varchar](1),
+	ScreenResult [varchar](1)
+	)
+
+	INSERT INTO @tbl4DataReportRow2
+	(
+		HVCasePK,
+		ReferralMade,
+		ScreenResult
+	)
+	SELECT h.HVCasePK,H1.ReferralMade,h1.ScreenResult FROM HVCase h
+	INNER JOIN CaseProgram cp ON cp.HVCaseFK = h.HVCasePK
+	INNER JOIN HVScreen h1 ON h1.HVCaseFK = h.HVCasePK AND h1.ProgramFK = cp.ProgramFK
+	inner join dbo.udfCaseFilters(@CaseFiltersPositive,'',@ProgramFKs) cf on cf.HVCaseFK = h.HVCasePK
+	WHERE h1.ScreenDate >= @StartDate AND h1.ScreenDate <= @EndDate
+	AND cp.ProgramFK = @ProgramFKs
+	
+	
+	DECLARE @n2a INT 
+	SET @n2a = (SELECT count(HVCasePK) FROM @tbl4DataReportRow2 WHERE ReferralMade = '1')
+	
+
+	-- Start-----3 @ Kempes this month  ----------
+	declare @tbl4DataReportRow3 table(
+	HVCasePK INT,
+	KempeResult BIT,
+	CaseStatus [varchar](2)
+	)
+
+	INSERT INTO @tbl4DataReportRow3
+	(
+		HVCasePK,
+		KempeResult,
+		CaseStatus
+	)
+	SELECT h.HVCasePK,p.KempeResult, p.CaseStatus FROM HVCase h
+	INNER JOIN CaseProgram cp ON cp.HVCaseFK = h.HVCasePK
+	INNER JOIN Preassessment p ON p.HVCaseFK = h.HVCasePK AND p.ProgramFK = cp.ProgramFK	
+	inner join dbo.udfCaseFilters(@CaseFiltersPositive,'',@ProgramFKs) cf on cf.HVCaseFK = h.HVCasePK
+	WHERE p.KempeDate >= @StartDate AND p.KempeDate <= @EndDate
+	AND cp.ProgramFK = @ProgramFKs
+	AND p.CaseStatus > '01'
+	
+	DECLARE @n3 INT 
+	SET @n3 = (SELECT count(HVCasePK) FROM @tbl4DataReportRow3)
+	
+	DECLARE @n3a INT 
+	SET @n3a = (SELECT count(HVCasePK) FROM @tbl4DataReportRow3 WHERE CaseStatus = '02' AND KempeResult = 1)
+
+
+	-- Start -----4 @ Screens Terminated this month  ----------
+	
+	declare @tbl4DataReportRow4 table(
+	HVCasePK INT
+	)
+
+	INSERT INTO @tbl4DataReportRow4
+	(
+		HVCasePK
+	)
+	SELECT h.HVCasePK FROM HVCase h
+	INNER JOIN CaseProgram cp ON cp.HVCaseFK = h.HVCasePK
+	INNER JOIN Preassessment p ON p.HVCaseFK = h.HVCasePK AND p.ProgramFK = cp.ProgramFK	
+	inner join dbo.udfCaseFilters(@CaseFiltersPositive,'',@ProgramFKs) cf on cf.HVCaseFK = h.HVCasePK
+	WHERE p.PADate >= @StartDate AND p.PADate <= @EndDate
+	AND cp.ProgramFK = @ProgramFKs
+	AND p.CaseStatus = '03'	
+
+	DECLARE @n4 INT 
+	SET @n4 = (SELECT count(HVCasePK) FROM @tbl4DataReportRow4)
+
+
+	-- Start -----PRE-INTAKE-------6 @ BEGINNING OF MONTH  ----------
+	declare @tbl4DataReportRow6 table(
+	HVCasePK INT
+	)
+
+	INSERT INTO @tbl4DataReportRow6
+	(
+		HVCasePK
+	)
+	SELECT h.HVCasePK FROM HVCase h
+	INNER JOIN CaseProgram cp ON cp.HVCaseFK = h.HVCasePK
+	INNER JOIN Preassessment p ON p.HVCaseFK = h.HVCasePK AND p.ProgramFK = cp.ProgramFK	
+	inner join dbo.udfCaseFilters(@CaseFiltersPositive,'',@ProgramFKs) cf on cf.HVCaseFK = h.HVCasePK
+	WHERE p.PADate < @StartDate AND p.PADate < @EndDate
+	AND cp.ProgramFK = @ProgramFKs
+	AND p.CaseStatus = '02'	
+	AND KempeResult = 1
+	AND (h.IntakeDate  >= @StartDate OR h.IntakeDate IS NULL)
+	AND (cp.DischargeDate >= @StartDate OR cp.DischargeDate IS NULL)
+
+	DECLARE @n6 INT 
+	SET @n6 = (SELECT count(HVCasePK) FROM @tbl4DataReportRow6)	
+	
+	
+	---- Start -----PRE-INTAKE-------8 TERM DURING MONTH	
+	declare @tbl4DataReportRow8 table(
+	HVCasePK INT
+	)
+
+	INSERT INTO @tbl4DataReportRow8
+	(
+		HVCasePK
+	)
+	SELECT h.HVCasePK FROM HVCase h
+	INNER JOIN CaseProgram cp ON cp.HVCaseFK = h.HVCasePK
+	--INNER JOIN Preintake pre ON pre.HVCaseFK = h.HVCasePK
+	left join codeLevel on cp.CurrentLevelFK = codeLevel.codeLevelPK	
+	inner join dbo.udfCaseFilters(@CaseFiltersPositive,'',@ProgramFKs) cf on cf.HVCaseFK = h.HVCasePK
+	WHERE 
+	cp.ProgramFK = @ProgramFKs
+	--AND pre.CaseStatus = '03'
+	and codeLevel.LevelName = 'Preintake-term'
+	AND (cp.DischargeDate >= @StartDate AND cp.DischargeDate <= @EndDate AND cp.DischargeDate IS NOT NULL)
+
+	DECLARE @n8 INT 
+	SET @n8 = (SELECT count(HVCasePK) FROM @tbl4DataReportRow8)		
+	
+	---- Start -----PRE-INTAKE------9 ENROLLED DURING MONTH
+	declare @tbl4DataReportRow9 table(
+	HVCasePK INT
+	)
+
+	INSERT INTO @tbl4DataReportRow9
+	(
+		HVCasePK
+	)
+	SELECT h.HVCasePK FROM HVCase h
+	INNER JOIN CaseProgram cp ON cp.HVCaseFK = h.HVCasePK
+	inner join dbo.udfCaseFilters(@CaseFiltersPositive,'',@ProgramFKs) cf on cf.HVCaseFK = h.HVCasePK
+	WHERE 
+	cp.ProgramFK = @ProgramFKs
+	AND (h.IntakeDate >= @StartDate AND h.IntakeDate <= @EndDate)
+
+	DECLARE @n9 INT 
+	SET @n9 = (SELECT count(HVCasePK) FROM @tbl4DataReportRow9)		
+	
+	
+	---- Start -----Active Families-------11 at beginning
+	declare @tbl4DataReportRow11 table(
+	HVCasePK INT
+	)
+
+	INSERT INTO @tbl4DataReportRow11
+	(
+		HVCasePK
+	)
+	SELECT h.HVCasePK  FROM HVCase h
+	INNER JOIN CaseProgram cp ON cp.HVCaseFK = h.HVCasePK
+	inner join dbo.udfCaseFilters(@CaseFiltersPositive,'',@ProgramFKs) cf on cf.HVCaseFK = h.HVCasePK
+	WHERE 
+	( cp.ProgramFK = @ProgramFKs OR  (cp.TransferredToProgramFK = @ProgramFKs AND cp.CaseStartDate < @StartDate) ) -- handling transfer cases
+	AND h.IntakeDate < @StartDate 
+	AND h.IntakeDate IS NOT null
+	AND (cp.DischargeDate >= @StartDate OR cp.DischargeDate IS NULL)
+	
+
+	DECLARE @n11 INT 
+	SET @n11 = (SELECT count(HVCasePK) FROM @tbl4DataReportRow11)	
+
+-- rspDataReport 5, '06/01/2012', '09/30/2012'		
+
+	---- Start -----Active  Families--------12a/b enrolled this month
+	declare @tbl4DataReportRow12 table(
+		HVCasePK INT,
+		Prenatal INT,
+		Postnatal int
+	)
+
+	INSERT INTO @tbl4DataReportRow12
+	(
+		HVCasePK,
+		Prenatal,
+		Postnatal
+	)
+	SELECT h.HVCasePK,
+	   CASE WHEN h.IntakeLevel = '1' THEN 1 ELSE 0 END AS Prenatal
+	 , CASE WHEN h.IntakeLevel = '2' THEN 1 ELSE 0 END AS Postnatal
+	
+	 FROM HVCase h
+	INNER JOIN CaseProgram cp ON cp.HVCaseFK = h.HVCasePK
+	inner join dbo.udfCaseFilters(@CaseFiltersPositive,'',@ProgramFKs) cf on cf.HVCaseFK = h.HVCasePK
+	WHERE 	
+	( cp.ProgramFK = @ProgramFKs OR  (cp.TransferredToProgramFK = @ProgramFKs AND (cp.CaseStartDate between  @StartDate AND @EndDate)) ) -- handling transfer cases for Families enrolled this period
+	AND h.IntakeDate >= @StartDate 
+	AND h.IntakeDate <= @EndDate 	
+	
+
+	DECLARE @n12 INT 
+	SET @n12 = (SELECT count(HVCasePK) AS count1  FROM @tbl4DataReportRow12)	
+
+	DECLARE @n12a INT 
+	SET @n12a = (SELECT sum(Prenatal) AS Prenatal  FROM @tbl4DataReportRow12)	
+	DECLARE @n12b INT 
+	SET @n12b = (SELECT sum(Postnatal) AS Postnatal FROM @tbl4DataReportRow12)	
+
+
+
+	---- Start -----Active Families--------13
+	declare @tbl4DataReportRow13 table(
+	HVCasePK INT
+	)
+
+	INSERT INTO @tbl4DataReportRow13
+	(
+		HVCasePK
+	)
+	SELECT h.HVCasePK FROM HVCase h
+	INNER JOIN CaseProgram cp ON cp.HVCaseFK = h.HVCasePK
+	inner join dbo.udfCaseFilters(@CaseFiltersPositive,'',@ProgramFKs) cf on cf.HVCaseFK = h.HVCasePK
+	WHERE 
+	( cp.ProgramFK = @ProgramFKs OR  (cp.TransferredToProgramFK = @ProgramFKs AND (cp.CaseStartDate between  @StartDate AND @EndDate)) ) -- handling transfer cases for Families enrolled this period
+	AND cp.DischargeDate >= @StartDate 
+	AND cp.DischargeDate <= @EndDate 	
+	AND h.IntakeDate IS NOT null
+
+
+	DECLARE @n13 INT 
+	SET @n13 = (SELECT count(HVCasePK) FROM @tbl4DataReportRow13)	
+
+
+	------ Start -----Active Families-----14 at end of month
+	declare @tbl4DataReportRow14 table(
+	HVCasePK INT,
+	IntakePK INT,
+	ProgramFK INT, 
+	PBTANF [char](1)
+	)
+
+	INSERT INTO @tbl4DataReportRow14
+	(
+		HVCasePK,
+		IntakePK,
+		ProgramFK,
+		PBTANF
+	)	
+	SELECT HVCasePK, IntakePK, cp.ProgramFK, PBTANF FROM HVCase h
+	INNER JOIN CaseProgram cp ON cp.HVCaseFK = h.HVCasePK
+	LEFT JOIN Intake i ON i.HVCaseFK = h.HVCasePK
+	INNER JOIN CommonAttributes ca ON ca.FormFK = i.IntakePK and ca.FormType = 'IN'  -- to get PBTANF, item 26 on the intake form
+	--inner join dbo.udfCaseFilters(@CaseFiltersPositive,'',@ProgramFKs) cf on cf.HVCaseFK = h.HVCasePK  --- Turning this ON, adds a minute to time execution. So I turned it off time being.
+	WHERE 
+	( cp.ProgramFK = @ProgramFKs OR  (cp.TransferredToProgramFK = @ProgramFKs AND (cp.CaseStartDate between  @StartDate AND @EndDate)) ) -- handling transfer cases for Families enrolled this period
+	AND (h.IntakeDate IS NOT NULL AND h.IntakeDate <= @EndDate)
+	AND (cp.DischargeDate IS NULL OR cp.DischargeDate > @EndDate)
+
+
+	DECLARE @n14a INT 
+	SET @n14a = (SELECT sum(CASE WHEN PBTANF = '1' THEN 1 ELSE 0 END) AS PBTANF FROM @tbl4DataReportRow14)	
+
+	DECLARE @n14a1 INT 
+	SET @n14a1 = (SELECT sum(CASE WHEN IntakePK IS NOT NULL THEN 1 ELSE 0 END) AS totalIntakesCompleted FROM @tbl4DataReportRow14)	
+
+	------ Start -----Active Families---figure out levels 
+	
+	declare @tbl4DataReportRow14RestOfIt table(
+	LevelName [char](50),
+	levelCount int
+	)	
+	
+	
+	;
+	
+	WITH cteDataReportRow14RestOfIt AS
+	(
+	SELECT LevelName, CASE WHEN hvlevelpk IS NOT NULL THEN 1 ELSE 0 END AS levelcount FROM @tbl4DataReportRow14 t14
+			left join (select hvlevel.hvlevelpk
+							 ,hvlevel.hvcasefk
+							 ,hvlevel.programfk
+							 ,hvlevel.levelassigndate
+							 ,levelname
+							 ,caseweight
+						   from hvlevel
+							   inner join codelevel on codelevelpk = levelfk
+							   inner join (select hvcasefk
+												 ,programfk
+												 ,max(levelassigndate) as levelassigndate
+											   from hvlevel h2
+											   where levelassigndate <= @EndDate
+											   group by hvcasefk
+													   ,programfk) e2 on e2.hvcasefk = hvlevel.hvcasefk and e2.programfk = hvlevel.programfk and e2.levelassigndate = hvlevel.levelassigndate)
+													    e3 on e3.hvcasefk = t14.hvcasepk and e3.programfk = t14.programfk
+				
+	)
+
+	INSERT INTO @tbl4DataReportRow14RestOfIt
+	(
+	LevelName,
+	levelCount
+	)	
+	SELECT 
+		   lr.LevelName
+		  ,CASE when levelCount IS NOT NULL THEN 1 ELSE 0 END AS levelCount
+		  FROM cteDataReportRow14RestOfIt	t14Rest
+	RIGHT JOIN (SELECT [LevelName] FROM [codeLevel] WHERE LevelName LIKE 'level%' AND Enrolled = 1)lr ON lr.LevelName = t14Rest.LevelName  -- add missing levelnames
+	ORDER BY LevelName 
+	
+
+	DECLARE @n14b INT 
+	SET @n14b = (SELECT sum(CASE when levelCount IS NOT NULL THEN 1 ELSE 0 END) AS tlevelCount FROM @tbl4DataReportRow14RestOfIt WHERE LevelName = 'Preintake-enroll')	
+	IF @n14b IS NULL BEGIN SET @n14b = 0 END 
+
+
+	DECLARE @n14c INT 
+	SET @n14c = (SELECT sum(levelCount) AS tlevelCount FROM @tbl4DataReportRow14RestOfIt WHERE LevelName = 'Level 1-Prenatal')	
+	DECLARE @n14d INT 
+	SET @n14d = (SELECT sum(levelCount) AS tlevelCount FROM @tbl4DataReportRow14RestOfIt WHERE LevelName = 'Level 1-SS')	
+	DECLARE @n14e INT 
+	SET @n14e = (SELECT sum(levelCount) AS tlevelCount FROM @tbl4DataReportRow14RestOfIt WHERE LevelName = 'Level 1')	
+	DECLARE @n14f INT 
+	SET @n14f = (SELECT sum(levelCount) AS tlevelCount FROM @tbl4DataReportRow14RestOfIt WHERE LevelName = 'Level 2')	
+	DECLARE @n14g INT 
+	SET @n14g = (SELECT sum(levelCount) AS tlevelCount FROM @tbl4DataReportRow14RestOfIt WHERE LevelName = 'Level 3')	
+	DECLARE @n14h INT 
+	SET @n14h = (SELECT sum(levelCount) AS tlevelCount FROM @tbl4DataReportRow14RestOfIt WHERE LevelName = 'Level 4')	
+	DECLARE @n14i INT 
+	SET @n14i = (SELECT sum(levelCount) AS tlevelCount FROM @tbl4DataReportRow14RestOfIt WHERE LevelName = 'Level X')	
+
+	
+
+
+
+--SELECT * FROM  @tbl4DataReportRow14RestOfIt
+-- rspDataReport 5, '06/01/2012', '09/30/2012'			
+
+	
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES ('1.  Positive Screens Pending Assessment at Beginning of this Period', @nposScreens)
+
+
+
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('2.  New Screens this Period', (SELECT count(HVCasePK) FROM @tbl4DataReportRow2))
+
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('    2a. Positive Screens Referred for Assessment', @n2a)
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('    2b. Positive Screens Not Referred for Assessment', (SELECT count(HVCasePK) FROM @tbl4DataReportRow2 WHERE ReferralMade = '0' AND ScreenResult = '1'))
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('    2c. Negative Screens', (SELECT count(HVCasePK) FROM @tbl4DataReportRow2 WHERE ScreenResult = '0'))
+
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('3.  Kempe Assessments this Period', @n3)
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('    3a. Positive Kempe Assigned ( or Pending Assingment) to FSW', @n3a)
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('    3b. Positive Kempe Assessments Not Assigned to FSW - Terminated', (SELECT count(HVCasePK) FROM @tbl4DataReportRow3 WHERE CaseStatus = '04'))
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('    3c. Negative Kempe Assessment', (SELECT count(HVCasePK) FROM @tbl4DataReportRow3 WHERE CaseStatus = '02' AND KempeResult = 0))
+
+
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('4.  Screens Terminated on Pre-Assessment Form this Period', @n4)
+
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('5.  Screens Pending Assessment at End of this Period ([(1+2a) - (3+4)])', (@nposScreens + @n2a) - (@n3 + @n4))
+
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('6.  Pre-Intake Families at Beginning of this Period (Participants Pending Enrollment at Beginning of this Period)', @n6)
+
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('7.  New Pre-Intake Families this Period (Same as 3a above)', @n3a)
+
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('8.  Families Terminated on Pre-Intake Form this Period', @n8)
+
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('9.  Families Enrolled on Pre-Intake Form this Period', @n9)
+
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('10.  Pre-Intake Families at the End of this Period ([(6+7) - (8+9)])', (@n6 + @n3a) - (@n8 + @n9))
+
+-- Enrolled Families
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('11.  Enrolled Families at the Beginning of this Period', @n11)
+
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('12.  Families Enrolled this Period', @n12)
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('    12a. Prenatal at Enrollment*', @n12a)
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('    12b. Postnatal at Enrollment*', @n12b)
+
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('13. Enrolled Families Discharged this Period', @n13)
+
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('14.  Enrolled Families at the End of this Period ([(11+12) - 13])', (@n11 + @n12) - @n13)
+
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('     a. Receiving TANF at Enrollment (Item 26 on the Intake Form)', @n14a)
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('       a1. Active Families with Intake Form Completed', @n14a1)
+
+INSERT INTO @tbl4DataReport(ReportTitle,Total)
+VALUES('       b. Pre-Intake Enroll', @n14b)
+
+INSERT INTO @tbl4DataReport(ReportTitle,Total)VALUES('       c. Level 1-Prenatal', @n14c)
+INSERT INTO @tbl4DataReport(ReportTitle,Total)VALUES('       d. Level 1-SS', @n14d)
+INSERT INTO @tbl4DataReport(ReportTitle,Total)VALUES('       e. Level 1', @n14e)
+INSERT INTO @tbl4DataReport(ReportTitle,Total)VALUES('       f. Level 2', @n14f)
+INSERT INTO @tbl4DataReport(ReportTitle,Total)VALUES('       g. Level 3', @n14g)
+INSERT INTO @tbl4DataReport(ReportTitle,Total)VALUES('       h. Level 4', @n14h)
+INSERT INTO @tbl4DataReport(ReportTitle,Total)VALUES('       i. Level X', @n14i)
+
+-- rspDataReport 5, '06/01/2012', '09/30/2012'			
+
+
+	SELECT * FROM @tbl4DataReport	
+	
+
+
+
+	
+
+
+end
+GO
