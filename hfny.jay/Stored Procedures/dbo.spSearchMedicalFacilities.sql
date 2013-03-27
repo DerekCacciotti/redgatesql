03a18f81e21ddacd222fc037e0d7c8e4630b5b7b
Chun Dar Chen <cchen2@albany.edu>
2013-03-27 14:32:36 +0000

    Medical Provider and Medical Facility Maintenance

--- hfny.jay/Stored Procedures/dbo.spSearchMedicalFacilities.sql
+++ hfny.jay/Stored Procedures/dbo.spSearchMedicalFacilities.sql
@@ -1,20 +1,22 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
 -- =============================================
 -- Author:		Jay Robohn
+-- Dar Chen 3/27/2013
 -- Create date: old
 -- Description:	Moved from FamSys on Feb 11, 2013
 -- =============================================
-create procedure [dbo].[spSearchMedicalFacilities]
+CREATE procedure [dbo].[spSearchMedicalFacilities]
 (
-    @MFName     varchar(50)    = null,
-    @MFAddress  varchar(40)    = null,
-    @MFCity     varchar(20)    = null,
-    @MFState    varchar(2)     = null,
-    @MFZip      varchar(10)    = null,
-    @MFPhone    varchar(12)    = null,
+    @MFName     varchar(200)    = '',
+    @MFAddress  varchar(40)    = '',
+    @MFCity     varchar(20)    = '',
+    @MFState    varchar(2)     = '',
+    @MFZip      varchar(10)    = '',
+    @MFPhone    varchar(12)    = '',
     @MFIsActive bit            = null,
     @ProgramFK  int            = null
 )
@@ -23,21 +25,48 @@ as
 
 	set nocount on;
 
-	select *
-		from listMedicalFacility mf
-		where (MFName like '%'+@MFName+'%'
-			 or MFAddress like '%'+@MFAddress+'%'
-			 or MFCity like '%'+@MFCity+'%'
-			 or MFState = @MFState
-			 or MFZip like @MFZip+'%'
-			 or MFPhone like '%'+@MFPhone+'%')
-			 and MFIsActive = ISNULL(@MFIsActive,mf.MFIsActive)
-			 and mf.ProgramFK = ISNULL(@ProgramFK,mf.ProgramFK)
-		order by
-				case when mf.MFName like '%'+@MFName+'%' then 1 else 0 end+
-				case when mf.MFAddress like '%'+@MFAddress+'%' then 1 else 0 end+
-				case when mf.MFCity like '%'+@MFCity+'%' then 1 else 0 end+
-				case when mf.MFState = @MFState then 1 else 0 end+
-				case when mf.MFZip like @MFZip+'%' then 1 else 0 end+
-				case when mf.MFIsActive = @MFIsActive then 1 else 0 end
+--DECLARE    @MFName     varchar(200)    = ''
+--DECLARE    @MFAddress  varchar(40)    = ''
+--DECLARE    @MFCity     varchar(20)    = ''
+--DECLARE    @MFState    varchar(2)     = ''
+--DECLARE    @MFZip      varchar(10)    = ''
+--DECLARE    @MFPhone    varchar(12)    = ''
+--DECLARE    @MFIsActive bit            = null
+--DECLARE    @ProgramFK  int            = 6
+
+
+; WITH v as (
+
+SELECT listMedicalFacilityPK, count(DISTINCT b.HVCaseFK) [n]
+FROM dbo.listMedicalFacility AS a
+LEFT OUTER JOIN CommonAttributes AS b ON (a.listMedicalFacilityPK = b.PC1MedicalFacilityFK
+OR a.listMedicalFacilityPK = b.TCMedicalFacilityFK)
+AND b.ProgramFK = @ProgramFK
+WHERE a.ProgramFK = @ProgramFK
+GROUP BY listMedicalFacilityPK
+)
+
+SELECT mf.*, b.n
+	FROM listMedicalFacility mf
+	LEFT OUTER JOIN v AS b ON mf.listMedicalFacilityPK = b.listMedicalFacilityPK
+	WHERE 
+	(@MFName = '' OR MFName LIKE @MFName + '%')
+	AND (@MFAddress = '' OR MFAddress LIKE '%' + @MFAddress + '%')
+	AND (@MFCity = '' OR MFCity LIKE @MFCity + '%')
+	AND (@MFState = '' OR MFState LIKE @MFState + '%')
+	AND (@MFZip = '' OR MFZip LIKE @MFZip + '%')
+	AND (@MFPhone = '' OR MFPhone LIKE '%' + @MFPhone + '%')
+	AND MFIsActive = ISNULL(@MFIsActive,MF.MFIsActive)
+	AND MF.ProgramFK = ISNULL(@ProgramFK, MF.ProgramFK)
+	--ORDER BY 
+	--CASE WHEN MF.MFName LIKE @MFName + '%' THEN 1 ELSE 0 END +
+	--CASE WHEN MF.MFAddress LIKE '%'+ @MFAddress + '%' THEN 1 ELSE 0 END +
+	--CASE WHEN MF.MFCity LIKE '%' + @MFCity + '%' THEN 1 ELSE 0 END +
+	--CASE WHEN MF.MFState = @MFState THEN 1 ELSE 0 END +
+	--CASE WHEN MF.MFZip LIKE @MFZip + '%' THEN 1 ELSE 0 END +
+	--CASE WHEN MF.MFIsActive = @MFIsActive THEN 1 ELSE 0 END
+	
+
+
+
 GO
--- hfny.jay/Stored Procedures/dbo.spSearchMedicalProviders.sql
+++ hfny.jay/Stored Procedures/dbo.spSearchMedicalProviders.sql
@@ -14,9 +14,34 @@ AS
 
 SET NOCOUNT ON;
 
-SELECT *,
-	MPFirstName+' '+MPLastName as MPName	
-	FROM listMedicalProvider mp
+--DECLARE @MPFirstName VARCHAR(200) = ''
+--DECLARE @MPLastName VARCHAR(200) = '' 
+--DECLARE @MPAddress VARCHAR(40)= ''
+--DECLARE @MPCity VARCHAR(20)=''
+--DECLARE @MPState VARCHAR(2)=''
+--DECLARE @MPZip VARCHAR(10)='' 
+--DECLARE @MPPhone VARCHAR(12)=''
+--DECLARE @MPIsActive BIT = NULL
+--DECLARE @ProgramFK INT = 6
+
+
+; WITH v as (
+
+SELECT listMedicalProviderPK, count(DISTINCT b.HVCaseFK) [n]
+FROM dbo.listMedicalProvider AS a
+LEFT OUTER JOIN CommonAttributes AS b ON (a.listMedicalProviderPK = b.PC1MedicalProviderFK
+OR a.listMedicalProviderPK = b.TCMedicalProviderFK)
+--AND b.FormType in ('IN', 'CH') 
+AND b.ProgramFK = @ProgramFK
+WHERE a.ProgramFK = @ProgramFK
+GROUP BY listMedicalProviderPK
+)
+
+SELECT mp.*,
+	MPFirstName+' '+MPLastName as MPName
+	, b.n
+	FROM listMedicalProvider AS mp
+	LEFT OUTER JOIN v AS b ON mp.listMedicalProviderPK = b.listMedicalProviderPK
 	WHERE 
 	(@MPFirstName = '' OR MPFirstName LIKE @MPFirstName + '%')
 	AND (@MPLastName = '' or MPLastName LIKE @MPLastName + '%')
@@ -27,14 +52,14 @@ SELECT *,
 	AND (@MPPhone = '' OR MPPhone LIKE '%' + @MPPhone + '%')
 	AND MPIsActive = ISNULL(@MPIsActive,mp.MPIsActive)
 	AND mp.ProgramFK = ISNULL(@ProgramFK, mp.ProgramFK)
-	ORDER BY 
-	CASE WHEN mp.MPFirstName LIKE '%'+ @MPFirstName + '%' THEN 1 ELSE 0 END +
-	CASE WHEN mp.MPLastName LIKE '%' + @MPLastName + '%' THEN 1 ELSE 0 END +
-	CASE WHEN mp.MPAddress LIKE '%'+ @MPAddress + '%' THEN 1 ELSE 0 END +
-	CASE WHEN mp.MPCity LIKE '%' + @MPCity + '%' THEN 1 ELSE 0 END +
-	CASE WHEN mp.MPState = @MPState THEN 1 ELSE 0 END +
-	CASE WHEN mp.MPZip LIKE @MPZip + '%' THEN 1 ELSE 0 END +
-	CASE WHEN mp.MPIsActive = @MPIsActive THEN 1 ELSE 0 END
+	--ORDER BY 
+	--CASE WHEN mp.MPFirstName LIKE '%'+ @MPFirstName + '%' THEN 1 ELSE 0 END +
+	--CASE WHEN mp.MPLastName LIKE '%' + @MPLastName + '%' THEN 1 ELSE 0 END +
+	--CASE WHEN mp.MPAddress LIKE '%'+ @MPAddress + '%' THEN 1 ELSE 0 END +
+	--CASE WHEN mp.MPCity LIKE '%' + @MPCity + '%' THEN 1 ELSE 0 END +
+	--CASE WHEN mp.MPState = @MPState THEN 1 ELSE 0 END +
+	--CASE WHEN mp.MPZip LIKE @MPZip + '%' THEN 1 ELSE 0 END +
+	--CASE WHEN mp.MPIsActive = @MPIsActive THEN 1 ELSE 0 END
 	
 
 
