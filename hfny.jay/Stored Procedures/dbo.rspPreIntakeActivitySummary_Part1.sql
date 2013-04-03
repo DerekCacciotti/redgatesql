183f4d255d6c97fd580779880bc47ff0b549243f
Chun Dar Chen <cchen2@albany.edu>
2013-04-03 14:09:07 +0000

    Pre-Intake Activity Summary Report based HVCase table

--- hfny.jay/Stored Procedures/dbo.rspPreIntakeActivitySummary_Part1.sql
+++ hfny.jay/Stored Procedures/dbo.rspPreIntakeActivitySummary_Part1.sql
@@ -17,75 +17,131 @@ CREATE PROCEDURE [dbo].[rspPreIntakeActivitySummary_Part1]
 	@SupervisorFK int
 AS
 
---DECLARE @StartDt DATE = '01/01/2011'
---DECLARE @EndDt DATE = '01/31/2011'
---DECLARE @programfk INT = 17
+--DECLARE @StartDt DATE = '03/01/2013'
+--DECLARE @EndDt DATE = '03/31/2013'
+--DECLARE @programfk INT = 1
+--DECLARE @WorkerFK INT = NULL
+--DECLARE	@SupervisorFK INT = null
 
-;
-with HV
-as
-(
-select distinct HVCaseFK
-	from Preintake pi
-	inner join WorkerProgram wp on wp.WorkerFK = PIFSWFK
-	inner join WorkerProgram sup on sup.WorkerFK = wp.SupervisorFK
-	where pi.ProgramFK = @programfk
-		 and PIDate < @StartDt
-		 and CaseStatus in ('02','03')
-		 and wp.WorkerFK = isnull(@WorkerFK, wp.WorkerFK)
-		 and sup.WorkerFK = isnull(@SupervisorFK, sup.WorkerFK)
-),
+; with
+
 At_Start_Of_Month
-as
-(select distinct HVCaseFK
-	from Preintake pi
-	inner join WorkerProgram wp on wp.WorkerFK = PIFSWFK
-	inner join WorkerProgram sup on sup.WorkerFK = wp.SupervisorFK
-	where HVCaseFK not in (select HV.HVCaseFK
-								from HV)
-		 and pi.ProgramFK = @programfk
-		 and PIDate < @StartDt
-		 and wp.WorkerFK = isnull(@WorkerFK, wp.WorkerFK)
-		 and sup.WorkerFK = isnull(@SupervisorFK, sup.WorkerFK)
+AS (
+	SELECT DISTINCT a.HVCasePK
+	FROM HVCase AS a
+	JOIN CaseProgram AS b ON a.HVCasePK = b.HVCaseFK
+	JOIN WorkerProgram wp on wp.WorkerFK = b.CurrentFSWFK
+	JOIN WorkerProgram sup on sup.WorkerFK = wp.SupervisorFK
+	WHERE b.ProgramFK = @programfk
+	AND (a.KempeDate IS NOT NULL AND a.KempeDate < @StartDT)
+	AND (IntakeDate IS NULL OR IntakeDate >= @StartDT)
+	AND (b.DischargeDate IS NULL OR b.DischargeDate >= @StartDT)
+	AND wp.WorkerFK = isnull(@WorkerFK, wp.WorkerFK)
+	AND sup.WorkerFK = isnull(@SupervisorFK, sup.WorkerFK)
 ),
-HV1
-as
-(
-select distinct HVCaseFK
-	from Preintake pi
-	inner join WorkerProgram wp on wp.WorkerFK = PIFSWFK
-	inner join WorkerProgram sup on sup.WorkerFK = wp.SupervisorFK
-	where pi.ProgramFK = @programfk
-		 and PIDate <= @EndDt
-		 and CaseStatus in ('02','03')
-		 and wp.WorkerFK = isnull(@WorkerFK, wp.WorkerFK)
-		 and sup.WorkerFK = isnull(@SupervisorFK, sup.WorkerFK)
-),
+
 At_End_Of_Month
-as
-(
-select distinct HVCaseFK
-	from Preintake pi
-	inner join WorkerProgram wp on wp.WorkerFK = PIFSWFK
-	inner join WorkerProgram sup on sup.WorkerFK = wp.SupervisorFK
-	where HVCaseFK not in (select HV1.HVCaseFK
-							   from HV1)
-		 and pi.ProgramFK = @programfk
-		 and PIDate <= @EndDt
-		 and wp.WorkerFK = isnull(@WorkerFK, wp.WorkerFK)
-		 and sup.WorkerFK = isnull(@SupervisorFK, sup.WorkerFK)
-)
-,
+AS (
+	SELECT DISTINCT a.HVCasePK
+	FROM HVCase AS a
+	JOIN CaseProgram AS b ON a.HVCasePK = b.HVCaseFK
+	JOIN WorkerProgram wp on wp.WorkerFK = b.CurrentFSWFK
+	JOIN WorkerProgram sup on sup.WorkerFK = wp.SupervisorFK
+	WHERE b.ProgramFK = @programfk
+	AND (a.KempeDate IS NOT NULL AND a.KempeDate <= @EndDT)
+	AND (IntakeDate IS NULL OR IntakeDate > @EndDT)
+	AND (b.DischargeDate IS NULL OR b.DischargeDate > @EndDT)
+	AND wp.WorkerFK = isnull(@WorkerFK, wp.WorkerFK)
+	AND sup.WorkerFK = isnull(@SupervisorFK, sup.WorkerFK)
+),
 
 PreAssessmentFSWAssignDate
-AS
-(
+AS (
 SELECT DISTINCT a.HVCaseFK
 FROM Preassessment AS a
 WHERE a.FSWAssignDate BETWEEN @StartDt AND @EndDt
 AND a.ProgramFK = @programfk
-)
+),
 
+PreIntakeCase 
+AS (
+SELECT a.HVCaseFK, a.PIDate, a.CaseStatus, a.PIFSWFK
+FROM Preintake AS a
+JOIN (SELECT HVCaseFK, max(PIDate) max_pidate
+FROM Preintake
+WHERE PIDate BETWEEN @StartDt AND @EndDt
+AND ProgramFK = @programfk
+GROUP BY HVCaseFK) AS b ON a.HVCaseFK = b.HVCaseFK AND a.PIDate = b.max_pidate
+),
+
+PreIntakeActivity
+AS (
+SELECT HVCaseFK
+,sum(ISNULL(PIParentLetter,0)) [Letters]
+,sum(ISNULL(PICall2Parent,0)) [Call2Parent]
+,sum(ISNULL(PICallFromParent,0)) [CallFromParent]
+,sum(ISNULL(PIVisitAttempt,0)) [VisitAttempted]
+,sum(ISNULL(PIVisitMade,0)) [VisitConducted]
+,sum(ISNULL(PIOtherHVProgram,0)) [Referrals]
+,sum(ISNULL(PIParent2Office,0)) [Parent2Office]
+,sum(ISNULL(PIProgramMaterial,0)) [ProgramMaterial]
+,sum(ISNULL(PIGift,0)) [Gift]
+,sum(ISNULL(PICaseReview,0)) [CaseReview]
+,sum(ISNULL(PIOtherActivity,0)) [OtherActivity]
+FROM Preintake
+WHERE PIDate BETWEEN @StartDt AND @EndDt AND ProgramFK = @programfk
+GROUP BY HVCaseFK
+),
+
+AllInOne 
+AS (
+SELECT x.HVCasePK_At_Start, x.HVCasePK_At_End, 
+h.KempeDate, h.IntakeDate, c.DischargeDate, p.CaseStatus, p.PIDate,
+CASE WHEN (isnull(p.PIDate, h.KempeDate) NOT BETWEEN dateadd(day, -30.5,  @EndDt) AND  @EndDt) THEN 1 ELSE 0 END [No_Status],
+CASE WHEN p.PIFSWFK IS NOT NULL AND y.HVCaseFK IS NOT NULL then 1 else 0 END [AssignedFSW]
+, z.*
+FROM (SELECT isnull(a.HVCasePK, b.HVCasePK) [HVCasePK]
+, a.HVCasePK [HVCasePK_At_Start], b.HVCasePK [HVCasePK_At_End]
+FROM At_Start_Of_Month AS a 
+FULL OUTER JOIN At_End_Of_Month AS b ON a.HVCasePK = b.HVCasePK) x
+JOIN HVCase AS h ON h.HVCasePK = x.HVCasePK
+JOIN CaseProgram AS c ON c.HVCaseFK = x.HVCasePK
+LEFT OUTER JOIN PreIntakeCase AS p ON p.HVCaseFK = x.HVCasePK
+LEFT OUTER JOIN PreAssessmentFSWAssignDate AS y ON y.HVCaseFK = x.HVCasePK
+LEFT OUTER JOIN PreIntakeActivity AS z ON z.HVCaseFK = x.HVCasePK
+),
+
+AllSummary 
+AS (
+SELECT sum(CASE WHEN PIDate IS NOT NULL THEN 1 ELSE 0 END) [PreInTakeCases]
+--, sum(CASE WHEN HVCasePK_At_Start IS NULL THEN 1 ELSE 0 END) [New_PreIntake_Cases]
+, sum(CASE WHEN HVCasePK_At_Start IS NOT NULL THEN 1 ELSE 0 END) [At_Start_of_Month]
+, sum(CASE WHEN [AssignedFSW] = 1 THEN 1 ELSE 0 END) [AssignedFSW]
+, sum(CASE WHEN CaseStatus = '02' THEN 1 ELSE 0 END) [Enrolled]
+, sum(CASE WHEN CaseStatus = '03' THEN 1 ELSE 0 END) [Terminated]
+, sum(CASE WHEN CaseStatus = '01' THEN 1 ELSE 0 END) [Continued]
+, sum(CASE WHEN HVCasePK_At_End IS NOT NULL THEN 1 ELSE 0 END) [At_End_of_Month]
+,sum(ISNULL([Letters],0)) [Letters]
+,sum(ISNULL([Call2Parent],0)) [Call2Parent]
+,sum(ISNULL([CallFromParent],0)) [CallFromParent]
+,sum(ISNULL([VisitAttempted],0)) [VisitAttempted]
+,sum(ISNULL([VisitConducted],0)) [VisitConducted]
+,sum(ISNULL([Referrals],0)) [Referrals]
+,sum(ISNULL([Parent2Office],0)) [Parent2Office]
+,sum(ISNULL([ProgramMaterial],0)) [ProgramMaterial]
+,sum(ISNULL([Gift],0)) [Gift]
+,sum(ISNULL([CaseReview],0)) [CaseReview]
+,sum(ISNULL([OtherActivity],0)) [OtherActivity]
+FROM AllInOne
+
+)
+-- listing data
+--SELECT * from AllInOne  
+
+-- calculate summary 
+SELECT * FROM AllSummary
+
+/*
 select count(*) [PreInTakeCases]
 	  ,(select count(*)
 			from At_Start_Of_Month) [At_Start_of_Month]
@@ -115,4 +171,6 @@ select count(*) [PreInTakeCases]
 		 and PIDate between @StartDt and @EndDt
 		 and wp.WorkerFK = isnull(@WorkerFK, wp.WorkerFK)
 		 and sup.WorkerFK = isnull(@SupervisorFK, sup.WorkerFK)
+
+*/
 GO
--- hfny.jay/Stored Procedures/dbo.rspPreIntakeActivitySummary_Part2.sql
+++ hfny.jay/Stored Procedures/dbo.rspPreIntakeActivitySummary_Part2.sql
@@ -17,10 +17,136 @@ CREATE procedure [dbo].[rspPreIntakeActivitySummary_Part2]-- Add the parameters 
     @SupervisorFK int
 as
 
-	--DECLARE @StartDt DATE = '01/01/2011'
-	--DECLARE @EndDt DATE = '01/31/2011'
-	--DECLARE @programfk INT = 17
+--DECLARE @StartDt DATE = '03/01/2013'
+--DECLARE @EndDt DATE = '03/31/2013'
+--DECLARE @programfk INT = 1
+--DECLARE @WorkerFK INT = NULL
+--DECLARE	@SupervisorFK INT = null
 
+; with
+
+At_Start_Of_Month
+AS (
+	SELECT DISTINCT a.HVCasePK
+	FROM HVCase AS a
+	JOIN CaseProgram AS b ON a.HVCasePK = b.HVCaseFK
+	JOIN WorkerProgram wp on wp.WorkerFK = b.CurrentFSWFK
+	JOIN WorkerProgram sup on sup.WorkerFK = wp.SupervisorFK
+	WHERE b.ProgramFK = @programfk
+	AND (a.KempeDate IS NOT NULL AND a.KempeDate < @StartDT)
+	AND (IntakeDate IS NULL OR IntakeDate >= @StartDT)
+	AND (b.DischargeDate IS NULL OR b.DischargeDate >= @StartDT)
+	AND wp.WorkerFK = isnull(@WorkerFK, wp.WorkerFK)
+	AND sup.WorkerFK = isnull(@SupervisorFK, sup.WorkerFK)
+),
+
+At_End_Of_Month
+AS (
+	SELECT DISTINCT a.HVCasePK
+	FROM HVCase AS a
+	JOIN CaseProgram AS b ON a.HVCasePK = b.HVCaseFK
+	JOIN WorkerProgram wp on wp.WorkerFK = b.CurrentFSWFK
+	JOIN WorkerProgram sup on sup.WorkerFK = wp.SupervisorFK
+	WHERE b.ProgramFK = @programfk
+	AND (a.KempeDate IS NOT NULL AND a.KempeDate <= @EndDT)
+	AND (IntakeDate IS NULL OR IntakeDate > @EndDT)
+	AND (b.DischargeDate IS NULL OR b.DischargeDate > @EndDT)
+	AND wp.WorkerFK = isnull(@WorkerFK, wp.WorkerFK)
+	AND sup.WorkerFK = isnull(@SupervisorFK, sup.WorkerFK)
+),
+
+PreIntakeCase 
+AS (
+SELECT a.HVCaseFK
+,a.CaseStatus
+,a.ProgramFK
+,a.DischargeReason
+,a.PIDate
+,a.PIFSWFK
+,a.KempeFK
+FROM Preintake AS a
+JOIN (SELECT HVCaseFK, max(PIDate) max_pidate
+FROM Preintake
+WHERE PIDate BETWEEN @StartDt AND @EndDt
+AND ProgramFK = @programfk
+GROUP BY HVCaseFK) AS b ON a.HVCaseFK = b.HVCaseFK AND a.PIDate = b.max_pidate
+),
+
+PreIntakeActivity
+AS (
+SELECT HVCaseFK
+,sum(ISNULL(PIParentLetter,0)) [Letters]
+,sum(ISNULL(PICall2Parent,0)) [Call2Parent]
+,sum(ISNULL(PICallFromParent,0)) [CallFromParent]
+,sum(ISNULL(PIVisitAttempt,0)) [VisitAttempted]
+,sum(ISNULL(PIVisitMade,0)) [VisitConducted]
+,sum(ISNULL(PIOtherHVProgram,0)) [Referrals]
+,sum(ISNULL(PIParent2Office,0)) [Parent2Office]
+,sum(ISNULL(PIProgramMaterial,0)) [ProgramMaterial]
+,sum(ISNULL(PIGift,0)) [Gift]
+,sum(ISNULL(PICaseReview,0)) [CaseReview]
+,sum(ISNULL(PIOtherActivity,0)) [OtherActivity]
+FROM Preintake
+WHERE PIDate BETWEEN @StartDt AND @EndDt AND ProgramFK = @programfk
+GROUP BY HVCaseFK
+),
+
+AllInOne 
+AS (
+SELECT 
+c.PC1ID [Participant]
+,rtrim(w.LastName)+', '+rtrim(w.FirstName) [Worker]
+,cast(datediff(day,h.KempeDate,isnull(p.PIDate, @EndDt)) as varchar(12))+' days' [DaysInPreintake]
+,case when p.CaseStatus = '01' then 'Engagement Continue'
+      when p.CaseStatus = '02' then 'Enrolled '+ convert(varchar(12),h.IntakeDate,101)
+      when p.CaseStatus = '03' then isnull((select top 1 rtrim(ReportDischargeText)
+							   from codeDischarge
+							   where DischargeCode = p.DischargeReason)+' ','Terminated ')
+				                  +isnull(convert(varchar(12),c.DischargeDate,101),'')
+	  when (isnull(p.PIDate, h.KempeDate) NOT BETWEEN dateadd(day, -30.5,  @EndDt) AND  @EndDt) then 'No Status'
+      else '(Status Unknown)' end [CaseStatus]
+,z.[Letters]
+,z.[Call2Parent]
+,z.[CallFromParent]
+,z.[VisitAttempted]
+,z.[VisitConducted]
+,z.[Referrals]
+,z.[Parent2Office]
+,z.[ProgramMaterial]
+,z.[Gift]
+,z.[CaseReview]
+,z.[OtherActivity]
+
+--,p.CaseStatus [CaseStatus]
+,p.ProgramFK
+,p.DischargeReason
+,p.PIDate
+,p.PIFSWFK
+,p.KempeFK
+,p.HVCaseFK
+
+FROM 
+(SELECT isnull(a.HVCasePK, b.HVCasePK) [HVCasePK]
+, a.HVCasePK [HVCasePK_At_Start], b.HVCasePK [HVCasePK_At_End]
+FROM At_Start_Of_Month AS a 
+FULL OUTER JOIN At_End_Of_Month AS b ON a.HVCasePK = b.HVCasePK) x
+
+JOIN HVCase AS h ON h.HVCasePK = x.HVCasePK
+JOIN CaseProgram AS c ON c.HVCaseFK = x.HVCasePK
+LEFT OUTER JOIN PreIntakeCase AS p ON p.HVCaseFK = x.HVCasePK
+LEFT OUTER JOIN PreIntakeActivity AS z ON z.HVCaseFK = x.HVCasePK
+
+left outer join Worker as w on w.WorkerPK = isnull(p.PIFSWFK, c.CurrentFSWFK)
+inner join WorkerProgram wp on wp.WorkerFK = isnull(p.PIFSWFK, c.CurrentFSWFK)
+inner join WorkerProgram sup on sup.WorkerFK = wp.SupervisorFK
+
+where (p.CaseStatus is not null) or (isnull(p.PIDate, h.KempeDate) NOT BETWEEN dateadd(day, -30.5,  @EndDt) AND  @EndDt) 
+)
+
+select * from allinone
+
+
+/* -- old codes
 	select c.PC1ID [Participant]
 		  ,rtrim(w.LastName)+', '+rtrim(w.FirstName) [Worker]
 		  ,cast(datediff(day,b.KempeDate,a.PIDate) as varchar(12))+' days' [DaysInPreintake]
@@ -65,4 +191,6 @@ as
 			and wp.WorkerFK = isnull(@WorkerFK, wp.WorkerFK)
 			and sup.WorkerFK = isnull(@SupervisorFK, sup.WorkerFK)
 		order by [Participant]
+		
+		*/
 GO
