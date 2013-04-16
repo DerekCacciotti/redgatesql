6e2305f5a93673d7bf03ba454074b9318112abe4
Chris Papas <cpapas@albany.edu>
2013-04-16 17:22:56 +0000

    FatherFigureOBPPC2 fix

--- hfny.jay/Stored Procedures/dbo.spGetFatherFiguresByHVCaseFK.sql
+++ hfny.jay/Stored Procedures/dbo.spGetFatherFiguresByHVCaseFK.sql
@@ -47,8 +47,11 @@ as
 		  ,case when FF.IsOBP=1 then 'Yes' else 'No' end as OBPOfCase
 		  ,case when FF.IsPC2=1 then 'Yes' else 'No' end as PC2OfCase
 		  ,case when FF.LiveInPC1Home=1 then 'Yes' else 'No' end as InPC1Home
+		  , h.OBPFK
+		  , h.PC2FK
 		from FatherFigure FF
 		inner join PC P on P.PCPK = FF.PCFK
+		INNER JOIN HVCase h ON h.HVCasePK = FF.HVCaseFK
 		left outer join dbo.FormReviewedTableList('FF',@ProgramFK) frtl on FormFK = FatherFigurePK
 		where HVCaseFK = @HVCaseFK
 				and ProgramFK = @ProgramFK
