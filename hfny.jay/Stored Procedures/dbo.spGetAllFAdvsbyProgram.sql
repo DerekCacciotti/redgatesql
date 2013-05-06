0250ed9224898a4f450a036f7a96918e1cf15591
Chris Papas <cpapas@albany.edu>
2013-05-06 14:44:07 +0000

    'as per John, the Father Advocate form needs a different routine because we are filling         'the form with FA's who are either currently active (regardless of date started)         'or if the FA was working at the time the form was completed

--- hfny.jay/Stored Procedures/dbo.spGetAllFAdvsbyProgram.sql
+++ hfny.jay/Stored Procedures/dbo.spGetAllFAdvsbyProgram.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -6,7 +7,11 @@ GO
 -- =============================================
 -- Author:		Jay Robohn
 -- Create date: July 17, 2012
+-- Edited by: Chris Papas
+-- Edite Date: May 6, 2013
 -- Description:	Return Father Advocate workers, who are working at a specified date from a specified program
+-- Edit Reason: As per John, we are to return all Father Advocates who were either working at the
+--              time the form was created OR are currently working
 -- =============================================
 
 CREATE procedure [dbo].[spGetAllFAdvsbyProgram]
@@ -16,16 +21,14 @@ as
 begin
 	set nocount on;
 
-	exec spGetAllWorkersbyProgram @ProgramFK, @EventDate, 'FAdv'
+	--exec spGetAllWorkersbyProgram @ProgramFK, @EventDate, 'FAdv'
 
-	--select WorkerPK
-	--	  ,FirstName
-	--	  ,LastName
-	--	from Worker w
-	--		inner join WorkerProgram wp on wp.WorkerFK = w.WorkerPK
-	--	where programfk = @ProgramFK
-	--		 and
-	--		 @EventDate between FatherAdvocateStartDate and isnull(FatherAdvocateEndDate,dateadd(dd,270,datediff(dd,0,getdate())))
-	--	order by LastName
+	select WorkerPK,FirstName,LastName
+		from Worker w
+			inner join WorkerProgram wp on wp.WorkerFK = w.WorkerPK
+		where programfk = @ProgramFK
+		AND (TerminationDate IS NULL OR FatherAdvocateStartDate < @EventDate)
+		AND FatherAdvocate=1
+		order by LastName
 end
 GO
