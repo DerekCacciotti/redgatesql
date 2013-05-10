a93c67cef32aeabc632ad3ddd5bcbb9ab69868d4
Jay Robohn <jrobohn@albany.edu>
2013-05-10 17:10:03 +0000

    Cleanup

--- hfny.jay/Functions/dbo.IsNullOrEmpty.sql
+++ hfny.jay/Functions/dbo.IsNullOrEmpty.sql
@@ -1,28 +1,29 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
 -- =============================================
--- Author:		IsNullOrEmpty
+-- Function:	IsNullOrEmpty
+-- Author:		jrobohn
 -- Create date: July 20, 2012
 -- Description:	Mimics the VB/.NET function of the same name
 -- =============================================
-CREATE FUNCTION [dbo].[IsNullOrEmpty]	
-(
-	-- Add the parameters for the function here
-	@VarString varchar(max)
+CREATE function [dbo].[IsNullOrEmpty]
+(-- Add the parameters for the function here
+    @VarString varchar(max)
 )
 returns bit
-AS
-BEGIN
+as
+begin
 	-- Declare the return variable here
-	DECLARE @ReturnValue Bit
+	declare @ReturnValue bit
 
 	-- Add the T-SQL statements to compute the return value here
-	set @ReturnValue = Case when @VarString is null or @VarString = '' then 1 else 0 end
+	set @ReturnValue = case when @VarString is null or @VarString = '' then 1 else 0 end
 
 	-- Return the result of the function
-	RETURN @ReturnValue
+	return @ReturnValue
 
-END
+end
 GO
