386e511df8a6ebc9931b3186fa3d7923f2878a73
Chris Papas <cpapas@albany.edu>
2013-05-17 16:31:58 +0000

    New report Training 10_2 Orientation

--- hfny.jay/Stored Procedures/dbo.rspTraining_10_2_Orientation.sql
+++ hfny.jay/Stored Procedures/dbo.rspTraining_10_2_Orientation.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -17,13 +18,11 @@ BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-
-
 ;WITH  cteMain AS (
 	SELECT g.WorkerPK, g.WrkrLName, g.FirstHomeVisitDate
 	, g.FirstKempeDate, g.SupervisorFirstEvent
 	, g.FirstEvent
-		, w.FirstName + ' ' + w.LastName AS WorkerName
+		, RTRIM(w.FirstName) + ' ' + RTRIM(w.LastName) AS WorkerName
 		, wp.Supervisor
 		, wp.FSW
 		, wp.FAW
@@ -60,7 +59,7 @@ BEGIN
 			LEFT JOIN Training t ON t.TrainingPK = ta.TrainingFK
 			LEFT JOIN TrainingDetail td ON td.TrainingFK = t.TrainingPK
 			LEFT JOIN codeTopic t1 ON td.TopicFK=t1.codeTopicPK
-	WHERE t1.TopicCode=1.0
+	WHERE t1.TopicCode = 1.0
 	GROUP BY WorkerPK, WrkrLName, FirstHomeVisitDate
 	, FirstKempeDate, SupervisorFirstEvent, FirstEvent
 			, t1.TopicCode
@@ -70,6 +69,25 @@ BEGIN
 			, FSW
 			, FAW
 			, rownumber
+)
+
+, cteAddMissingWorkers_cte10_2a AS (
+	--if a worker has NO trainings, they won't appear at all, so add them back
+	SELECT cteMain.RowNumber
+		, cteMain.workerpk
+		, '1.0' AS TopicCode
+		, cte10_2a.topicname
+		, cte10_2a.TrainingDate
+		, cte10_2a.FirstHomeVisitDate
+		, cte10_2a.FirstKempeDate
+		, cte10_2a.SupervisorFirstEvent
+		, cte10_2a.FirstEvent
+		, cteMain.WorkerName
+		, cteMain.Supervisor
+		, cteMain.FSW
+		, cteMain.FAW
+	FROM cte10_2a
+	RIGHT JOIN cteMain ON cteMain.WorkerPK = cte10_2a.WorkerPK
 )
 
 --Now we get the trainings (or lack thereof) for topic code 2.0
@@ -104,6 +122,26 @@ BEGIN
 			, rownumber
 )
 
+
+, cteAddMissingWorkers_cte10_2b AS (
+	--if a worker has NO trainings, they won't appear at all, so add them back
+	SELECT cteMain.RowNumber
+		, cteMain.workerpk
+		, '2.0' AS TopicCode
+		, cte10_2b.topicname
+		, cte10_2b.TrainingDate
+		, cte10_2b.FirstHomeVisitDate
+		, cte10_2b.FirstKempeDate
+		, cte10_2b.SupervisorFirstEvent
+		, cte10_2b.FirstEvent
+		, cteMain.WorkerName
+		, cteMain.Supervisor
+		, cteMain.FSW
+		, cteMain.FAW
+	FROM cte10_2b
+	RIGHT JOIN cteMain ON cteMain.WorkerPK = cte10_2b.WorkerPK
+)
+
 --Now we get the trainings (or lack thereof) for topic code 3.0
 , cte10_2c AS (
 	SELECT RowNumber
@@ -134,6 +172,25 @@ BEGIN
 			, FSW
 			, FAW
 			, rownumber
+)
+
+, cteAddMissingWorkers_cte10_2c AS (
+	--if a worker has NO trainings, they won't appear at all, so add them back
+	SELECT cteMain.RowNumber
+		, cteMain.workerpk
+		, '3.0' AS TopicCode
+		, cte10_2c.topicname
+		, cte10_2c.TrainingDate
+		, cte10_2c.FirstHomeVisitDate
+		, cte10_2c.FirstKempeDate
+		, cte10_2c.SupervisorFirstEvent
+		, cte10_2c.FirstEvent
+		, cteMain.WorkerName
+		, cteMain.Supervisor
+		, cteMain.FSW
+		, cteMain.FAW
+	FROM cte10_2c
+	RIGHT JOIN cteMain ON cteMain.WorkerPK = cte10_2c.WorkerPK
 )
 
 --Now we get the trainings (or lack thereof) for topic code 4.0
@@ -168,6 +225,25 @@ BEGIN
 			, rownumber
 )
 
+, cteAddMissingWorkers_cte10_2d AS (
+	--if a worker has NO trainings, they won't appear at all, so add them back
+	SELECT cteMain.RowNumber
+		, cteMain.workerpk
+		, '4.0' AS TopicCode
+		, cte10_2d.topicname
+		, cte10_2d.TrainingDate
+		, cte10_2d.FirstHomeVisitDate
+		, cte10_2d.FirstKempeDate
+		, cte10_2d.SupervisorFirstEvent
+		, cte10_2d.FirstEvent
+		, cteMain.WorkerName
+		, cteMain.Supervisor
+		, cteMain.FSW
+		, cteMain.FAW
+	FROM cte10_2d
+	RIGHT JOIN cteMain ON cteMain.WorkerPK = cte10_2d.WorkerPK
+)
+
 --Now we get the trainings (or lack thereof) for topic code 5.0
 , cte10_2e AS (
 	SELECT RowNumber
@@ -200,17 +276,37 @@ BEGIN
 			, rownumber
 )
 
-, cteUNION AS(
-	SELECT * FROM cte10_2a
-	UNION
-	SELECT * FROM cte10_2b
-	UNION	
-	SELECT * FROM cte10_2c
-	UNION	
-	SELECT * FROM cte10_2d
-	UNION	
-	SELECT * FROM cte10_2e
+, cteAddMissingWorkers_cte10_2e AS (
+	--if a worker has NO trainings, they won't appear at all, so add them back
+	SELECT cteMain.RowNumber
+		, cteMain.workerpk
+		, '5.0' AS TopicCode
+		, cte10_2e.topicname
+		, cte10_2e.TrainingDate
+		, cte10_2e.FirstHomeVisitDate
+		, cte10_2e.FirstKempeDate
+		, cte10_2e.SupervisorFirstEvent
+		, cte10_2e.FirstEvent
+		, cteMain.WorkerName
+		, cteMain.Supervisor
+		, cteMain.FSW
+		, cteMain.FAW
+	FROM cte10_2e
+	RIGHT JOIN cteMain ON cteMain.WorkerPK = cte10_2e.WorkerPK
 )
+
+, cteUNION AS(
+	SELECT * FROM cteAddMissingWorkers_cte10_2a
+	UNION
+	SELECT * FROM cteAddMissingWorkers_cte10_2b
+	UNION	
+	SELECT * FROM cteAddMissingWorkers_cte10_2c
+	UNION	
+	SELECT * FROM cteAddMissingWorkers_cte10_2d
+	UNION	
+	SELECT * FROM cteAddMissingWorkers_cte10_2e
+)
+
 
 , cteMeetTarget AS (
 	SELECT MAX(RowNumber) OVER(PARTITION BY TopicCode) as TotalWorkers
@@ -226,7 +322,7 @@ BEGIN
 	, FirstKempeDate
 	, cteunion.SupervisorFirstEvent
 	, FirstEvent
-	, CASE WHEN TrainingDate <= dateadd(day, 183, FirstEvent) THEN 'Meeting' ELSE 'Not Meeting' END AS 'Meets Target'
+	, CASE WHEN TrainingDate <= dateadd(day, 183, FirstEvent) THEN 'T' ELSE 'F' END AS 'Meets Target'
 	FROM cteUNION
 	GROUP BY cteunion.WorkerPK
 	, WorkerName
@@ -247,11 +343,39 @@ BEGIN
 , cteCountMeeting AS (
 		SELECT TopicCode, count(*) AS totalmeetingcount
 		FROM cteMeetTarget
-		WHERE [Meets Target]='Meeting'
+		WHERE [Meets Target]='T'
 		GROUP BY TopicCode
 )
 
-SELECT *, CAST(totalmeetingcount AS decimal(10,2)) / CAST(TotalWorkers AS decimal(10,2)) AS MeetingPercent
+
+SELECT TotalWorkers
+, WorkerPK
+, WorkerName
+, Supervisor
+, FSW
+, FAW
+, cteMeetTarget.topiccode
+, cteCountMeeting.TopicCode
+	, CASE WHEN cteCountMeeting.TopicCode = 1.0 THEN '10-2a. Staff (assessment workers, home visitors and supervisors) are oriented
+		to their roles as they relate to the programs goals, services policies and operating procedures and philosophy of home visiting
+		/family support prior to direct work with children and families' 
+	WHEN cteCountMeeting.TopicCode = 2.0 THEN '10-2b. Staff (assessment workers, home visitors and supervisors) are oriented 
+		to the programs relationship with other community resources prior to direct work with children and families'  
+	WHEN cteCountMeeting.TopicCode = 3.0 THEN '10-2c. Staff (assessment workers, home visitors and supervisors) are oriented
+		to child abuse and neglect indicators and reporting requirements prior to direct work with children and families' 
+	WHEN cteCountMeeting.TopicCode = 4.0 THEN '10-2d. Staff (assessment workers, home visitors and supervisors) are oriented
+		to issues of confidentiality prior to direct work with children and families' 
+	WHEN cteCountMeeting.TopicCode = 5.0 THEN '10-2e. Staff (assessment workers, home visitors and supervisors) are oriented
+		to issues related to boundaries prior to direct work with children and families' 
+	END AS TopicName
+, TrainingDate
+, FirstHomeVisitDate
+, FirstKempeDate
+, SupervisorFirstEvent
+, FirstEvent
+, [Meets Target]
+, totalmeetingcount
+, CONVERT(VARCHAR(MAX), CONVERT(INT,100*(CAST(totalmeetingcount AS decimal(10,2)) / CAST(TotalWorkers AS decimal(10,2)))))+ '%'  AS MeetingPercent
 ,	CASE WHEN cast(totalmeetingcount AS DECIMAL) / cast(TotalWorkers AS DECIMAL) = 1 THEN '3' 
 	WHEN cast(totalmeetingcount AS DECIMAL) / cast(TotalWorkers AS DECIMAL) > .9 THEN '2'
 	WHEN cast(totalmeetingcount AS DECIMAL) / cast(TotalWorkers AS DECIMAL) < .9 THEN '1'
@@ -259,7 +383,6 @@ SELECT *, CAST(totalmeetingcount AS decimal(10,2)) / CAST(TotalWorkers AS decima
 FROM cteMeetTarget
 INNER JOIN cteCountMeeting ON cteCountMeeting.TopicCode = cteMeetTarget.TopicCode
 ORDER BY cteCountMeeting.topiccode
-
 
 END
 GO
