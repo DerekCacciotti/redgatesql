9042c38381886c1948558c309ba3084e98d827cc
Chun Dar Chen <cchen2@albany.edu>
2013-05-24 13:36:35 +0000

    worker

--- hfny.jay/Stored Procedures/dbo.spGetAllTerminatedWorkers.sql
+++ hfny.jay/Stored Procedures/dbo.spGetAllTerminatedWorkers.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -10,12 +11,11 @@ set nocount on
 SELECT c.ProgramName
 , a.LastName, a.FirstName
 , convert(varchar(12), b.TerminationDate, 101) [TerminationDate]
-, CASE WHEN b.FAW = 1 THEN 'FAW ' ELSE '' END +
-CASE WHEN b.FAW = 1 THEN 'FAW ' ELSE '' END +
-CASE WHEN b.FSW = 1 THEN 'FSW ' ELSE '' END +
-CASE WHEN b.FatherAdvocate = 1 THEN 'FAdv ' ELSE '' END +
-CASE WHEN b.Supervisor = 1 THEN 'SUP ' ELSE '' END +
-CASE WHEN b.ProgramManager= 1 THEN 'PM ' ELSE '' END [Roles]
+, CASE WHEN b.FAWStartDate IS NOT NULL THEN 'FAW ' ELSE '' END +
+CASE WHEN b.FSWStartDate IS NOT NULL THEN 'FSW ' ELSE '' END +
+CASE WHEN b.FatherAdvocateStartDate IS NOT NULL THEN 'FAdv ' ELSE '' END +
+CASE WHEN b.SupervisorStartDate IS NOT NULL THEN 'SUP ' ELSE '' END +
+CASE WHEN b.ProgramManagerStartDate IS NOT NULL THEN 'PM ' ELSE '' END [Roles]
 , b.ProgramFK
 , a.WorkerPK
 , b.WorkerProgramPK
@@ -24,6 +24,7 @@ JOIN WorkerProgram AS b ON a.WorkerPK = b.WorkerFK
 JOIN HVProgram AS c ON c.HVProgramPK = b.ProgramFK
 WHERE b.TerminationDate IS NOT NULL
 AND ProgramFK = isnull(@ProgramFK, ProgramFK)
+AND (LastName NOT LIKE 'Supervisor%' AND FirstName NOT LIKE 'Historical%')
 ORDER BY c.ProgramName, a.LastName, a.FirstName
 
 
