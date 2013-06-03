b99fc1df4e4321caf2bdd6c763cbd9b1117d08b6
Jay Robohn <jrobohn@albany.edu>
2013-06-03 15:21:45 +0000

    Added ASQ-SE History report and tweaked GetAllReports

--- hfny.jay/Stored Procedures/dbo.spGetAllReports.sql
+++ hfny.jay/Stored Procedures/dbo.spGetAllReports.sql
@@ -9,78 +9,81 @@ GO
 -- Description:	<Gets all items in the codeReportCatalog table>
 -- =============================================
 CREATE procedure [dbo].[spGetAllReports]
-(@strProgramFK varchar(3) = null,
- @UserName varchar(15) = null
+(
+    @strProgramFK varchar(3)     = null,
+    @UserName     varchar(15)    = null
 )
-
 as
 begin
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	set nocount on;
 	declare @ProgramFK int = cast(@strProgramFK as int);
-	
+
 	-- Insert statements for procedure here
 	with cteLastRunByUser
-		as 
-		(
-		select ProgramFK,ReportFK,max(TimeRun) as UserLastRun
-		from ReportHistory rh
-		where ProgramFK=@ProgramFK
-				and UserFK=@UserName
-		group by ProgramFK, ReportFK
-		)
-	,cteLastRunByProgram
-		as 
-		(
-		select ProgramFK,ReportFK,max(TimeRun) as ProgramLastRun
-		from ReportHistory rh
-		where ProgramFK=@ProgramFK
-		group by ProgramFK, ReportFK
-		)
-	,cteFrequencyByUser
-		as
-		(
-		select ROW_NUMBER() over(order by count(ReportFK) desc) as UserRank,Count(ReportFK) as UserCount,ReportFK
-		from ReportHistory rh
-		where ProgramFK=@ProgramFK
-				and UserFK=@UserName
-		group by ProgramFK, ReportFK
-		
-		)
-	,cteFrequencyByProgram
-		as
-		(
-		select ROW_NUMBER() over(order by count(ReportFK) desc) as ProgramRank,Count(ReportFK) as ProgramCount,ReportFK
-		from ReportHistory rh
-		where ProgramFK=@ProgramFK
-		group by ProgramFK, ReportFK
-		
-		)
-select codeReportCatalogPK
-	  ,	rc.ReportName
-	  ,	ReportCategory
-	  ,	ReportDescription
-	  ,	ReportClass
-	  ,	CriteriaOptions
-	  ,	Defaults
-	  ,	'O:'+rtrim(CriteriaOptions)+' D(calc):'
-			+rtrim(dbo.CalculateReportDefaults([Defaults]))+' D(raw):'+rtrim([Defaults]) as DefaultsBag
-	  ,	Keywords
-	  ,	convert(varchar(10),UserLastRun,101) as UserLastRun
-	  ,	convert(varchar(10),ProgramLastRun,101) as ProgramLastRun
-	  ,	UserRank 
-	  ,	UserCount
-	  ,	ProgramRank
-	  ,	ProgramCount
-	from codeReportCatalog rc
-		left outer join cteLastRunByUser lrbu on lrbu.ReportFK=rc.codeReportCatalogPK
-		left outer join cteLastRunByProgram lrbp on lrbp.ReportFK=rc.codeReportCatalogPK
-		left outer join cteFrequencyByUser fbu on fbu.ReportFK=rc.codeReportCatalogPK
-		left outer join cteFrequencyByProgram fbp on fbp.ReportFK = rc.codeReportCatalogPK
-	where ReportClass is not null
-	order by [ReportCategory]
-			,[ReportName]
+	as (select ProgramFK
+			  ,ReportFK
+			  ,max(TimeRun) as UserLastRun
+			from ReportHistory rh
+			where ProgramFK = @ProgramFK
+				 and UserFK = @UserName
+			group by ProgramFK
+					,ReportFK
+	),
+	cteLastRunByProgram
+	as (select ProgramFK
+			  ,ReportFK
+			  ,max(TimeRun) as ProgramLastRun
+			from ReportHistory rh
+			where ProgramFK = @ProgramFK
+			group by ProgramFK
+					,ReportFK
+	),
+	cteFrequencyByUser
+	as (select row_number() over (order by count(ReportFK) desc) as UserRank
+			  ,count(ReportFK) as UserCount
+			  ,ReportFK
+			from ReportHistory rh
+			where ProgramFK = @ProgramFK
+				 and UserFK = @UserName
+			group by ProgramFK
+					,ReportFK
+
+	),
+	cteFrequencyByProgram
+	as (select row_number() over (order by count(ReportFK) desc) as ProgramRank
+			  ,count(ReportFK) as ProgramCount
+			  ,ReportFK
+			from ReportHistory rh
+			where ProgramFK = @ProgramFK
+			group by ProgramFK
+					,ReportFK
+
+	)
+	select codeReportCatalogPK
+		  ,rc.ReportName
+		  ,ReportCategory
+		  ,ReportDescription
+		  ,ReportClass
+		  ,CriteriaOptions
+		  ,Defaults
+		  --,'O:'+rtrim(CriteriaOptions)+' D(calc):'+rtrim(dbo.CalculateReportDefaults([Defaults]))+' D(raw):'+rtrim([Defaults]) as DefaultsBag
+		  ,Keywords
+		  ,convert(varchar(10),UserLastRun,101) as UserLastRun
+		  ,convert(varchar(10),ProgramLastRun,101) as ProgramLastRun
+		  ,UserRank
+		  ,UserCount
+		  ,ProgramRank
+		  ,ProgramCount
+		from codeReportCatalog rc
+			left outer join cteLastRunByUser lrbu on lrbu.ReportFK = rc.codeReportCatalogPK
+			left outer join cteLastRunByProgram lrbp on lrbp.ReportFK = rc.codeReportCatalogPK
+			left outer join cteFrequencyByUser fbu on fbu.ReportFK = rc.codeReportCatalogPK
+			left outer join cteFrequencyByProgram fbp on fbp.ReportFK = rc.codeReportCatalogPK
+		where ReportClass is not null
+		order by [ReportCategory]
+				,[ReportName]
 
 --	,cteFrequencyByUser
 --		as
@@ -93,7 +96,7 @@ select codeReportCatalogPK
 --		where rh.ProgramFK=@ProgramFK
 --				and UserFK=@UserName
 --		group by rh.ProgramFK, rh.ReportFK, UserLastRun
-		
+
 --		)
 --	,cteFrequencyByProgram
 --		as
@@ -105,7 +108,7 @@ select codeReportCatalogPK
 --		inner join cteLastRunByProgram lrbp on lrbp.ReportFK=rh.ReportFK
 --		where rh.ProgramFK=@ProgramFK
 --		group by rh.ProgramFK, rh.ReportFK, ProgramLastRun
-		
+
 --		)
 --select codeReportCatalogPK
 --		,isnull(UserRank,0) as UserRank
@@ -123,5 +126,4 @@ select codeReportCatalogPK
 --				convert(varchar(5),isnull(ProgramRank,0)) + ' (' +  convert(varchar(5),isnull(ProgramCount,0)) + ')' 
 --		end as ProgramRank
 end
-
 GO
