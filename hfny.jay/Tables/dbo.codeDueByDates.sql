6b644679d411735aec1e48977ceab7f7c077d0da
Devinder Singh Khalsa <dkhalsa@albany.edu>
2013-07-10 19:07:13 +0000

    Added a new column Frequency

--- hfny.jay/Tables/dbo.codeDueByDates.sql
+++ hfny.jay/Tables/dbo.codeDueByDates.sql
@@ -6,8 +6,9 @@ CREATE TABLE [dbo].[codeDueByDates]
 [Interval] [char] (2) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [MaximumDue] [int] NOT NULL,
 [MinimumDue] [int] NOT NULL,
-[ScheduledEvent] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+[ScheduledEvent] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Frequency] [int] NOT NULL
 ) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[codeDueByDates] ADD CONSTRAINT [PK__codeDueB__C38D5ED933D4B598] PRIMARY KEY CLUSTERED  ([codeDueByDatesPK]) ON [PRIMARY]
+ALTER TABLE [dbo].[codeDueByDates] ADD 
+CONSTRAINT [PK__codeDueB__C38D5ED933D4B598] PRIMARY KEY CLUSTERED  ([codeDueByDatesPK]) ON [PRIMARY]
 GO
