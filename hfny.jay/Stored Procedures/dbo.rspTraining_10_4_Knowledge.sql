19a6432e385319ebf51d779dd933f8c0bdb3a4ce
Chris Papas <cpapas@albany.edu>
2013-08-14 16:24:31 +0000

    Training 10_4 Knowledge report

---
+++ hfny.jay/Stored Procedures/dbo.rspTraining_10_4_Knowledge.sql
@@ -0,0 +1,262 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		Chris Papas
+-- Create date: 08/08/2013
+-- Description:	Training [NYS 4] Knowledge Training by 6 Months
+-- EXEC rspTraining_10_2_Orientation @progfk = 30, @sdate = '07/01/2008'
+-- =============================================
+CREATE PROCEDURE [dbo].[rspTraining_10_4_Knowledge]
+	-- Add the parameters for the stored procedure here
+	@progfk AS INT,
+	@sdate AS date
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+;WITH  cteMain AS (
+	SELECT w.WorkerPK
+		, RTRIM(w.FirstName) + ' ' + RTRIM(w.LastName) AS WorkerName
+		, wp.HireDate
+		, wp.Supervisor
+		, wp.FSW
+		, wp.FAW
+		, wp.ProgramManager
+		, wp.FatherAdvocate
+		, ROW_NUMBER() OVER(ORDER BY w.workerpk DESC) AS 'RowNumber'
+	FROM Worker w 
+	INNER JOIN WorkerProgram wp ON w.WorkerPK = wp.WorkerFK
+	WHERE wp.HireDate > @sdate
+	AND ((wp.FAW = 1 AND wp.FAWEndDate IS NULL)
+	OR (wp.FSW = 1 AND wp.FSWEndDate IS NULL)
+	OR (wp.Supervisor =1 AND wp.SupervisorEndDate IS NULL))
+	AND wp.TerminationDate IS NULL
+	AND wp.ProgramFK=@progfk
+	
+)
+
+
+--GetAll TrainingCodes/subtopics required for this report
+, cteCodesSubtopics AS (
+SELECT [TopicName]
+      ,[TopicCode]
+      ,[SATInterval]
+      ,[SATName]
+      ,[SATReqBy]
+      ,[SubTopicPK]
+      ,[ProgramFK]
+      ,[RequiredBy]
+      ,[SATFK]
+      ,[SubTopicCode]
+      ,[SubTopicName]
+      ,[TopicFK]
+  FROM codeTopic
+  INNER JOIN subtopic on subtopic.topicfk=codetopic.codetopicPK
+  where topiccode between 14.0 and 19.0 AND requiredby='HFA'
+  )
+  
+, cteWorkersTopics AS (
+	 Select workerpk
+		, WorkerName
+		, Supervisor
+		, cteMain.HireDate
+		, FSW
+		, FAW
+		, ProgramManager
+		, FatherAdvocate
+		, RowNumber
+		, [TopicName]
+		, [TopicCode]
+		, [SATName]
+		, [SubTopicCode]
+		, [SubTopicName]
+	  from cteMain, cteCodesSubtopics
+ )
+ 
+ 
+--Now we get the trainings (or lack thereof) for topic code 1.0
+, cte10_4a AS (
+	SELECT workerfk
+		, t1.TopicCode
+		, t1.topicname
+		, s.subtopiccode
+		, MIN(trainingdate) AS TrainingDate
+		, cteMain.HireDate
+	FROM TrainingAttendee ta 
+			INNER JOIN Training t ON t.TrainingPK = ta.TrainingFK
+			INNER JOIN TrainingDetail td ON td.TrainingFK = t.TrainingPK
+			INNER JOIN codeTopic t1 ON td.TopicFK=t1.codeTopicPK
+			INNER JOIN Subtopic s ON s.TopicFK=t1.codeTopicPK AND s.SubTopicPK=td.SubTopicFK
+			INNER JOIN cteMain on cteMain.WorkerPK = ta.workerfk
+	WHERE t1.TopicCode between 14.0 and 19.0 AND requiredby='HFA'
+	GROUP BY  workerfk
+			, t1.TopicCode
+			, t1.topicname
+			, HireDate
+			, s.subtopiccode
+
+)
+
+, cteAddMissingWorkers AS (
+	--if a worker has NO trainings, they won't appear at all, so add them back
+		SELECT DISTINCT  workerfk
+		, TrainingDate
+		, workerpk
+		, WorkerName
+		, cteWorkersTopics.HireDate
+		, Supervisor
+		, FSW
+		, FAW
+		, ProgramManager
+		, FatherAdvocate
+		, RowNumber
+		, cteWorkersTopics.[TopicName]
+		, cteWorkersTopics.[TopicCode]
+		, [SATName]
+		, cteWorkersTopics.[SubTopicCode]
+		, [SubTopicName]
+		FROM cte10_4a
+		right JOIN cteWorkersTopics ON cteWorkersTopics.workerpk = cte10_4a.workerfk 
+		AND cte10_4a.TopicCode = cteWorkersTopics.TopicCode AND cte10_4a.SubTopicCode = cteWorkersTopics.SubTopicCode
+		)
+
+
+, cteMeetTarget AS (
+	SELECT MAX(RowNumber) OVER(PARTITION BY TopicCode) as TotalWorkers
+	, cteAddMissingWorkers.WorkerPK
+	, WorkerName
+	, HireDate
+	, Supervisor
+	, FSW
+	, FAW
+	, ProgramManager
+	, FatherAdvocate
+	, TopicCode
+	, subtopiccode
+	, TrainingDate
+	, CASE WHEN TrainingDate IS NOT NULL THEN 1 END AS ContentCompleted
+	, CASE WHEN TrainingDate <= dateadd(day, 183, HireDate) THEN 1 ELSE 0 END AS 'Meets Target'
+	FROM cteAddMissingWorkers
+	GROUP BY WorkerPK
+	, WorkerName
+	, HireDate
+	, Supervisor
+	, FSW
+	, FAW
+	, ProgramManager
+	, FatherAdvocate
+	, TopicCode
+	, TopicName
+	, subtopiccode
+	, TrainingDate
+	, rownumber
+)
+
+--Now calculate the number meeting count, by currentrole
+, cteCountMeeting AS (
+		SELECT TopicCode, subtopiccode, workerpk, count(*) OVER (PARTITION BY TopicCode, subtopiccode) AS totalmeetingcount
+		FROM cteMeetTarget
+		WHERE [Meets Target]=1
+		GROUP BY TopicCode, subtopiccode, workerpk
+)
+
+--SELECT * FROM cteCountMeeting ORDER BY workerpk, topiccode, subtopiccode
+, cteAlmostFinal AS (
+		SELECT TotalWorkers
+		, cteMeetTarget.WorkerPK
+		, WorkerName
+		, Supervisor
+		, FSW
+		, FAW
+		, ProgramManager
+		, FatherAdvocate
+		, cteMeetTarget.topiccode
+		, cteMeetTarget.subtopiccode
+		, SUM(ContentCompleted) OVER (PARTITION BY cteMeetTarget.Workerpk, cteMeetTarget.TopicCode) AS ContentCompleted	
+		, SUM([Meets Target]) OVER (PARTITION BY cteMeetTarget.Workerpk, cteMeetTarget.TopicCode) AS CAMeetingTarget	
+		, CASE WHEN cteMeetTarget.TopicCode = 14.0 THEN '10-4a. Staff (assessment workers, home visitors and supervisors) are oriented to their roles as they relate to the programs goals, services policies and operating procedures and philosophy of home visiting/family support prior to direct work with children and families' 
+			WHEN cteMeetTarget.TopicCode = 15.0 THEN '10-4b. Staff (assessment workers, home visitors and supervisors) are oriented to the programs relationship with other community resources prior to direct work with children and families'  
+			WHEN cteMeetTarget.TopicCode = 16.0 THEN '10-4c. Staff (assessment workers, home visitors and supervisors) are oriented to child abuse and neglect indicators and reporting requirements prior to direct work with children and families' 
+			WHEN cteMeetTarget.TopicCode = 17.0 THEN '10-4d. Staff (assessment workers, home visitors and supervisors) are oriented to issues of confidentiality prior to direct work with children and families' 
+			WHEN cteMeetTarget.TopicCode = 18.0 THEN '10-4e. Staff (assessment workers, home visitors and supervisors) are oriented to issues related to boundaries prior to direct work with children and families' 
+			WHEN cteMeetTarget.TopicCode = 19.0 THEN '10-4f. Staff (assessment workers, home visitors and supervisors) are oriented to issues related to boundaries prior to direct work with children and families' 
+			END AS TopicName
+		, TrainingDate
+		, HireDate
+		, [Meets Target]
+		, TotalMeetingCount
+		, COUNT(TotalMeetingCount) OVER (PARTITION BY cteMeetTarget.TopicCode) AS MeetingCountCalculated
+		, CONVERT(VARCHAR(MAX), CONVERT(INT,100*(CAST(MAX(TotalMeetingCount) OVER (PARTITION BY cteMeetTarget.TopicCode, cteMeetTarget.SubTopicCode) AS decimal(10,2)) / CAST(TotalWorkers AS decimal(10,2)))))+ '%'  AS MeetingPercent
+		, CASE WHEN 
+				CAST(SUM([Meets Target]) OVER (PARTITION BY cteMeetTarget.WorkerPK, cteMeetTarget.TopicCode) AS decimal(10,2))
+					/ CAST(COUNT([Meets Target]) OVER (PARTITION BY cteMeetTarget.WorkerPK, cteMeetTarget.TopicCode) AS decimal(10,2))
+				= 1 THEN 1
+			END AS MeetsTargetForAll
+		, CASE WHEN 
+				CAST(SUM([Meets Target]) OVER (PARTITION BY cteMeetTarget.WorkerPK, cteMeetTarget.TopicCode) AS decimal(10,2))
+					/ CAST(COUNT([Meets Target]) OVER (PARTITION BY cteMeetTarget.WorkerPK, cteMeetTarget.TopicCode) AS decimal(10,2))
+				BETWEEN .5 AND .99 THEN 1
+			END AS MeetsTargetForMajority
+		, CAST(COUNT([Meets Target]) OVER (PARTITION BY cteMeetTarget.WorkerPK, cteMeetTarget.TopicCode) AS INT) AS TotalContentAreasByTopicAndWorker
+		FROM cteMeetTarget
+		LEFT JOIN cteCountMeeting ON cteCountMeeting.TopicCode = cteMeetTarget.TopicCode AND ctecountmeeting.subtopiccode=cteMeetTarget.subtopiccode AND ctecountmeeting.workerpk=cteMeetTarget.workerpk
+		GROUP BY TotalWorkers
+		, cteMeetTarget.WorkerPK
+		, WorkerName
+		, Supervisor
+		, FSW
+		, FAW
+		, ProgramManager
+		, FatherAdvocate
+		, cteMeetTarget.topiccode
+		, cteMeetTarget.subtopiccode
+		, cteMeetTarget.ContentCompleted
+		, cteMeetTarget.[Meets Target]
+		, cteMeetTarget.TrainingDate
+		, cteMeetTarget.Hiredate
+		, cteCountMeeting.totalmeetingcount
+)
+
+SELECT TotalWorkers
+		, WorkerPK
+		, WorkerName
+		, Supervisor
+		, FSW
+		, FAW
+		, ProgramManager
+		, FatherAdvocate
+		, topiccode
+		, subtopiccode
+		, ContentCompleted	
+		, CAMeetingTarget	
+		, TopicName
+		, TrainingDate
+		, HireDate
+		, [Meets Target]
+		, TotalMeetingCount
+		, TotalContentAreasByTopicAndWorker AS SubtopicCA_PerTopic
+		, MeetingCountCalculated
+		, MeetingPercent
+		,	CASE WHEN CAMeetingTarget = TotalContentAreasByTopicAndWorker THEN '3' 
+			WHEN ContentCompleted = TotalContentAreasByTopicAndWorker THEN '2'
+			ELSE '1'
+			END AS TopicRatingByWorker
+		,	CASE WHEN SUM(MeetsTargetForAll) OVER (PARTITION BY topiccode) / TotalContentAreasByTopicAndWorker = TotalWorkers THEN '3' 
+			WHEN (SUM(MeetsTargetForAll) OVER (PARTITION BY topiccode) / TotalContentAreasByTopicAndWorker) + (SUM(MeetsTargetForMajority) OVER (PARTITION BY topiccode) / TotalContentAreasByTopicAndWorker) = TotalWorkers THEN '2'
+			ELSE '1'
+			END AS TopicRatingBySite
+		, CASE WHEN SUM(MeetsTargetForAll) OVER (PARTITION BY topiccode) / TotalContentAreasByTopicAndWorker > .9 THEN SUM(MeetsTargetForAll) OVER (PARTITION BY topiccode) / TotalContentAreasByTopicAndWorker
+		  ELSE 0
+		  END AS TotalMeetsTargetForAll
+		, CASE WHEN SUM(MeetsTargetForMajority) OVER (PARTITION BY topiccode) / TotalContentAreasByTopicAndWorker > .9 THEN SUM(MeetsTargetForMajority) OVER (PARTITION BY topiccode) / TotalContentAreasByTopicAndWorker
+		  ELSE 0
+		  END AS TotalMeetsTargetForMajority
+		FROM cteAlmostFinal
+		ORDER BY topiccode, subtopiccode
+
+END
+GO
