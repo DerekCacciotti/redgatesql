22d479e8f4050d21c19f8fdb3d295f6b7f865afc
Jay Robohn <jrobohn@albany.edu>
2013-10-09 16:05:40 +0000

    Fix search PC routine froms New Screen - sort properly

--- hfny.jay/Stored Procedures/dbo.spSearchPC.sql
+++ hfny.jay/Stored Procedures/dbo.spSearchPC.sql
@@ -4,131 +4,188 @@ GO
 SET ANSI_NULLS ON
 GO
 
+-- exec spSearchPC	'jennifer', 'mott', '19760716', null, null, null, null, 11
 
-
-CREATE PROCEDURE [dbo].[spSearchPC] (
-	@PCFirstName VARCHAR(20) = NULL, @PCLastName VARCHAR(30) = NULL, @PCDOB DATETIME = NULL, 
-	@PCPhone VARCHAR(12) = NULL, @PCEmergencyPhone VARCHAR(12) = NULL, 
-	@Ethnicity VARCHAR(30) = NULL, @Race VARCHAR(2) = NULL, @ProgramFK INT = NULL)
-
-AS
-
-SET NOCOUNT ON
-/*
-SELECT pc.pcpk, pc.pcfirstname,pc.pclastname,pc.pcdob,
-	pc.pcphone,pc.PCEmergencyPhone,
-	CASE WHEN AppCodeText = 'Other' THEN racespecify ELSE AppCodeText END AS race,racespecify--,
-	--caseprogram.dischargedate
-FROM pc
-LEFT JOIN codeApp
-ON appcode = pc.race
-AND appcodegroup = 'Race'
---INNER JOIN hvcase
---ON pcpk IN(cpfk, pc1fk, pc2fk, obpfk)
---INNER JOIN caseprogram
---ON hvcasefk = hvcasepk
-WHERE (pc.pcfirstname LIKE @PCFirstName + '%'
-OR pc.pclastname LIKE @PCLastName + '%'
-OR pc.pcdob = @PCDOB
-OR pc.pcphone = @PCPhone
-OR pc.pcemergencyphone = @PCEmergencyPhone
-OR pc.ethnicity = @Ethnicity
-OR pc.race = @Race)
---AND ProgramFK = ISNULL(@ProgramFK, ProgramFK)
-AND pcpk IN (SELECT pcfk FROM pcprogram WHERE ProgramFK = ISNULL(@ProgramFK, ProgramFK))
-ORDER BY
-CASE WHEN pc.pcfirstname LIKE @PCFirstName + '%' THEN 1 ELSE 0 END +
-CASE WHEN pc.pclastname LIKE @PCLastName + '%' THEN 1 ELSE 0 END +
-CASE WHEN pc.pcdob = @PCDOB THEN 1 ELSE 0 END +
-CASE WHEN pc.pcphone = @PCPhone THEN 1 ELSE 0 END +
-CASE WHEN pc.pcemergencyphone = @PCEmergencyPhone THEN 1 ELSE 0 END +
-CASE WHEN pc.ethnicity = @Ethnicity THEN 1 ELSE 0 END +
-CASE WHEN pc.race = @Race THEN 1 ELSE 0 END DESC
-*/
-
-
---DECLARE @PCFirstName VARCHAR(20) = NULL
---DECLARE @PCLastName VARCHAR(30) = 'green'
---DECLARE @PCDOB DATETIME = NULL
---DECLARE @PCPhone VARCHAR(12) = NULL
---DECLARE @PCEmergencyPhone VARCHAR(12) = NULL
---DECLARE @ProgramFK INT = NULL
-
-; WITH xxx AS (
-SELECT DISTINCT pc.pcpk, pc.pcfirstname, pc.pclastname, pc.pcdob, pc.pcphone
-, pc.PCEmergencyPhone, pc.race, pc.racespecify
-FROM PC AS pc
-WHERE 
-(pc.pcfirstname LIKE @PCFirstName + '%'
-OR pc.pclastname LIKE @PCLastName + '%'
-OR pc.pcdob = @PCDOB
-OR pc.pcphone = @PCPhone
-OR pc.pcemergencyphone = @PCEmergencyPhone)
-AND pc.pcpk IN (SELECT pcfk FROM pcprogram WHERE ProgramFK = ISNULL(@ProgramFK, ProgramFK))
+CREATE procedure [dbo].[spSearchPC]
+(
+    @PCFirstName      varchar(20)    = null,
+    @PCLastName       varchar(30)    = null,
+    @PCDOB            datetime       = null,
+    @PCPhone          varchar(12)    = null,
+    @PCEmergencyPhone varchar(12)    = null,
+    @Ethnicity        varchar(30)    = null,
+    @Race             varchar(2)     = null,
+    @ProgramFK        int            = null
 )
 
-, yyyNew AS (
-SELECT b.PCPK
-, max(convert(VARCHAR(8),isnull(c.DischargeDate, getdate()),112) + cast(c.CaseProgramPK AS VARCHAR(10))) max1
-FROM HVCase AS a 
-JOIN xxx AS b ON b.PCPK = a.PC1FK
-JOIN CaseProgram AS c ON a.HVCasePK = c.HVCaseFK
-GROUP BY b.PCPK
-)
+as
 
-, yyy AS (
-SELECT a.PCPK, c.LevelName
-FROM yyyNew AS a
-JOIN CaseProgram b ON b.CaseProgramPK = cast(substring(a.max1,9,10) AS INT)
-JOIN codeLevel AS c ON c.codeLevelPK = b.CurrentLevelFK
-)
-
-, zzzPC1 AS (
-SELECT b.PCPK, count(*) [pc1]
-FROM HVCase AS a
-JOIN xxx AS b ON b.PCPK = a.pc1fk
-GROUP BY b.PCPK
-)
-
-, zzzPC2 AS (
-SELECT b.PCPK, count(*) [pc2]
-FROM HVCase AS a
-JOIN xxx AS b ON b.PCPK = a.pc2fk
-GROUP BY b.PCPK
-)
-
-, zzzOBP AS (
-SELECT b.PCPK, count(*) [obp]
-FROM HVCase AS a
-JOIN xxx AS b ON b.PCPK = a.obpfk
-GROUP BY b.PCPK
-)
-
-, qqq AS (
-SELECT 
-a.pcpk, a.pcfirstname, a.pclastname, a.pcdob, a.pcphone, a.PCEmergencyPhone
-,CASE WHEN AppCodeText = 'Other' THEN racespecify ELSE AppCodeText END AS race, a.racespecify
-, status = 
-CASE WHEN b.PCPK IS NOT NULL THEN b.LevelName ELSE '' END
-, roles = 
-CASE WHEN e.PCPK IS NOT NULL THEN 'PC1 ' ELSE '' END +
-CASE WHEN c.PCPK IS NOT NULL THEN 'PC2 ' ELSE '' END +
-CASE WHEN d.PCPK IS NOT NULL THEN 'OBP ' ELSE '' END
-FROM xxx AS a
-LEFT OUTER JOIN yyy AS b ON b.pcpk = a.pcpk
-LEFT OUTER JOIN zzzPC1 AS e ON e.pcpk = a.pcpk
-LEFT OUTER JOIN zzzPC2 AS c ON c.pcpk = a.pcpk
-LEFT OUTER JOIN zzzOBP AS d ON d.pcpk = a.pcpk
-LEFT JOIN codeApp ON appcode = a.race AND appcodegroup = 'Race'
-)
-
-select * from qqq ORDER BY pclastname, pcfirstname
+	set nocount on
+	/*
+		SELECT pc.pcpk, pc.pcfirstname,pc.pclastname,pc.pcdob,
+			pc.pcphone,pc.PCEmergencyPhone,
+			CASE WHEN AppCodeText = 'Other' THEN racespecify ELSE AppCodeText END AS race,racespecify--,
+			--caseprogram.dischargedate
+		FROM pc
+		LEFT JOIN codeApp
+		ON appcode = pc.race
+		AND appcodegroup = 'Race'
+		--INNER JOIN hvcase
+		--ON pcpk IN(cpfk, pc1fk, pc2fk, obpfk)
+		--INNER JOIN caseprogram
+		--ON hvcasefk = hvcasepk
+		WHERE (pc.pcfirstname LIKE @PCFirstName + '%'
+		OR pc.pclastname LIKE @PCLastName + '%'
+		OR pc.pcdob = @PCDOB
+		OR pc.pcphone = @PCPhone
+		OR pc.pcemergencyphone = @PCEmergencyPhone
+		OR pc.ethnicity = @Ethnicity
+		OR pc.race = @Race)
+		--AND ProgramFK = ISNULL(@ProgramFK, ProgramFK)
+		AND pcpk IN (SELECT pcfk FROM pcprogram WHERE ProgramFK = ISNULL(@ProgramFK, ProgramFK))
+		ORDER BY
+		CASE WHEN pc.pcfirstname LIKE @PCFirstName + '%' THEN 1 ELSE 0 END +
+		CASE WHEN pc.pclastname LIKE @PCLastName + '%' THEN 1 ELSE 0 END +
+		CASE WHEN pc.pcdob = @PCDOB THEN 1 ELSE 0 END +
+		CASE WHEN pc.pcphone = @PCPhone THEN 1 ELSE 0 END +
+		CASE WHEN pc.pcemergencyphone = @PCEmergencyPhone THEN 1 ELSE 0 END +
+		CASE WHEN pc.ethnicity = @Ethnicity THEN 1 ELSE 0 END +
+		CASE WHEN pc.race = @Race THEN 1 ELSE 0 END DESC
+	*/
 
 
+	--DECLARE @PCFirstName VARCHAR(20) = NULL
+	--DECLARE @PCLastName VARCHAR(30) = 'green'
+	--DECLARE @PCDOB DATETIME = NULL
+	--DECLARE @PCPhone VARCHAR(12) = NULL
+	--DECLARE @PCEmergencyPhone VARCHAR(12) = NULL
+	--DECLARE @ProgramFK INT = NULL
 
+	;
+	with xxx
+	as (
+	select distinct pc.pcpk
+				   ,pc.pcfirstname
+				   ,pc.pclastname
+				   ,pc.pcdob
+				   ,pc.pcphone
+				   ,pc.PCEmergencyPhone
+				   ,pc.race
+				   ,pc.racespecify
+		from PC as pc
+		where
+			 (pc.pcfirstname like @PCFirstName+'%'
+			 or pc.pclastname like @PCLastName+'%'
+			 or pc.pcdob = @PCDOB
+			 or pc.pcphone = @PCPhone
+			 or pc.pcemergencyphone = @PCEmergencyPhone)
+			 and pc.pcpk in (select pcfk
+								 from pcprogram
+								 where ProgramFK = isnull(@ProgramFK,ProgramFK))
+	)
 
+	,
+	yyyNew
+	as (
+	select b.PCPK
+		  ,max(convert(varchar(8),isnull(c.DischargeDate,getdate()),112)+cast(c.CaseProgramPK as varchar(10))) max1
+		from HVCase as a
+			join xxx as b on b.PCPK = a.PC1FK
+			join CaseProgram as c on a.HVCasePK = c.HVCaseFK
+		group by b.PCPK
+	)
 
+	,
+	yyy
+	as (
+	select a.PCPK
+		  ,c.LevelName
+		from yyyNew as a
+			join CaseProgram b on b.CaseProgramPK = cast(substring(a.max1,9,10) as int)
+			join codeLevel as c on c.codeLevelPK = b.CurrentLevelFK
+	)
 
+	,
+	zzzPC1
+	as (
+	select b.PCPK
+		  ,count(*) [pc1]
+		from HVCase as a
+			join xxx as b on b.PCPK = a.pc1fk
+		group by b.PCPK
+	)
 
+	,
+	zzzPC2
+	as (
+	select b.PCPK
+		  ,count(*) [pc2]
+		from HVCase as a
+			join xxx as b on b.PCPK = a.pc2fk
+		group by b.PCPK
+	)
 
+	,
+	zzzOBP
+	as (
+	select b.PCPK
+		  ,count(*) [obp]
+		from HVCase as a
+			join xxx as b on b.PCPK = a.obpfk
+		group by b.PCPK
+	)
+
+	,
+	qqq
+	as (
+	select
+		  a.pcpk
+		 ,a.pcfirstname
+		 ,a.pclastname
+		 ,a.pcdob
+		 ,a.pcphone
+		 ,a.PCEmergencyPhone
+		 ,case when AppCodeText = 'Other' then racespecify else AppCodeText end as race
+		 ,a.racespecify
+		 ,status =
+		  case when b.PCPK is not null then b.LevelName else '' end
+		 ,roles =
+		  case when e.PCPK is not null then 'PC1 ' else '' end+
+		  case when c.PCPK is not null then 'PC2 ' else '' end+
+		  case when d.PCPK is not null then 'OBP ' else '' end
+		from xxx as a
+			left outer join yyy as b on b.pcpk = a.pcpk
+			left outer join zzzPC1 as e on e.pcpk = a.pcpk
+			left outer join zzzPC2 as c on c.pcpk = a.pcpk
+			left outer join zzzOBP as d on d.pcpk = a.pcpk
+			left join codeApp on appcode = a.race and appcodegroup = 'Race'
+	)
+
+	select top 100 pcpk	
+					, pcfirstname
+					, pclastname
+					, pcdob
+					, pcphone
+					, PCEmergencyPhone
+					, race
+					, racespecify
+					, status
+					, roles
+					, orderColumn = case when pcfirstname like @PCFirstName+'%' then 1 else 0 end+
+									case when pclastname like @PCLastName+'%' then 1 else 0 end+
+									case when pcdob = @PCDOB then 1 else 0 end+
+									case when pcphone = @PCPhone then 1 else 0 end+
+									case when pcemergencyphone = @PCEmergencyPhone then 1 else 0 end
+		from qqq
+		order by case when pcfirstname like @PCFirstName+'%' then 1 else 0 end+
+				case when pclastname like @PCLastName+'%' then 1 else 0 end+
+				case when pcdob = @PCDOB then 1 else 0 end+
+				case when pcphone = @PCPhone then 1 else 0 end+
+				case when pcemergencyphone = @PCEmergencyPhone then 1 else 0 end desc
+				, pclastname
+				, pcfirstname
+				--case when ethnicity = @Ethnicity then 1 else 0 end+
+				--case when race = @Race then 1 else 0 end desc
+--order by pclastname
+--		,pcfirstname
 GO
