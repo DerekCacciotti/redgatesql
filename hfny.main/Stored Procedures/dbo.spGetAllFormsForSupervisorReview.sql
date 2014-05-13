9f9e4b7f7b9818dac2402dba3e335b43439e2110
Jay Robohn <jayrobot@gmail.com>
2014-05-13 14:16:26 -0400

    Add Trainings and Supervisions to Form Review for Dashboard

--- hfny.main/Stored Procedures/dbo.spGetAllFormsForSupervisorReview.sql
+++ hfny.main/Stored Procedures/dbo.spGetAllFormsForSupervisorReview.sql
@@ -26,7 +26,6 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-
 	with cteSupervisors	as 
 	(
 		select ltrim(rtrim(LastName)) + ', ' + ltrim(rtrim(FirstName)) as WorkerName
@@ -43,105 +42,166 @@ BEGIN
 		--								, @EventDate = null
 		--								, @WorkerType = 'SUP'
 		--								, @AllWorkers = 0
-	)
-	--,
-	--cteHomeVisitLogRow
-	
-	select FormReviewPK
-		  ,PC1ID
-		  ,codeFormName
-		  ,convert(varchar(10),FormDate,101) as FormDate
-		  ,FormFK
-		  ,FormReviewCreateDate
-		  ,FormReviewCreator
-		  ,FormReviewEditDate
-		  ,FormReviewEditor
-		  ,fr.FormType
-		  ,fr.HVCaseFK
-		  ,fr.ProgramFK
-		  ,ReviewDateTime
-		  ,ReviewedBy
-		  ,FormReviewStartDate
-		  ,FormReviewEndDate
-		  ,'CaseHome.aspx?pc1id='+PC1ID as CaseHomeLink
-		  ,
-				 case 
-				  -- Handling some special url redirections 
-				  when fr.FormType='PA' then 'preassessment.aspx?pc1id='+PC1ID+ '&papk=' + convert(varchar,FormFK)		  -- Note: here we use preassessment.aspx
-				  when fr.FormType='PI' then 'PreIntake.aspx?pc1id='+PC1ID+ '&pipk=' + convert(varchar,FormFK)		  -- Note: here we use PreIntakes.aspx
-				  when fr.FormType='SR' then 'ServiceReferral.aspx?pc1id='+PC1ID+ '&srpk=' + convert(varchar,FormFK)		  -- Note: here we use ServiceReferral.aspx
-				  when fr.FormType='VL' then 'HomeVisitLog.aspx?pc1id='+PC1ID+ '&hvlogpk=' + convert(varchar,FormFK)		  -- Note: here we use HomeVisitLogs.aspx
-				  when fr.FormType='IN' then 'Intake.aspx?pc1id='+PC1ID+ '&ipk=' + convert(varchar,FormFK)		  -- Note: here we use Intake.aspx
-				  
-				  
-				  when fr.FormType='DS' then 'PreDischarge.aspx?pc1id='+PC1ID	-- Note: here we use PreDischarge.aspx
-				  when fr.FormType='TM' then 'SelectTC.aspx?pc1id='+PC1ID+ '&form=TCMedical'	  -- only three forms use SelectTC.aspx
-				  when fr.FormType='AS' then 'SelectTC.aspx?pc1id='+PC1ID+ '&form=ASQSE'		  -- only three forms use SelectTC.aspx			  
-				  when fr.FormType='AQ' then 'SelectTC.aspx?pc1id='+PC1ID+ '&form=ASQ1'			  -- only three forms use SelectTC.aspx
-				  when fr.FormType='PS' then 'PSI.aspx?pc1id='+PC1ID		  -- Note: here we use PSI.aspx
-				  when fr.FormType='FU' then 'FollowUps.aspx?pc1id='+PC1ID		  -- Note: here we use FollowUps.aspx
-				  
-				  when fr.FormType='SC' then 'HVScreen.aspx?pc1id='+PC1ID+ '&hvscreenpk=' + convert(varchar,FormFK)		  
-				  when fr.FormType='KE' then 'Kempe.aspx?pc1id='+PC1ID+ '&kempepk=' + convert(varchar,FormFK)		  
-				  
-				  when fr.FormType='TC' then 'TCIDs.aspx?pc1id='+PC1ID		  -- Note: here we use TCIDs.aspx
+	),
+	cteFormReview as 
+	(	
+		select FormReviewPK
+			  ,PC1ID
+			  ,codeFormName
+			  ,convert(varchar(10),FormDate,101) as FormDate
+			  ,FormFK
+			  ,FormReviewCreateDate
+			  ,FormReviewCreator
+			  ,FormReviewEditDate
+			  ,FormReviewEditor
+			  ,fr.FormType
+			  ,fr.HVCaseFK
+			  ,fr.ProgramFK
+			  ,ReviewDateTime
+			  ,ReviewedBy
+			  ,FormReviewStartDate
+			  ,FormReviewEndDate
+			  ,'CaseHome.aspx?pc1id='+PC1ID as CaseHomeLink
+			  ,case 
+					-- Handling some special url redirections 
+					when fr.FormType='PA' then 'preassessment.aspx?pc1id='+PC1ID+ '&papk=' + convert(varchar,FormFK)		  -- Note: here we use preassessment.aspx
+					when fr.FormType='PI' then 'PreIntake.aspx?pc1id='+PC1ID+ '&pipk=' + convert(varchar,FormFK)		  -- Note: here we use PreIntakes.aspx
+					when fr.FormType='SR' then 'ServiceReferral.aspx?pc1id='+PC1ID+ '&srpk=' + convert(varchar,FormFK)		  -- Note: here we use ServiceReferral.aspx
+					when fr.FormType='VL' then 'HomeVisitLog.aspx?pc1id='+PC1ID+ '&hvlogpk=' + convert(varchar,FormFK)		  -- Note: here we use HomeVisitLogs.aspx
+					when fr.FormType='IN' then 'Intake.aspx?pc1id='+PC1ID+ '&ipk=' + convert(varchar,FormFK)		  -- Note: here we use Intake.aspx
+					when fr.FormType='DS' then 'PreDischarge.aspx?pc1id='+PC1ID	-- Note: here we use PreDischarge.aspx
+					when fr.FormType='TM' then 'SelectTC.aspx?pc1id='+PC1ID+ '&form=TCMedical'	  -- only three forms use SelectTC.aspx
+					when fr.FormType='AS' then 'SelectTC.aspx?pc1id='+PC1ID+ '&form=ASQSE'		  -- only three forms use SelectTC.aspx			  
+					when fr.FormType='AQ' then 'SelectTC.aspx?pc1id='+PC1ID+ '&form=ASQ1'			  -- only three forms use SelectTC.aspx
+					when fr.FormType='PS' then 'PSI.aspx?pc1id='+PC1ID		  -- Note: here we use PSI.aspx
+					when fr.FormType='FU' then 'FollowUps.aspx?pc1id='+PC1ID		  -- Note: here we use FollowUps.aspx
+					when fr.FormType='SC' then 'HVScreen.aspx?pc1id='+PC1ID+ '&hvscreenpk=' + convert(varchar,FormFK)		  
+					when fr.FormType='KE' then 'Kempe.aspx?pc1id='+PC1ID+ '&kempepk=' + convert(varchar,FormFK)		  
+					when fr.FormType='TC' then 'TCIDs.aspx?pc1id='+PC1ID		  -- Note: here we use TCIDs.aspx
+				else
+					ltrim(rtrim(replace(replace(codeFormName,'-',''),' ','')))+'.aspx?pc1id='+PC1ID
+				end as FormLink
 
-	  
-				  else
-					  ltrim(rtrim(replace(replace(codeFormName,'-',''),' ','')))+'.aspx?pc1id='+PC1ID
-				  end as FormLink
-				  	  
+			  ,isnull(FormReviewEndDate, current_timestamp) as EffectiveEndDate
+			  ,dateadd(day, @DaysToLoad*-1, isnull(FormReviewEndDate, current_timestamp)) as EffectiveStartDate
+			  ,ltrim(rtrim(LastName)) + ', ' + ltrim(rtrim(FirstName)) as WorkerName
+			  ,sup.WorkerName as SupervisorName
+			  
+		from FormReview fr
+		inner join FormReviewOptions fro on fro.FormType = fr.FormType and fro.ProgramFK = isnull(@ProgramFK,fro.ProgramFK)
+		inner join codeForm f on codeFormAbbreviation = fr.FormType
+		inner join CaseProgram cp on cp.HVCaseFK = fr.HVCaseFK
+									and cp.ProgramFK = fr.ProgramFK
 
-		  ,isnull(FormReviewEndDate, current_timestamp) as EffectiveEndDate
-		  ,dateadd(day, @DaysToLoad*-1, isnull(FormReviewEndDate, current_timestamp)) as EffectiveStartDate
-		  ,ltrim(rtrim(LastName)) + ', ' + ltrim(rtrim(FirstName)) as WorkerName
-		  ,sup.WorkerName as SupervisorName
-		  
-	from FormReview fr
-	inner join FormReviewOptions fro on fro.FormType = fr.FormType and fro.ProgramFK = isnull(@ProgramFK,fro.ProgramFK)
-	inner join codeForm f on codeFormAbbreviation = fr.FormType
-	inner join CaseProgram cp on cp.HVCaseFK = fr.HVCaseFK
-								and cp.ProgramFK = fr.ProgramFK
-								
 	--inner join HVScreen hv on hv.HVCaseFK = fr.HVCaseFK
 	--inner join Kempe k on k.HVCaseFK = fr.HVCaseFK	
 	--left join cteLastPreIntake p on p.HVCaseFK = fr.HVCaseFK  -- must use Left join. For sure PreIntake forms, if any, will have hvcasefk
 	--left join cteLastServiceReferral sr on sr.HVCaseFK = fr.HVCaseFK  -- must use Left join. For sure ServiceReferral forms, if any, will have hvcasefk
-			
-								
-	inner join WorkerProgram wp on wp.WorkerFK = cp.CurrentFSWFK
-	inner join Worker w on w.WorkerPK = wp.WorkerFK
-	inner join cteSupervisors sup on wp.SupervisorFK = sup.WorkerPK
-	where fr.ProgramFK = isnull(@ProgramFK, fr.ProgramFK)	
-			and ReviewedBy is null
-			and FormDate between FormReviewStartDate and isnull(FormReviewEndDate, current_timestamp)
-			and FormDate between dateadd(day, @DaysToLoad*-1, isnull(FormReviewEndDate, current_timestamp)) and isnull(FormReviewEndDate, current_timestamp) 
+
+
+		inner join WorkerProgram wp on wp.WorkerFK = cp.CurrentFSWFK
+		inner join Worker w on w.WorkerPK = wp.WorkerFK
+		inner join cteSupervisors sup on wp.SupervisorFK = sup.WorkerPK
+		where fr.ProgramFK = isnull(@ProgramFK, fr.ProgramFK)	
+				and ReviewedBy is null
+				and FormDate between FormReviewStartDate and isnull(FormReviewEndDate, current_timestamp)
+				and FormDate between dateadd(day, @DaysToLoad*-1, isnull(FormReviewEndDate, current_timestamp)) and isnull(FormReviewEndDate, current_timestamp) 
+		union all 
+		select FormReviewPK
+			  ,left(TrainingTitle,12)+'…' as PC1ID
+			  ,codeFormName
+			  ,convert(varchar(10),FormDate,101) as FormDate
+			  ,FormFK
+			  ,FormReviewCreateDate
+			  ,FormReviewCreator
+			  ,FormReviewEditDate
+			  ,FormReviewEditor
+			  ,fr.FormType
+			  ,null
+			  ,fr.ProgramFK
+			  ,ReviewDateTime
+			  ,ReviewedBy
+			  ,FormReviewStartDate
+			  ,FormReviewEndDate
+			  ,null as CaseHomeLink
+			  ,'Training.aspx?TrainingPK='+rtrim(cast(TrainingPK as varchar(12))) as FormLink
+			  ,isnull(FormReviewEndDate, current_timestamp) as EffectiveEndDate
+			  ,dateadd(day, @DaysToLoad*-1, isnull(FormReviewEndDate, current_timestamp)) as EffectiveStartDate
+			  ,null as WorkerName
+			  ,null as SupervisorName
+		from FormReview fr
+		inner join FormReviewOptions fro on fro.FormType = fr.FormType and fro.ProgramFK = isnull(@ProgramFK,fro.ProgramFK)
+		inner join codeForm f on codeFormAbbreviation = fr.FormType
+		inner join Training t on TrainingPK = FormFK and fr.FormType = 'TR' 
+		where fr.ProgramFK = isnull(@ProgramFK, fr.ProgramFK)	
+				and ReviewedBy is null
+				and FormDate between FormReviewStartDate and isnull(FormReviewEndDate, current_timestamp)
+				and FormDate between dateadd(day, @DaysToLoad*-1, isnull(FormReviewEndDate, current_timestamp)) and isnull(FormReviewEndDate, current_timestamp) 
+		union all 
+		select FormReviewPK
+			  ,null as PC1ID
+			  ,codeFormName
+			  ,convert(varchar(10),FormDate,101) as FormDate
+			  ,FormFK
+			  ,FormReviewCreateDate
+			  ,FormReviewCreator
+			  ,FormReviewEditDate
+			  ,FormReviewEditor
+			  ,fr.FormType
+			  ,null
+			  ,fr.ProgramFK
+			  ,ReviewDateTime
+			  ,ReviewedBy
+			  ,FormReviewStartDate
+			  ,FormReviewEndDate
+			  ,null as CaseHomeLink
+			  ,'Supervision.aspx?AddSupervision=0&SupervisionPK=' + rtrim(cast(SupervisionPK as varchar(12))) + 
+				'&WorkerFK=' + rtrim(cast(s.WorkerFK as varchar(12))) + 
+				'&SupervisorFK=' + rtrim(cast(s.SupervisorFK as varchar(12))) as FormLink
+			  ,isnull(FormReviewEndDate, current_timestamp) as EffectiveEndDate
+			  ,dateadd(day, @DaysToLoad*-1, isnull(FormReviewEndDate, current_timestamp)) as EffectiveStartDate
+			  ,ltrim(rtrim(w.LastName)) + ', ' + ltrim(rtrim(w.FirstName)) as WorkerName
+			  ,ltrim(rtrim(sups.LastName)) + ', ' + ltrim(rtrim(sups.FirstName)) as SupervisorName
+		from FormReview fr
+		inner join FormReviewOptions fro on fro.FormType = fr.FormType and fro.ProgramFK = isnull(@ProgramFK,fro.ProgramFK)
+		inner join codeForm f on codeFormAbbreviation = fr.FormType
+		inner join Supervision s on SupervisionPK = FormFK and fr.FormType = 'SU'
+		inner join WorkerProgram wp on wp.WorkerFK = s.WorkerFK
+		inner join Worker w on w.WorkerPK = s.WorkerFK
+		inner join Worker sups on sups.WorkerPK = s.SupervisorFK
+		where fr.ProgramFK = isnull(@ProgramFK, fr.ProgramFK)	
+				and ReviewedBy is null
+				and FormDate between FormReviewStartDate and isnull(FormReviewEndDate, current_timestamp)
+				and FormDate between dateadd(day, @DaysToLoad*-1, isnull(FormReviewEndDate, current_timestamp)) and isnull(FormReviewEndDate, current_timestamp) 
+	)
+	
+	select * from cteFormReview
 	order by 
-	case when fr.FormType='SC' then 1 
-				  when fr.FormType='PA' then 2
-				  when fr.FormType='KE' then 3
-				  when fr.FormType='PI' then 4
-				  when fr.FormType='SR' then 5
-				  when fr.FormType='ID' then 6
-				  when fr.FormType='VL' then 7
-				  when fr.FormType='IN' then 8
-				  when fr.FormType='LV' then 9
-				  when fr.FormType='TC' then 10
-				  when fr.FormType='PM' then 11
-				  when fr.FormType='DS' then 12
-				  when fr.FormType='FF' then 13
-				  when fr.FormType='TM' then 14
-				  when fr.FormType='AQ' then 15
-				  when fr.FormType='AS' then 16
-				  when fr.FormType='PS' then 17
-				  when fr.FormType='FU' then 18
-				  when fr.FormType='SU' then 19
-				  when fr.FormType='TR' then 20
-				  when fr.FormType='GR' then 21
+	case when FormType='SC' then 1 
+				  when FormType='PA' then 2
+				  when FormType='KE' then 3
+				  when FormType='PI' then 4
+				  when FormType='SR' then 5
+				  when FormType='ID' then 6
+				  when FormType='VL' then 7
+				  when FormType='IN' then 8
+				  when FormType='LV' then 9
+				  when FormType='TC' then 10
+				  when FormType='PM' then 11
+				  when FormType='DS' then 12
+				  when FormType='FF' then 13
+				  when FormType='TM' then 14
+				  when FormType='AQ' then 15
+				  when FormType='AS' then 16
+				  when FormType='PS' then 17
+				  when FormType='FU' then 18
+				  when FormType='SU' then 19
+				  when FormType='TR' then 20
+				  when FormType='GR' then 21
 			end
-			,
-			PC1ID
-			,FormDate
+			--,
+			--PC1ID
+			--,FormDate
 				  --when fr.FormType='AQ' then 15
 				  --when fr.FormType='AS' then 16
 				  --when fr.FormType='DS' then 12
@@ -163,5 +223,5 @@ BEGIN
 				  --when fr.FormType='TM' then 14
 				  --when fr.FormType='TR' then 20
 				  --when fr.FormType='VL' then 7	
-END
+end
 GO
