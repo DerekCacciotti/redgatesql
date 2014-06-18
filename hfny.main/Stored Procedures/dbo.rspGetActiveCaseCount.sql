eaa271ae7db0a1f355163766a6e0dd164d290fb3
cpapas <cpapas@albany.edu>
2014-06-18 15:13:14 -0400

    Generic Case Count to limit new Screens when Active Cases exceed a specified value in the code

---
+++ hfny.main/Stored Procedures/dbo.rspGetActiveCaseCount.sql
@@ -0,0 +1,29 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+-- =============================================
+-- Author:		Chris Papas
+-- Create date: 06/18/2014
+-- Description:	Get everyone who's Active
+-- Edit date: 10/11/2013 CP - workerprogram was duplicating cases when worker transferred
+-- =============================================
+CREATE procedure [dbo].[rspGetActiveCaseCount]
+(
+    @programfk int    = null
+)
+
+as
+begin
+
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	set nocount on;
+
+	select COUNT(PC1ID) AS theCount FROM dbo.CaseProgram
+	WHERE ProgramFK = @programfk
+	AND DischargeDate IS NULL
+	
+end
+GO
