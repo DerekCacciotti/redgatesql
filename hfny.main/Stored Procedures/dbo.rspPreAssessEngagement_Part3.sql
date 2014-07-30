b2971b9fe711ccd26b1aa3a9b656f7e4a060b442
Jay Robohn <jayrobot@gmail.com>
2014-07-30 09:10:15 -0400

    Pre-Assessment Engagement - reformatted and fixed bug which was including closed cases

--- hfny.main/Stored Procedures/dbo.rspPreAssessEngagement_Part3.sql
+++ hfny.main/Stored Procedures/dbo.rspPreAssessEngagement_Part3.sql
@@ -11,104 +11,117 @@ GO
 -- =============================================
 CREATE procedure [dbo].[rspPreAssessEngagement_Part3]
 (
-    @programfk    VARCHAR(MAX) = null,
-    @StartDtT     DATETIME = NULL,
-    @StartDt      DATETIME = null,
-    @EndDt        DATETIME = null
+    @programfk varchar(max)    = null,
+    @StartDtT  datetime        = null,
+    @StartDt   datetime        = null,
+    @EndDt     datetime        = null
 )
 as
-
-if @programfk is null
+	if @programfk is null
 	begin
 		select @programfk = substring((select ','+ltrim(rtrim(str(HVProgramPK)))
 										   from HVProgram
 										   for xml path ('')),2,8000)
 	end
-set @programfk = replace(@programfk,'"','')
-	
---DECLARE @StartDtT DATE = '09/01/2012'
---DECLARE @StartDt DATE = '09/01/2012'
---DECLARE @EndDt DATE = '11/30/2012'
---DECLARE @programfk INT = 4
+	set @programfk = replace(@programfk,'"','')
 
--- no status pre-assessment cases (no form this month)
-;WITH zzz AS (
-SELECT x.HVCaseFK, x.PADate, 'xx' [CaseStatus]
-FROM Preassessment AS x
-JOIN (SELECT a.HVCaseFK, max(a.PADate) [maxDate]
-FROM Preassessment AS a
-JOIN dbo.SplitString(@programfk,',') on a.programfk = listitem
-WHERE --a.ProgramFK = @programfk AND 
-a.PADate < @StartDt
-GROUP BY a.HVCaseFK) AS y
-ON x.HVCaseFK = y.HVCaseFK AND x.PADate = maxDate
-WHERE x.CaseStatus = '01')
-,
+	--DECLARE @StartDtT DATE = '09/01/2012'
+	--DECLARE @StartDt DATE = '09/01/2012'
+	--DECLARE @EndDt DATE = '11/30/2012'
+	--DECLARE @programfk INT = 4
 
-qqq AS (
-SELECT *
-FROM Preassessment AS a
-JOIN dbo.SplitString(@programfk,',') on a.programfk = listitem
-WHERE --a.ProgramFK = @programfk AND 
-a.PADate BETWEEN @StartDt AND @EndDt
-)
-,
-NoStatus AS (
-SELECT a.*, '1' [NoStatus]
-FROM zzz AS a LEFT OUTER JOIN qqq AS b ON a.HVCaseFK = b.HVCaseFK
-WHERE b.HVCaseFK IS NULL
-)
-,
+	-- no status pre-assessment cases (no form this month)
+	;
+	with zzz
+	as (select x.HVCaseFK
+			  ,x.PADate
+			  ,'xx' [CaseStatus]
+			from Preassessment as x
+				join (select a.HVCaseFK
+							,max(a.PADate) [maxDate]
+						  from Preassessment as a
+							  join dbo.SplitString(@programfk,',') on a.programfk = listitem
+						  where --a.ProgramFK = @programfk AND 
+							   a.PADate < @StartDt
+						  group by a.HVCaseFK) as y on x.HVCaseFK = y.HVCaseFK and x.PADate = maxDate
+			where x.CaseStatus = '01'),
+	qqq
+	as (select *
+			from Preassessment as a
+				join dbo.SplitString(@programfk,',') on a.programfk = listitem
+			where --a.ProgramFK = @programfk AND 
+				 a.PADate between @StartDt and @EndDt
+	),
+	NoStatus
+	as (select a.*
+			  ,'1' [NoStatus]
+			from zzz as a
+				left outer join qqq as b on a.HVCaseFK = b.HVCaseFK
+			where b.HVCaseFK is null
+	),
+	No_PreAssessment
+	as (select a.HVCaseFK
+			  ,null [PADate]
+			  ,'xx' [CaseStatus]
+			  ,'1' [NoStatus]
+			from HVScreen as a
+				join CaseProgram as c on c.HVCaseFK = a.HVCaseFK
+				join dbo.SplitString(@programfk,',') on c.programfk = listitem
+				left outer join Preassessment as b on b.HVCaseFK = a.HVCaseFK and b.PADate <= @EndDt
+			where --a.ProgramFK = @programfk AND 
+				 a.ScreenDate <= @EndDt
+				 and (c.DischargeDate IS NULL OR c.DischargeDate > @StartDt)
+				 and a.ScreenResult = '1'
+				 and a.ReferralMade = '1'
+				 and b.HVCaseFK is null
+	),
+	AllPreAssessment
+	as (select x.HVCaseFK
+			  ,x.PADate
+			  ,x.CaseStatus
+			  ,'0' [NoStatus]
+			from Preassessment as x
+				join (select p.HVCaseFK
+							,max(p.PADate) [max_PADATE]
+						  from PreAssessment as p
+							  join dbo.SplitString(@programfk,',') on p.programfk = listitem
+						  where p.PADate between @StartDt and @EndDt --AND p.ProgramFK = @programfk
+						  group by p.HVCaseFK) as y on x.HVCaseFK = y.HVCaseFK and x.PADate = y.max_PADATE
+		union all
+		select *
+			from NoStatus
+		union all
+		select *
+			from No_PreAssessment
+	)
+	select b.PC1ID
+		  ,convert(varchar(12),a.ScreenDate,101) [ScreenDate]
+		  ,convert(varchar(12),x.PADate,101) [PADate]
+		  ,x.HVCaseFK
+		  ,case
+			   when x.NoStatus = '1' then
+				   'No Status'
+			   when x.CaseStatus = '01' and datediff(d,x.PADate,@EndDt) <= 30 then
+				   'Engagement Continue'
+			   when x.CaseStatus = '01' and datediff(d,x.PADate,@EndDt) > 30 then
+				   'No Status'
+			   when x.CaseStatus = '02' then
+				   'Positive, Assigned'
+			   when x.CaseStatus = '03' then
+				   'Terminated'
+			   when x.CaseStatus = '04' then
+				   'Positive, Not Assigned'
+			   else
+				   'No Status'
+		   end [CaseStatusText]
+		  ,rtrim(w.FirstName)+' '+rtrim(w.LastName) [WorkName]
 
-No_PreAssessment AS (
-SELECT a.HVCaseFK, NULL [PADate], 'xx' [CaseStatus], '1' [NoStatus]
-FROM HVScreen AS a
-JOIN CaseProgram AS c ON c.HVCaseFK = a.HVCaseFK
-JOIN dbo.SplitString(@programfk,',') on c.programfk = listitem
-LEFT OUTER JOIN Preassessment AS b ON b.HVCaseFK = a.HVCaseFK AND b.PADate <= @EndDt
-WHERE --a.ProgramFK = @programfk AND 
-a.ScreenDate <= @EndDt AND a.ScreenResult = '1' AND a.ReferralMade = '1'
-AND b.HVCaseFK IS NULL
-)
-,
+		from AllPreAssessment x
+			join HVCase as a on x.HVCaseFK = a.HVCasePK
+			join CaseProgram as b on b.HVCaseFK = x.HVCaseFK
+			join Worker as w on w.WorkerPK = b.CurrentFAWFK
 
-AllPreAssessment AS (
-SELECT x.HVCaseFK, x.PADate, x.CaseStatus, '0' [NoStatus]
-FROM Preassessment AS x
-JOIN (SELECT p.HVCaseFK, max(p.PADate) [max_PADATE]
-FROM PreAssessment AS p 
-JOIN dbo.SplitString(@programfk,',') on p.programfk = listitem
-WHERE p.PADate BETWEEN @StartDt AND @EndDt --AND p.ProgramFK = @programfk
-GROUP BY p.HVCaseFK) AS y
-ON x.HVCaseFK = y.HVCaseFK AND x.PADate = y.max_PADATE
-UNION ALL
-SELECT * FROM NoStatus
-UNION ALL
-SELECT * FROM No_PreAssessment
-)
-
-SELECT b.PC1ID
-, Convert(VARCHAR(12), a.ScreenDate, 101) [ScreenDate]
-, Convert(VARCHAR(12), x.PADate, 101) [PADate]
-, x.HVCaseFK
-, CASE WHEN x.NoStatus = '1' THEN 'No Status'
-WHEN x.CaseStatus = '01' AND datediff( d, x.PADate, @EndDt) <= 30 THEN  'Engagement Continue'
-WHEN x.CaseStatus = '01' AND datediff( d, x.PADate, @EndDt) > 30 THEN  'No Status'
-WHEN x.CaseStatus = '02' THEN 'Positive, Assigned'
-WHEN x.CaseStatus = '03' THEN 'Terminated'
-WHEN x.CaseStatus = '04' THEN 'Positive, Not Assigned'
-ELSE 'No Status' END [CaseStatusText]
-, rtrim(w.FirstName) + ' ' + rtrim(w.LastName) [WorkName]
-
-FROM AllPreAssessment x
-JOIN HVCase AS a ON x.HVCaseFK = a.HVCasePK
-JOIN CaseProgram AS b ON b.HVCaseFK = x.HVCaseFK
-JOIN Worker AS w ON w.WorkerPK = b.CurrentFAWFK
-
-ORDER BY [WorkName], b.PC1ID
-
-
-
-
+		order by [WorkName]
+				,b.PC1ID
 
 GO
