83eb5060d2f8ef291a9f40d64aa679392e165e6e
Jay Robohn <jayrobot@gmail.com>
2014-10-31 10:32:44 -0400

    Add functions to list of latest stored procs

--- hfny.main/Stored Procedures/dbo.pr_GetLatestStoredProcs.sql
+++ hfny.main/Stored Procedures/dbo.pr_GetLatestStoredProcs.sql
@@ -1,7 +1,16 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
+
+-- =============================================
+-- Author:		jrobohn
+-- Create date: June 21, 2014
+-- Description:	return stored procs and functions 
+--				modified with the last <n> days	
+-- exec pr_GetLatestStoredProcs @CutoffDays = 100 -- int
+-- =============================================
 CREATE procedure [dbo].[pr_GetLatestStoredProcs] (@CutoffDays int)
 as
 begin
@@ -16,14 +25,25 @@ begin
 
 	declare @CutoffDate date = DATEADD(dd, @CutoffDays, CURRENT_TIMESTAMP)
 	
-	select OBJECT_SCHEMA_NAME([object_id])
-		  ,name
-		  ,create_date
-		  ,modify_date
+	select object_schema_name([object_id]) as SchemaName
+		  , name
+		  , create_date
+		  , modify_date
+		  , 'StoredProc                  ' as type
 		from sys.procedures
 		where modify_date >= @CutoffDate
-		order by modify_date desc;
-
-
+	union all
+	select object_schema_name([object_id])
+		  , name
+		  , create_date
+		  , modify_date
+		  , case when type = 'FN' then 'Scalar Function'
+				when type = 'IF' then 'Inline Table-valued Function' 
+				when type = 'TF' then 'Table-valued Function' 
+			end as type
+	from sys.objects
+	where type IN ('FN', 'IF', 'TF')  -- scalar, inline table-valued, table-valued
+			and modify_date >= @CutoffDate
+	order by modify_date desc;
 end
 GO
