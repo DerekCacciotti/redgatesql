827d68687e92ceabe1a9780af5c095fc55ece51e
chun d chen <cchen2@albany.edu>
2015-03-11 08:27:28 -0400

    alter attachment table
    Add 2 fields to Attachment table (CreateDate and Creator)

--- hfny.main/Tables/dbo.Attachment.sql
+++ hfny.main/Tables/dbo.Attachment.sql
@@ -6,7 +6,9 @@ CREATE TABLE [dbo].[Attachment]
 [FormType] [char] (2) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [HVCaseFK] [int] NOT NULL,
 [ProgramFK] [int] NOT NULL,
-[Attachment] [varbinary] (max) NULL
+[Attachment] [varbinary] (max) NULL,
+[AttachmentCreateDate] [datetime] NULL CONSTRAINT [DF_Attachment_AttachmentCreateDate] DEFAULT (getdate()),
+[AttachmentCreator] [char] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Attachment] ADD CONSTRAINT [PK_Attachment] PRIMARY KEY CLUSTERED  ([AttachmentPK]) ON [PRIMARY]
