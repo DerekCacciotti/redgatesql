6860f7e4c1d76c8d7b6d0b2903e976e18f5d592c
chun d chen <cchen2@albany.edu>
2015-09-01 14:54:45 -0400

    try to fix martial status for unknown category again for CredentialingKempeAnalysis  report

--- hfny.main/Stored Procedures/dbo.rspCredentialingKempeAnalysis_Summary.sql
+++ hfny.main/Stored Procedures/dbo.rspCredentialingKempeAnalysis_Summary.sql
@@ -681,7 +681,7 @@ UNION
 
 	  FROM @tblCohort
 	  WHERE IntakeDate  IS NOT NULL   
-	  AND MaritalStatus = '06' OR MaritalStatus IS NULL
+	  AND (MaritalStatus IS NULL OR MaritalStatus NOT IN ('01', '02', '03', '04', '05'))  -- MaritalStatus = '06' OR 
 )
 ,
  cteMaritalStatusNotEnrolled AS
@@ -778,7 +778,8 @@ UNION
 
 	  FROM @tblCohort
 	  WHERE DischargeDate IS NOT NULL AND  IntakeDate  IS  NULL  
-	  AND MaritalStatus = '06' OR MaritalStatus IS NULL
+	  AND (MaritalStatus IS NULL OR MaritalStatus NOT IN ('01', '02', '03', '04', '05'))  -- MaritalStatus = '06' OR 
+	 
 
 )
 ,
