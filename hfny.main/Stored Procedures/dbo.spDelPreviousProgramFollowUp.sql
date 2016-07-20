8c1be056044ac848fb25c7d1cfb9ed845a8bde13
Jay Robohn <jayrobot@gmail.com>
2016-07-20 13:52:16 -0400

    Added paranoid transaction support

--- hfny.main/Stored Procedures/dbo.spDelPreviousProgramFollowUp.sql
+++ hfny.main/Stored Procedures/dbo.spDelPreviousProgramFollowUp.sql
@@ -17,6 +17,21 @@ CREATE procedure [dbo].[spDelPreviousProgramFollowUp]
 	, @ok as varchar(200) output
 as
 begin
+	-- Detect whether the procedure was called from an active transaction and save  
+	-- that for later use. In the procedure, @TranCounter = 0 means there was no active transaction  
+	-- and the procedure started one. @TranCounter > 0 means an active  
+	-- transaction was started before the procedure was called.  
+    declare @TranCounter INT;  
+    set @TranCounter = @@TRANCOUNT  
+    if @TranCounter > 0  
+		-- Procedure called when there is an active transaction.  
+		-- Create a savepoint to be able to roll back only the work done  
+		-- in the procedure if there is an error.  
+        save transaction ProcedureSave;  
+    else 
+		-- Procedure must start its own transaction.  
+        begin transaction;  
+	
 	begin try
 		-- SET NOCOUNT ON added to prevent extra result sets from
 		-- interfering with SELECT statements.
@@ -34,43 +49,75 @@ begin
 		if @@ROWCOUNT = 0
 			return
 			
-		begin transaction
+		delete from FollowUp 
+		where HVCaseFK = @HVCaseFK 
+				and FollowUpInterval = '99'
 
-			delete from FollowUp 
-			where HVCaseFK = @HVCaseFK 
-					and FollowUpInterval = '99'
+		delete from PC1Issues
+		where PC1IssuesPK = @PC1IssuesFK
 
-			delete from PC1Issues
-			where PC1IssuesPK = 138052
+		delete from Employment
+		where FormFK = @FollowUpFK
+				and FormType = 'FU'
+				and Interval = '99'
 
-			delete from Employment
-			where FormFK = @FollowUpFK
-					and FormType = 'FU'
-					and Interval = '99'
+		delete from Education
+		where FormFK = @FollowUpFK
+				and FormType = 'FU'
+				and Interval = '99'
 
-			delete from Education
-			where FormFK = @FollowUpFK
-					and FormType = 'FU'
-					and Interval = '99'
+		delete from PHQ9
+		where FormFK = @FollowUpFK
+				and FormType = 'FU'
+				and FormInterval = '99'
 
-			delete from PHQ9
-			where FormFK = @FollowUpFK
-					and FormType = 'FU'
-					and FormInterval = '99'
-
-			delete from CommonAttributes
-			where HVCaseFK = @HVCaseFK 
-					and FormType like 'FU%' 
-					and FormInterval = '99'
+		delete from CommonAttributes
+		where HVCaseFK = @HVCaseFK 
+				and FormType like 'FU%' 
+				and FormInterval = '99'
 
 		-- Successfully deleted 
 		set @ok = 'good'
-		commit
-	end try
-	begin catch
-		if @@TRANCOUNT > 0
-			rollback
-	end	catch
+		
+		-- Get here if no errors; must commit any transaction started in the  
+		-- procedure, but not commit a transaction started before the transaction was called.  
 
+		if @TranCounter = 0
+			-- @TranCounter = 0 means no transaction was started before the procedure was called.  
+			-- The procedure must commit the transaction it started. 
+			commit transaction
+
+	end try
+
+	begin catch
+		-- An error occurred; must determine which type of rollback will roll  
+		-- back only the work done in the procedure.  
+		if @TranCounter = 0
+			-- Transaction started in procedure. Roll back complete transaction.  
+			rollback transaction
+		else
+			-- Transaction started before procedure called, do not roll back modifications  
+			-- made before the procedure was called.  
+			if xact_state() <> -1
+				rollback transaction ProcedureSave
+				-- If the transaction is uncommitable, a rollback to the savepoint is not allowed  
+				-- because the savepoint rollback writes to the log. Just return to the caller, which  
+				-- should roll back the outer transaction.  
+		
+		-- After the appropriate rollback, echo error  
+		-- information to the caller.  
+        declare @ErrorMessage nvarchar(4000);  
+        declare @ErrorSeverity int;  
+        declare @ErrorState int;  
+  
+        select @ErrorMessage = error_message();  
+        select @ErrorSeverity = error_severity();  
+        select @ErrorState = error_state();  
+  
+        raiserror (@ErrorMessage, -- Message text.  
+                   @ErrorSeverity, -- Severity.  
+                   @ErrorState -- State.  
+                   );  
+    end catch  
 end
 GO
