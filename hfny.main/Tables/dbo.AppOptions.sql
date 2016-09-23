403bcfbb205e4bb56ce6def34a3490e4a600a4ee
Jay Robohn <jayrobot@gmail.com>
2016-09-23 14:04:22 -0400

    Made ProgramFK nullable for program-wide options

--- hfny.main/Tables/dbo.AppOptions.sql
+++ hfny.main/Tables/dbo.AppOptions.sql
@@ -7,12 +7,12 @@ CREATE TABLE [dbo].[AppOptions]
 [OptionItem] [char] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [OptionStart] [datetime] NOT NULL,
 [OptionValue] [char] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[ProgramFK] [int] NOT NULL
+[ProgramFK] [int] NULL
 ) ON [PRIMARY]
-ALTER TABLE [dbo].[AppOptions] WITH NOCHECK ADD
-CONSTRAINT [FK_AppOptions_ProgramFK] FOREIGN KEY ([ProgramFK]) REFERENCES [dbo].[HVProgram] ([HVProgramPK])
-CREATE NONCLUSTERED INDEX [IX_FK_AppOptions_ProgramFK] ON [dbo].[AppOptions] ([ProgramFK]) ON [PRIMARY]
-
 GO
 ALTER TABLE [dbo].[AppOptions] ADD CONSTRAINT [PK__AppOptio__6E5B65BC7F60ED59] PRIMARY KEY CLUSTERED  ([AppOptionsPK]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IX_FK_AppOptions_ProgramFK] ON [dbo].[AppOptions] ([ProgramFK]) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[AppOptions] WITH NOCHECK ADD CONSTRAINT [FK_AppOptions_ProgramFK] FOREIGN KEY ([ProgramFK]) REFERENCES [dbo].[HVProgram] ([HVProgramPK])
 GO
