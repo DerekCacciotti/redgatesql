1269058f73e3b8cb323b562326527007f5935826
Jay Robohn <jayrobot@gmail.com>
2017-02-09 12:36:25 -0500

    Added Retention Rate Percentage report

---
+++ hfny.main/Stored Procedures/dbo.rspRetentionRatePercentage.sql
@@ -0,0 +1,469 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		jrobohn
+-- Create date: 03/31/11
+-- Description:	Main storedproc for Retention Rate - Percentage report
+-- Description: <copied from FamSys Feb 20, 2012 - see header below>
+-- exec rspRetentionRates 9, '05/01/09', '04/30/11'
+-- exec rspRetentionRates 19, '20080101', '20121231'
+-- exec rspRetentionRates 37, '20090401', '20110331'
+-- exec rspRetentionRates 17, '20090401', '20110331'
+-- exec rspRetentionRates 20, '20080401', '20110331'
+-- exec rspRetentionRates '15,16', '20091201', '20111130'
+-- exec rspRetentionRates 1, '03/01/10', '02/29/12', null, null, ''
+-- exec rspRetentionRates 1, '03/01/10', '02/29/12', 85, null, '' 85 = Daisy Flores
+-- exec rspRetentionRates 1, '03/01/10', '02/29/12', null, 1, '' 1 = Children Youth & Families
+-- exec rspRetentionRates 1, '03/01/10', '02/29/12', null, null, ''
+-- Fixed Bug HW963 - Retention Rage Report ... Khalsa 3/20/2014
+-- =============================================
+-- =============================================
+create procedure [dbo].[rspRetentionRatePercentage]
+	-- Add the parameters for the stored procedure here
+	@ProgramFK varchar(max)
+	, @StartDate datetime
+	, @EndDate datetime
+    , @WorkerFK int = null
+	, @SiteFK int = null
+	, @CaseFiltersPositive varchar(100) = ''
+as
+BEGIN 
+-- SET NOCOUNT ON added to prevent extra result sets from
+-- interfering with SELECT statements.
+SET NOCOUNT ON;
+
+--#region declarations
+	set @SiteFK = case when dbo.IsNullOrEmpty(@SiteFK) = 1 then 0
+					   else @SiteFK
+				  end
+	set @casefilterspositive = case	when @casefilterspositive = '' then null
+									else @casefilterspositive
+							   end
+
+	declare @tblResults table (
+		LineDescription varchar(50)
+		, LineGroupingLevel int
+		, DisplayPercentages bit
+		, TotalEnrolledParticipants int
+		, RetentionRateSixMonths decimal(5,3)
+		, RetentionRateOneYear decimal(5,3)
+		, RetentionRateEighteenMonths decimal(5,3)
+		, RetentionRateTwoYears decimal(5,3)
+		, EnrolledParticipantsSixMonths int
+		, EnrolledParticipantsOneYear int
+		, EnrolledParticipantsEighteenMonths int
+		, EnrolledParticipantsTwoYears int
+		, RunningTotalDischargedSixMonths int
+		, RunningTotalDischargedOneYear int
+		, RunningTotalDischargedEighteenMonths int
+		, RunningTotalDischargedTwoYears int
+		, TotalNSixMonths int
+		, TotalNOneYear int
+		, TotalNEighteenMonths int
+		, TotalNTwoYears int
+		, AllParticipants int
+		, SixMonthsIntake int
+		, SixMonthsDischarge int
+		, OneYearIntake int 
+		, OneYearDischarge int
+		, EighteenMonthsIntake int
+		, EighteenMonthsDischarge int
+		, TwoYearsIntake int
+		, TwoYearsDischarge int);
+
+	declare @tblPC1withStats table (
+		PC1ID char(13)
+		, IntakeDate datetime
+		, DischargeDate datetime
+		, LastHomeVisit datetime
+		, RetentionMonths int
+		, ActiveAt6Months int
+		, ActiveAt12Months int
+		, ActiveAt18Months int
+		, ActiveAt24Months int);
+
+--#endregion
+--#region cteCohort - Get the cohort for the report
+	with cteCohort as
+	-----------------------
+		(select HVCasePK
+			from HVCase h 
+			inner join CaseProgram cp on cp.HVCaseFK = h.HVCasePK
+			inner join dbo.SplitString(@ProgramFK, ',') ss on ss.ListItem = cp.ProgramFK
+			inner join dbo.udfCaseFilters(@casefilterspositive, '', @programfk) cf on cf.HVCaseFK = h.HVCasePK
+			left outer join Worker w on w.WorkerPK = cp.CurrentFSWFK
+			left outer join WorkerProgram wp on wp.WorkerFK = w.WorkerPK and wp.ProgramFK = cp.ProgramFK
+			where case when @SiteFK = 0 then 1
+							 when wp.SiteFK = @SiteFK then 1
+							 else 0
+						end = 1
+				and (IntakeDate is not null and IntakeDate between @StartDate and @EndDate)
+				and  w.WorkerPK = isnull(@WorkerFK, w.WorkerPK)
+				-- and cp.ProgramFK=@ProgramFK
+		)	
+
+	--select * 
+	--from cteCohort
+	--order by HVCasePK
+
+	--select HVCasePK, count(HVCasePK)
+
+	--from cteCohort
+	--group by HVCasePK
+	--having count(HVCasePK) > 1
+
+--#endregion
+--#region cteLastFollowUp - Get last follow up completed for all cases in cohort
+	, cteLastFollowUp as 
+	-----------------------
+		(select max(FollowUpPK) as FollowUpPK
+			   , max(FollowUpDate) as FollowUpDate
+			   , fu.HVCaseFK
+			from FollowUp fu
+			inner join cteCohort c on c.HVCasePK = fu.HVCaseFK
+			group by fu.HVCaseFK
+		)
+
+	--select * 
+	--from cteLastFollowUp 
+	--order by HVCaseFK
+
+--#endregion
+--#region cteFollowUp* - get follow up common attribute rows and columns that we need for each person from the last follow up
+--#region cteDischargeData - Get all discharge related data
+    , cteDischargeData as 
+	------------------------
+		(select h.HVCasePK as HVCaseFK
+			    ,cd.DischargeReason as DischargeReason
+		from HVCase h
+		inner join CaseProgram cp on cp.HVCaseFK=h.HVCasePK
+		inner join Kempe k on k.HVCaseFK=h.HVCasePK
+		inner join codeLevel cl ON cl.codeLevelPK=CurrentLevelFK
+		inner join codeDischarge cd on cd.DischargeCode=cp.DischargeReason 
+		inner join cteCohort c on h.HVCasePK = c.HVCasePK
+		)
+
+	--select * from cteDischargeData
+	--order by HVCaseFK
+
+--#endregion
+--#region cteCaseLastHomeVisit - get the last home visit for each case in the cohort 
+	, cteCaseLastHomeVisit AS 
+	-----------------------------
+		(select HVCaseFK
+				  , max(vl.VisitStartTime) as LastHomeVisit
+				  , count(vl.VisitStartTime) as CountOfHomeVisits
+			from HVLog vl
+			inner join HVCase c on c.HVCasePK = vl.HVCaseFK
+			inner join dbo.SplitString(@ProgramFK, ',') ss on ss.ListItem = vl.ProgramFK
+			inner join cteCohort co on co.HVCasePK = c.HVCasePK
+			where VisitType <> '0001' and 
+					(IntakeDate is not null and IntakeDate between @StartDate and @EndDate)
+							 -- and vl.ProgramFK = @ProgramFK
+			group by HVCaseFK
+		)
+--#endregion
+--#region cteMain - main select for the report sproc, gets data at intake and joins to data at discharge
+	, cteMain as
+	------------------------
+		(select PC1ID
+			   ,IntakeDate
+			   ,LastHomeVisit
+			   ,DischargeDate
+			   ,cp.DischargeReason AS DischargeReasonCode
+               ,dd.DischargeReason
+			   ,case 
+				when DischargeDate is null and current_timestamp-IntakeDate > 182.125 then 1
+				when DischargeDate is not null and LastHomeVisit-IntakeDate > 182.125 then 1
+					else 0
+				end	as ActiveAt6Months
+			   ,case
+				when DischargeDate is null and current_timestamp-IntakeDate > 365.25 then 1
+				when DischargeDate is not null and LastHomeVisit-IntakeDate > 365.25 then 1
+					else 0
+				end as ActiveAt12Months
+			   ,case
+				when DischargeDate is null and current_timestamp-IntakeDate > 547.375 then 1
+				when DischargeDate is not null and LastHomeVisit-IntakeDate > 547.375 then 1
+					else 0
+				end as ActiveAt18Months
+			   ,case
+				when DischargeDate is null and current_timestamp-IntakeDate > 730.50 then 1
+				when DischargeDate is not null and LastHomeVisit-IntakeDate > 730.50 then 1
+					else 0
+				end as ActiveAt24Months
+			FROM HVCase c
+			inner join CaseProgram cp on cp.HVCaseFK=c.HVCasePK
+			inner join PC on PC.PCPK=c.PC1FK
+			inner join Kempe k on k.HVCaseFK=c.HVCasePK
+			inner join PC1Issues pc1i ON pc1i.HVCaseFK=k.HVCaseFK AND pc1i.PC1IssuesPK=k.PC1IssuesFK
+			--inner join codeLevel cl ON cl.codeLevelPK=CurrentLevelFK
+			inner join dbo.SplitString(@ProgramFK, ',') ss on ss.ListItem = cp.ProgramFK
+			inner join cteCaseLastHomeVisit lhv ON lhv.HVCaseFK=c.HVCasePK
+			left outer join cteDischargeData dd ON dd.hvcasefk=c.HVCasePK
+			where (IntakeDate is not null and IntakeDate between @StartDate and @EndDate)
+				  -- and cp.ProgramFK=@ProgramFK
+		)
+
+--#endregion
+
+--select *
+--from cteDischargeData
+
+--select *
+--from cteMain
+
+--where DischargeReasonCode is NULL or DischargeReasonCode not in ('07', '17', '18', '20', '21', '23', '25', '37') 
+--order by DischargeReasonCode, PC1ID
+
+--#region Add rows to @tblPC1withStats for each case/pc1id in the cohort, which will create the basis for the final stats
+insert into @tblPC1withStats 
+		(PC1ID
+		, IntakeDate
+		, DischargeDate
+		, LastHomeVisit
+		, RetentionMonths
+		, ActiveAt6Months
+		, ActiveAt12Months
+		, ActiveAt18Months
+		, ActiveAt24Months)
+select distinct PC1ID
+		, IntakeDate
+		, DischargeDate
+		, LastHomeVisit
+				   ,case when DischargeDate is not null then 
+						datediff(mm,IntakeDate,LastHomeVisit)
+					else
+						datediff(mm,IntakeDate,current_timestamp)
+					end as RetentionMonths
+
+
+		, ActiveAt6Months
+		, ActiveAt12Months
+		, ActiveAt18Months
+		, ActiveAt24Months
+from cteMain
+-- where DischargeReason not in ('Out of Geographical Target Area','Miscarriage/Pregnancy Terminated','Target Child Died')
+where DischargeReasonCode is NULL or DischargeReasonCode not in ('07', '17', '18', '20', '21', '23', '25', '37')
+		-- (DischargeReasonCode not in ('07', '17', '18', '20', '21', '23', '25', '37') or datediff(day,IntakeDate,DischargeDate)>=(4*6*30.44))
+order by PC1ID,IntakeDate
+--#endregion
+--select * from @tblPC1withStats
+
+declare @TotalCohortCount int
+
+-- now we have all the rows from the cohort in @tblPC1withStats
+-- get the total count
+select @TotalCohortCount = COUNT(*) 
+  from @tblPC1withStats
+
+--#region declare vars to collect counts for final stats
+declare @LineGroupingLevel int
+		, @TotalEnrolledParticipants int
+		, @RetentionRateSixMonths decimal(5,3)
+		, @RetentionRateOneYear decimal(5,3)
+		, @RetentionRateEighteenMonths decimal(5,3)
+		, @RetentionRateTwoYears decimal(5,3)
+		, @EnrolledParticipantsSixMonths int
+		, @EnrolledParticipantsOneYear int
+		, @EnrolledParticipantsEighteenMonths int
+		, @EnrolledParticipantsTwoYears int
+		, @RunningTotalDischargedSixMonths int
+		, @RunningTotalDischargedOneYear int
+		, @RunningTotalDischargedEighteenMonths int
+		, @RunningTotalDischargedTwoYears int
+		, @TotalNSixMonths int
+		, @TotalNOneYear int
+		, @TotalNEighteenMonths int
+		, @TotalNTwoYears int
+
+declare @AllEnrolledParticipants int
+		, @SixMonthsTotal int
+		, @TwelveMonthsTotal int
+		, @EighteenMonthsTotal int
+		, @TwentyFourMonthsTotal int
+		, @SixMonthsAtIntake int
+		, @SixMonthsAtDischarge int
+		, @TwelveMonthsAtIntake int
+		, @TwelveMonthsAtDischarge int
+		, @EighteenMonthsAtIntake int
+		, @EighteenMonthsAtDischarge int
+		, @TwentyFourMonthsAtIntake int
+		, @TwentyFourMonthsAtDischarge int
+--#endregion
+--#region Retention Rate %
+select @SixMonthsTotal = count(PC1ID)
+from @tblPC1withStats
+where ActiveAt6Months = 1
+
+select @TwelveMonthsTotal = count(PC1ID)
+from @tblPC1withStats
+where ActiveAt12Months = 1
+
+select @EighteenMonthsTotal = count(PC1ID)
+from @tblPC1withStats
+where ActiveAt18Months = 1
+
+select @TwentyFourMonthsTotal = count(PC1ID)
+from @tblPC1withStats
+where ActiveAt24Months = 1
+
+set @RetentionRateSixMonths = case when @TotalCohortCount = 0 then 0.0000 else round((@SixMonthsTotal / (@TotalCohortCount * 1.0000)),4) end
+set @RetentionRateOneYear = case when @TotalCohortCount = 0 then 0.0000 else round((@TwelveMonthsTotal / (@TotalCohortCount * 1.0000)),4) end
+set @RetentionRateEighteenMonths = case when @TotalCohortCount = 0 then 0.0000 else round((@EighteenMonthsTotal / (@TotalCohortCount * 1.0000)),4) end
+set @RetentionRateTwoYears = case when @TotalCohortCount = 0 then 0.0000 else round((@TwentyFourMonthsTotal / (@TotalCohortCount * 1.0000)),4) end
+--#endregion
+--#region Enrolled Participants
+select @EnrolledParticipantsSixMonths = count(*)
+from @tblPC1withStats
+where ActiveAt6Months = 1 
+
+select @EnrolledParticipantsOneYear = count(*)
+from @tblPC1withStats
+where ActiveAt12Months = 1 
+
+select @EnrolledParticipantsEighteenMonths = count(*)
+from @tblPC1withStats
+where ActiveAt18Months = 1 
+
+select @EnrolledParticipantsTwoYears = count(*)
+from @tblPC1withStats
+where ActiveAt24Months = 1
+--#endregion
+--#region Running Total Discharged
+select @RunningTotalDischargedSixMonths = count(*)
+from @tblPC1withStats
+where ActiveAt6Months = 0 and LastHomeVisit is not null
+
+select @RunningTotalDischargedOneYear = count(*) + @RunningTotalDischargedSixMonths
+from @tblPC1withStats
+where ActiveAt6Months = 1 and ActiveAt12Months = 0 and LastHomeVisit is not null
+
+select @RunningTotalDischargedEighteenMonths = count(*) + @RunningTotalDischargedOneYear
+from @tblPC1withStats
+where ActiveAt12Months = 1 and ActiveAt18Months = 0 and LastHomeVisit is not null
+
+select @RunningTotalDischargedTwoYears = count(*) + @RunningTotalDischargedEighteenMonths
+from @tblPC1withStats
+where ActiveAt18Months = 1 and ActiveAt24Months = 0 and LastHomeVisit is not null
+--select @RunningTotalDischargedSixMonths = count(*)
+--from @tblPC1withStats
+--where ActiveAt6Months = 0 and LastHomeVisit between IntakeDate and dateadd(day, 6*30.44, IntakeDate)
+--
+--select @RunningTotalDischargedOneYear = count(*) + @RunningTotalDischargedSixMonths
+--from @tblPC1withStats
+--where ActiveAt12Months = 0 and LastHomeVisit between dateadd(day, (6*30.44)+1, IntakeDate) and dateadd(day, 12*30.44, IntakeDate)
+--
+--select @RunningTotalDischargedEighteenMonths = count(*) + @RunningTotalDischargedOneYear
+--from @tblPC1withStats
+--where ActiveAt18Months = 0 and LastHomeVisit between dateadd(day, (12*30.44)+1, IntakeDate) and dateadd(day, 18*30.44, IntakeDate)
+--
+--select @RunningTotalDischargedTwoYears = count(*) + @RunningTotalDischargedEighteenMonths
+--from @tblPC1withStats
+--where ActiveAt24Months = 0 and LastHomeVisit between dateadd(day, (18*30.44)+1, IntakeDate) and dateadd(day, 24*30.44, IntakeDate)
+--#endregion
+--#region Total (N) - (Discharged)
+select @TotalNSixMonths = count(*)
+from @tblPC1withStats
+where ActiveAt6Months = 0 and LastHomeVisit is not null
+
+select @TotalNOneYear = count(*)
+
+from @tblPC1withStats
+where ActiveAt6Months = 1 and ActiveAt12Months = 0 and LastHomeVisit is not null
+
+select @TotalNEighteenMonths = count(*)
+from @tblPC1withStats
+where ActiveAt12Months = 1 and ActiveAt18Months = 0 and LastHomeVisit is not null
+
+select @TotalNTwoYears = count(*)
+from @tblPC1withStats
+where ActiveAt18Months = 1 and ActiveAt24Months = 0 and LastHomeVisit is not null
+--#endregion
+
+insert into @tblResults (LineDescription
+						, LineGroupingLevel
+						, DisplayPercentages
+						, TotalEnrolledParticipants
+						, RetentionRateSixMonths
+						, RetentionRateOneYear
+						, RetentionRateEighteenMonths
+						, RetentionRateTwoYears
+						, EnrolledParticipantsSixMonths
+						, EnrolledParticipantsOneYear
+						, EnrolledParticipantsEighteenMonths
+						, EnrolledParticipantsTwoYears
+						, RunningTotalDischargedSixMonths
+						, RunningTotalDischargedOneYear
+						, RunningTotalDischargedEighteenMonths
+						, RunningTotalDischargedTwoYears
+						, TotalNSixMonths
+						, TotalNOneYear
+						, TotalNEighteenMonths
+						, TotalNTwoYears)
+values ('Totals'
+		, @LineGroupingLevel
+		, 1
+        , @TotalCohortCount
+		, @RetentionRateSixMonths
+		, @RetentionRateOneYear
+		, @RetentionRateEighteenMonths
+		, @RetentionRateTwoYears
+		, @EnrolledParticipantsSixMonths
+		, @EnrolledParticipantsOneYear
+		, @EnrolledParticipantsEighteenMonths
+		, @EnrolledParticipantsTwoYears
+		, @RunningTotalDischargedSixMonths
+		, @RunningTotalDischargedOneYear
+		, @RunningTotalDischargedEighteenMonths
+		, @RunningTotalDischargedTwoYears
+		, @TotalNSixMonths
+		, @TotalNOneYear
+		, @TotalNEighteenMonths
+		, @TotalNTwoYears)
+
+select LineDescription
+	  ,1 as LineGroupingLevel
+	  ,DisplayPercentages
+	  ,TotalEnrolledParticipants
+	  ,case when datediff(ww,@enddate,getdate()) >= 26 then RetentionRateSixMonths else null end as RetentionRateSixMonths
+	  ,case when datediff(ww,@enddate,getdate()) >= 52 then RetentionRateOneYear else null end as RetentionRateOneYear
+	  ,case when datediff(ww,@enddate,getdate()) >= 78 then RetentionRateEighteenMonths else null end as RetentionRateEighteenMonths
+	  ,case when datediff(ww,@enddate,getdate()) >= 104 then RetentionRateTwoYears else null end as RetentionRateTwoYears
+	  ,case when datediff(ww,@enddate,getdate()) >= 26 then EnrolledParticipantsSixMonths else null end as EnrolledParticipantsSixMonths
+	  ,case when datediff(ww,@enddate,getdate()) >= 52 then EnrolledParticipantsOneYear else null end as EnrolledParticipantsOneYear
+	  ,case when datediff(ww,@enddate,getdate()) >= 78 then EnrolledParticipantsEighteenMonths else null end as EnrolledParticipantsEighteenMonths
+	  ,case when datediff(ww,@enddate,getdate()) >= 104 then EnrolledParticipantsTwoYears else null end as EnrolledParticipantsTwoYears
+	  ,case when datediff(ww,@enddate,getdate()) >= 26 then RunningTotalDischargedSixMonths else null end as RunningTotalDischargedSixMonths
+	  ,case when datediff(ww,@enddate,getdate()) >= 52 then RunningTotalDischargedOneYear else null end as RunningTotalDischargedOneYear
+	  ,case when datediff(ww,@enddate,getdate()) >= 78 then RunningTotalDischargedEighteenMonths else null end as RunningTotalDischargedEighteenMonths
+	  ,case when datediff(ww,@enddate,getdate()) >= 104 then RunningTotalDischargedTwoYears else null end as RunningTotalDischargedTwoYears
+	  ,case when datediff(ww,@enddate,getdate()) >= 26 then TotalNSixMonths else null end as TotalNSixMonths
+	  ,case when datediff(ww,@enddate,getdate()) >= 52 then TotalNOneYear else null end as TotalNOneYear
+	  ,case when datediff(ww,@enddate,getdate()) >= 78 then TotalNEighteenMonths else null end as TotalNEighteenMonths
+	  ,case when datediff(ww,@enddate,getdate()) >= 104 then TotalNTwoYears else null end as TotalNTwoYears
+	  ,AllParticipants
+	  ,case when datediff(ww,@enddate,getdate()) >= 26 then SixMonthsIntake else null end as SixMonthsIntake
+	  ,case when datediff(ww,@enddate,getdate()) >= 26 then SixMonthsDischarge else null end as SixMonthsDischarge
+	  ,case when datediff(ww,@enddate,getdate()) >= 52 then OneYearIntake else null end as OneYearIntake
+	  ,case when datediff(ww,@enddate,getdate()) >= 52 then OneYearDischarge else null end as OneYearDischarge
+	  ,case when datediff(ww,@enddate,getdate()) >= 78 then EighteenMonthsIntake else null end as EighteenMonthsIntake
+	  ,case when datediff(ww,@enddate,getdate()) >= 78 then EighteenMonthsDischarge else null end as EighteenMonthsDischarge
+	  ,case when datediff(ww,@enddate,getdate()) >= 104 then TwoYearsIntake else null end as TwoYearsIntake
+	  ,case when datediff(ww,@enddate,getdate()) >= 104 then TwoYearsDischarge else null end as TwoYearsDischarge
+from @tblResults
+--SELECT * from @tblResults
+
+--select VendorID, Employee, Orders
+--from
+--   (SELECT ActiveAt6Months, ActiveAt12Months, ActiveAt18Months, ActiveAt24Months
+--   FROM @tblPC1withStats) p
+--UNPIVOT
+--   (Orders FOR Employee IN 
+--      (Emp1, Emp2, Emp3, Emp4, Emp5)
+--)AS unpvt;
+
+end
+GO
