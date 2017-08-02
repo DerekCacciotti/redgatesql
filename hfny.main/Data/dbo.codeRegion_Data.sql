a6bb64c0994338a1abe98a489207e0d8a12a20bc
Jay Robohn <jayrobot@gmail.com>
2017-08-02 11:10:03 -0400

    Added unfunded programs to Regions

--- hfny.main/Data/dbo.codeRegion_Data.sql
+++ hfny.main/Data/dbo.codeRegion_Data.sql
@@ -1,4 +1,7 @@
 SET IDENTITY_INSERT [dbo].[codeRegion] ON
+INSERT INTO [dbo].[codeRegion] ([codeRegionPK], [RegionDescription], [RegionName]) VALUES (5, 'Unfunded programs', 'Unfunded')
+SET IDENTITY_INSERT [dbo].[codeRegion] OFF
+SET IDENTITY_INSERT [dbo].[codeRegion] ON
 INSERT INTO [dbo].[codeRegion] ([codeRegionPK], [RegionDescription], [RegionName]) VALUES (1, 'The Capital District of NY State', 'Capital District')
 INSERT INTO [dbo].[codeRegion] ([codeRegionPK], [RegionDescription], [RegionName]) VALUES (2, 'The Hudson Valley region of NY State', 'Hudson Valley')
 INSERT INTO [dbo].[codeRegion] ([codeRegionPK], [RegionDescription], [RegionName]) VALUES (3, 'The central/western region of NY State', 'Central/Western')
