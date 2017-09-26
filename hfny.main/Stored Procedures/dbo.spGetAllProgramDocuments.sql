ba2db070b0a52495e85cf1b51ea1ac5d69f99a25
rickdjiii <rjordan@albany.edu>
2017-09-26 14:19:10 -0400

    Filter program documents by program

--- hfny.main/Stored Procedures/dbo.spGetAllProgramDocuments.sql
+++ hfny.main/Stored Procedures/dbo.spGetAllProgramDocuments.sql
@@ -2,28 +2,27 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
+
 -- =============================================
 -- Author:		<Jay Robohn>
 -- Create date: <Sep. 16, 2016>
--- Description:	<Gets all Program documents from the Attachment table>
+-- Description:	<Gets all Program documents from the Attachment table for a specific program>
 -- =============================================
 
-create procedure [dbo].[spGetAllProgramDocuments]
-as
-	begin
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-		set nocount on;
+CREATE procedure [dbo].[spGetAllProgramDocuments]
+	@programFK INT
+AS
+BEGIN
+	SET NOCOUNT ON;
 
-    -- Insert statements for procedure here
-		select	AttachmentPK
-			  , replace(AttachmentTitle, '.pdf', '') as AttachmentTitle
-			  , AttachmentDescription
-			  , convert(varchar(10), FormDate, 101) as FormDate
-			  , AttachmentCreateDate
-			  , AttachmentCreator
-		from	Attachment a 
-		where	FormType = 'AD'
-		order by AttachmentCreateDate desc;
-	end;
+	SELECT AttachmentPK
+	, REPLACE(AttachmentTitle, '.pdf', '') AS 'AttachmentTitle'
+	, AttachmentDescription
+	, CONVERT(VARCHAR(10), FormDate, 101) AS 'FormDate'
+	, AttachmentCreateDate
+	, AttachmentCreator
+	FROM Attachment
+	WHERE FormType = 'AD' AND ProgramFK = @programFK
+	ORDER BY AttachmentCreateDate DESC;
+END
 GO
