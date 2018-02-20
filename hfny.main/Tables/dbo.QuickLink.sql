4ae0583ac0c6951c8a4cafe36116bcd2e764d3af
Benjamin Simmons <brsimmons@albany.edu>
2018-02-20 16:15:30 -0500

    Fixed QuickLink design so that the Generator works

--- hfny.main/Tables/dbo.QuickLink.sql
+++ hfny.main/Tables/dbo.QuickLink.sql
@@ -3,7 +3,9 @@ CREATE TABLE [dbo].[QuickLink]
 [QuickLinkPK] [int] NOT NULL IDENTITY(1, 1),
 [LinkType] [char] (4) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [LinkURL] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[QuickLink] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[QuickLinkDescription] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [UserName] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[QuickLink] ADD CONSTRAINT [PK_QuickLink] PRIMARY KEY CLUSTERED  ([QuickLinkPK]) ON [PRIMARY]
 GO
