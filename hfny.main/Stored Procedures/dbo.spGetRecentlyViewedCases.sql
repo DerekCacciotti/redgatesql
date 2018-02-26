6a8aeed0c31f5df9c94d795b62450b4050554981
Jay Robohn <jayrobot@gmail.com>
2018-02-26 08:00:36 -0500

    Renamed get recent cases procedure

--- hfny.main/Stored Procedures/dbo.spGetRecentlyViewedCases.sql
+++ hfny.main/Stored Procedures/dbo.spGetRecentlyViewedCases.sql
@@ -9,24 +9,23 @@ GO
 -- viewed cases for a specific user and filters those results by program.
 -- The @ProgramFK parameter is now optional.
 -- =============================================
-CREATE PROCEDURE [dbo].[spGetRecentlyViewedCasesByUserNameAndProgramFK] (@UserName VARCHAR(255), @ProgramFK INT)
-AS
-BEGIN
+create procedure [dbo].[spGetRecentlyViewedCases] (@UserName varchar(255), @ProgramFK int)
+as begin
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
+	set noCount on ;
 
-    WITH cteCaseViews AS 
-	(
-		SELECT TOP 1000 cv.PC1ID, MAX(cv.ViewDate) AS ViewDateMax 
-			FROM CaseView cv
-			INNER JOIN CaseProgram cp ON cp.PC1ID = cv.PC1ID
-			WHERE cv.Username = @UserName 
-			AND cp.ProgramFK = ISNULL(@ProgramFK, cp.ProgramFK)
-			GROUP BY cv.PC1ID
-			ORDER BY ViewDateMax DESC
+	with cteCaseViews
+	as (
+		select		top 1000 cv.PC1ID
+						, max(cv.ViewDate) as ViewDateMax
+		from		CaseView cv
+		inner join	CaseProgram cp on cp.PC1ID = cv.PC1ID
+		where		cv.Username = @UserName and cp.ProgramFK = isnull(@ProgramFK, cp.ProgramFK)
+		group by	cv.PC1ID
+		order by	ViewDateMax desc
 	)
 
-	SELECT TOP 50 PC1ID FROM cteCaseViews
-END
+	select top 20 PC1ID from cteCaseViews ;
+end ;
 GO
