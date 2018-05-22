84a42aafd851b9585fd2d5348ccb61d194f503b1
Bill O'Brien <wobrien2@albany.edu>
2018-05-22 14:02:22 -0400

    added index recommended by ssms

--- hfny.main/Stored Procedures/dbo.rspCredentialingASQ.sql
+++ hfny.main/Stored Procedures/dbo.rspCredentialingASQ.sql
@@ -48,9 +48,8 @@ AS
 	declare @n int = 0
 	select @n = case when @UnderCutoffOnly = 'Y' then 1 else 0 end
 
-	if object_id('tempdb..#cteAll') is not null drop table #cteAll
 
-	create table #cteAll (
+	declare @cteAll table (
 		   supervisor varchar(100)
 		 , worker varchar(100)
 		 , PC1ID char(13)
@@ -64,21 +63,33 @@ AS
 		 , ASQTCReceiving int
 	)
 
-;with cteMain
-as (
+	declare @cteMain table (
+		supervisor char(50)
+		,worker char(50)
+		,PC1ID char(15)
+		,TCName char(50)
+		,TCDOB datetime
+		,GestationalAge int
+		,TCAge varchar(150)
+		,DateCompleted datetime
+		,InWindow char(15)
+		,TCAgeCode int
+		,ASQTCReceiving bit
+	)
+	insert into @cteMain
 	select
-		  LTRIM(RTRIM(supervisor.firstname))+' '+LTRIM(RTRIM(supervisor.lastname)) supervisor
-		 ,LTRIM(RTRIM(fsw.firstname))+' '+LTRIM(RTRIM(fsw.lastname)) worker
+		  LTRIM(RTRIM(supervisor.firstname))+' '+LTRIM(RTRIM(supervisor.lastname))
+		 ,LTRIM(RTRIM(fsw.firstname))+' '+LTRIM(RTRIM(fsw.lastname))
 		 ,d.PC1ID
-		 ,LTRIM(RTRIM(c.TCFirstName))+' '+LTRIM(RTRIM(c.TCLastName)) TCName
-		 ,convert(varchar(12),c.TCDOB,101) TCDOB
+		 ,LTRIM(RTRIM(c.TCFirstName))+' '+LTRIM(RTRIM(c.TCLastName))
+		 ,convert(varchar(12),c.TCDOB,101)
 		 ,c.GestationalAge
-		 ,ltrim(rtrim(replace(b.[AppCodeText],'(optional)',''))) TCAge
-		 ,convert(varchar(12),a.DateCompleted,101) DateCompleted
+		 ,ltrim(rtrim(replace(b.[AppCodeText],'(optional)','')))
+		 ,convert(varchar(12),a.DateCompleted,101) 
 		 ,case when ASQInWindow is null then 'Unknown'
-			  when ASQInWindow = 1 then 'In Window' else 'Out of Window' end InWindow
-		 ,a.TCAge [TCAgeCode]
-		 ,ISNULL(a.[ASQTCReceiving],0) ASQTCReceiving
+			  when ASQInWindow = 1 then 'In Window' else 'Out of Window' end 
+		 ,a.TCAge
+		 ,ISNULL(a.[ASQTCReceiving],0) 
 		from ASQ a
 			inner join codeApp b on a.TCAge = b.AppCode and b.AppCodeGroup = 'TCAge' and b.AppCodeUsedWhere like '%AQ%'
 			inner join TCID c on c.TCIDPK = a.TCIDFK
@@ -114,22 +125,33 @@ as (
 			 and wp.supervisorfk = ISNULL(@supervisorfk,wp.supervisorfk)
 			 and d.PC1ID = case when @pc1ID = '' then d.PC1ID else @pc1ID end
 			 and (case when @SiteFK = 0 then 1 when wp.SiteFK = @SiteFK then 1 else 0 end = 1)
-)
 
-, cteNone
-	as (
-	select
-		  LTRIM(RTRIM(supervisor.firstname))+' '+LTRIM(RTRIM(supervisor.lastname)) supervisor
-		 ,LTRIM(RTRIM(fsw.firstname))+' '+LTRIM(RTRIM(fsw.lastname)) worker
+
+    declare @cteNone table (
+		supervisor char(50)
+		,worker char(50)
+		,PC1ID char(15)
+		,TCName char(50)
+		,TCDOB datetime
+		,GestationalAge int
+		,TCAge varchar(150)
+		,DateCompleted datetime
+		,InWindow char(15)
+		,TCAgeCode int
+		,ASQTCReceiving bit
+	)
+	insert into @cteNone
+	select LTRIM(RTRIM(supervisor.firstname))+' '+LTRIM(RTRIM(supervisor.lastname))
+		 ,LTRIM(RTRIM(fsw.firstname))+' '+LTRIM(RTRIM(fsw.lastname))
 		 ,d.PC1ID
-		 ,LTRIM(RTRIM(c.TCFirstName))+' '+LTRIM(RTRIM(c.TCLastName)) TCName
-		 ,CASE WHEN c.TCDOB IS NOT NULL THEN convert(varchar(12),c.TCDOB,101) ELSE convert(varchar(12),h.EDC,101) END TCDOB
+		 ,LTRIM(RTRIM(c.TCFirstName))+' '+LTRIM(RTRIM(c.TCLastName))
+		 ,CASE WHEN c.TCDOB IS NOT NULL THEN convert(varchar(12),c.TCDOB,101) ELSE convert(varchar(12),h.EDC,101) END
 		 ,c.GestationalAge
-		 ,'[None]' TCAge
-		 ,'' DateCompleted
-		 ,'' InWindow
-		 ,'' [TCAgeCode]
-		 ,0 [ASQTCReceiving]
+		 ,'[None]'
+		 ,''
+		 ,''
+		 ,''
+		 ,0
 		from 
 			CaseProgram d 
 			INNER JOIN HVCase AS h ON h.HVCasePK = d.HVCaseFK
@@ -150,40 +172,70 @@ as (
 			 AND a.HVCaseFK IS NULL
 			 AND c.TCDOB IS NOT NULL 
 			 AND (CASE WHEN @UnderCutoffOnly = 'Y' THEN 1 ELSE 0 END = 0)
-)
 
-insert into #cteAll	
-	  SELECT DISTINCT * FROM cteMain
+
+insert into @cteAll	
+	  SELECT DISTINCT * FROM @cteMain
 	  UNION all
-	  SELECT * FROM cteNone 
+	  SELECT * FROM @cteNone 
 	
-; with cteAdjAge AS (
+declare @cteAdjAge table (
+	 supervisor varchar(100)
+	, worker varchar(100)
+	, PC1ID char(13)
+	, TCName varchar(450)
+	, TCDOB varchar(12)
+	, GestationalAge int
+	, TCAge varchar(150)
+	, DateCompleted varchar(12)
+	, InWindow varchar(50)
+	, [TCAgeCode] char(2)
+	, ASQTCReceiving int
+	, AdjTCDOB varchar(12)
+)
+	insert into @cteAdjAge
 	SELECT * 
-	
          ,CASE WHEN (CASE WHEN dateadd(year, datediff (year, a.TCDOB, getdate()), a.TCDOB) > getdate()
             THEN datediff(year, a.TCDOB, getdate()) - 1
             ELSE datediff(year, a.TCDOB, getdate())
           END) < 2 AND a.GestationalAge <= 40 
           THEN convert(DATETIME, a.TCDOB, 101) + (40 - a.GestationalAge) * 7
           ELSE convert(DATETIME, a.TCDOB, 101) END AdjTCDOB
-	FROM #cteAll AS a
-)
+	FROM @cteAll AS a
 
-, cteFinal AS (
+declare @cteFinal table (
+	supervisor varchar(100)
+	, worker varchar(100)
+	, PC1ID char(13)
+	, TCName varchar(450)
+	, TCDOB varchar(12)
+	, GestationalAge int
+	, TCAge varchar(150)
+	, DateCompleted varchar(12)
+	, InWindow varchar(50)
+	, [TCAgeCode] char(2)
+	, ASQTCReceiving int
+	, AdjTCDOB varchar(12)
+	, Age datetime
+	, AgeMonth int
+	,AgeAtDateCompleted int
+	,AgeAtDateCompletedMonth int
+)
+insert into @cteFinal
 	SELECT *
 	,CASE WHEN dateadd(year, datediff (year, AdjTCDOB, getdate()), AdjTCDOB) > getdate()
             THEN datediff(year, AdjTCDOB, getdate()) - 1
             ELSE datediff(year, AdjTCDOB, getdate())
-          END as Age
+          END
 	
-	,datediff(month, AdjTCDOB, getdate()) as AgeMonth
+	,datediff(month, AdjTCDOB, getdate())
 	
 	, CASE WHEN DateCompleted <> '' 
 	THEN CASE WHEN dateadd(year, datediff (year, AdjTCDOB, DateCompleted), AdjTCDOB) > DateCompleted
             THEN datediff(year, AdjTCDOB, DateCompleted) - 1
             ELSE datediff(year, AdjTCDOB, DateCompleted)
           END
-	ELSE -1 END AS AgeAtDateCompleted
+	ELSE -1 END
 	
 	, CASE WHEN DateCompleted <> '' 
 	THEN 
@@ -194,39 +246,59 @@ insert into #cteAll
 			ELSE datediff (month, AdjTCDOB, DateCompleted)
 		END
 
-	--datediff (month, AdjTCDOB, DateCompleted)
+	--datediff (month, AdjTCDOB, DateCompleted)	
+	ELSE -1 END
+		
+	FROM @cteAdjAge
 	
-	
-	ELSE -1 END AS AgeAtDateCompletedMonth
-	
-	
-	
-	FROM cteAdjAge
+	declare @cteXXX table (
+		PC1ID char(15)
+		,nASQ int
 	)
-	
-, cteXXX AS (
+	insert into @cteXXX
 	SELECT PC1ID, sum(CASE WHEN AgeAtDateCompleted = -1 THEN 0 
 	WHEN (Age - AgeAtDateCompleted) <= 1 THEN 1 ELSE 0 END) AS nASQ
-	FROM cteFinal
+	FROM @cteFinal
 	GROUP BY PC1ID
-)
 
-, cteYYY AS (
-	SELECT a.*, b.nASQ
-	, CASE WHEN a.Age = 1 THEN (CASE WHEN b.nASQ >= 2 THEN 'Yes' ELSE 'No' END)
-	       WHEN a.Age = 2 THEN (CASE WHEN b.nASQ >= 2 THEN 'Yes' ELSE 'No' END)
-	       ELSE (CASE WHEN b.nASQ >= 1 THEN 'Yes' ELSE 'No' END)
-	  END AS Meets
-	FROM cteFinal AS a JOIN cteXXX AS b ON a.PC1ID = b.PC1ID
+    declare @cteYYY table (
+		supervisor varchar(100)
+	, worker varchar(100)
+	, PC1ID char(13)
+	, TCName varchar(450)
+	, TCDOB varchar(12)
+	, GestationalAge int
+	, TCAge varchar(150)
+	, DateCompleted varchar(12)
+	, InWindow varchar(50)
+	, [TCAgeCode] char(2)
+	, ASQTCReceiving int
+	, AdjTCDOB varchar(12)
+	, Age datetime
+	, AgeMonth int
+	,AgeAtDateCompleted int
+	,AgeAtDateCompletedMonth int
+	,nASQ int
+	,Meets char(3)
 )
+insert into @cteYYY
+SELECT a.*, b.nASQ
+, CASE WHEN a.Age = 1 THEN (CASE WHEN b.nASQ >= 2 THEN 'Yes' ELSE 'No' END)
+       WHEN a.Age = 2 THEN (CASE WHEN b.nASQ >= 2 THEN 'Yes' ELSE 'No' END)
+       ELSE (CASE WHEN b.nASQ >= 1 THEN 'Yes' ELSE 'No' END)
+  END AS Meets
+FROM @cteFinal AS a JOIN @cteXXX AS b ON a.PC1ID = b.PC1ID
 
-, cteASQTCReceiving AS (
-	SELECT d.PC1ID, SUM(CASE WHEN  a.ASQTCReceiving = 1 then 1 ELSE 0 END) AS EI
-	FROM ASQ AS a
-	inner join CaseProgram d on d.HVCaseFK = a.HVCaseFK
-	inner join dbo.SplitString(@programfk,',') on d.programfk = listitem
-	GROUP BY d.PC1ID
+declare @cteASQTCReceiving table (
+	PC1ID char(15)
+	,EI int
 )
+insert into @cteASQTCReceiving
+SELECT d.PC1ID, SUM(CASE WHEN  a.ASQTCReceiving = 1 then 1 ELSE 0 END) AS EI
+FROM ASQ AS a
+inner join CaseProgram d on d.HVCaseFK = a.HVCaseFK
+inner join dbo.SplitString(@programfk,',') on d.programfk = listitem
+GROUP BY d.PC1ID
 
 -- Meets,
 SELECT 
@@ -235,9 +307,9 @@ DateCompleted, InWindow, [TCAgeCode], AdjTCDOB, Age, AgeMonth,
 CASE WHEN AgeAtDateCompleted = -1 THEN 0 ELSE AgeAtDateCompleted END AgeAtDateCompleted, 
 nASQ, AgeAtDateCompletedMonth, b.EI, CASE WHEN b.EI > 0 THEN 'EI' ELSE
 a.Meets END AS Meets, CASE WHEN a.[ASQTCReceiving] = 1 THEN 'Yes' ELSE 'No' END AS TCReceivingEI
-FROM cteYYY AS a JOIN cteASQTCReceiving AS b ON a.PC1ID = b.PC1ID
+FROM @cteYYY AS a JOIN @cteASQTCReceiving AS b ON a.PC1ID = b.PC1ID
 WHERE Age > 0 AND Age <= 5
 order by Age DESC, worker, PC1ID, TCAgeCode
 
-drop table #cteAll
+
 GO
