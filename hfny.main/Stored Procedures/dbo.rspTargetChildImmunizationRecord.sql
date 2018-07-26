177dde2945970ab681f3ab649b9e75d5aaaa5340
Benjamin Simmons <brsimmons@albany.edu>
2018-07-26 09:55:47 -0400

    Bug Fix - Target Child Immunization Record reports were not working with site or case filters

--- hfny.main/Stored Procedures/dbo.rspTargetChildImmunizationRecord.sql
+++ hfny.main/Stored Procedures/dbo.rspTargetChildImmunizationRecord.sql
@@ -2,15 +2,16 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-
 -- =============================================
 -- Author:    <Jay Robohn>
 -- Create date: <Feb 20, 2012>
 -- Description: <copied from FamSys - see header below>
 -- =============================================
-create procedure [dbo].[rspTargetChildImmunizationRecord]
+CREATE PROC [dbo].[rspTargetChildImmunizationRecord]
 (
     @programfk varchar(max)    = null,
+	@SiteFK	   INT			   = NULL,
+    @CaseFiltersPositive varchar(100) = '',
     @rdate     datetime
 )
 as
@@ -22,6 +23,8 @@ as
 	end
 
 	set @programfk = REPLACE(@programfk,'"','')
+	set @CaseFiltersPositive = case when @CaseFiltersPositive = '' then null else @CaseFiltersPositive end	
+	SET @SiteFK = isnull(@SiteFK, 0)
 
 	---- TCMedical
 	select PC1ID
@@ -69,6 +72,7 @@ as
 									   ,null as TCItemDate
 									   ,0 IsDelayed
 									   ,0 TCMedicalPK
+										,CurrentFSWFK
 							from caseprogram
 								inner join hvcase on hvcasepk = caseprogram.hvcasefk
 								inner join tcid on tcid.hvcasefk = hvcasepk and tcid.programfk = caseprogram.programfk
@@ -79,6 +83,7 @@ as
 												from codeduebydates
 													inner join codeMedicalItem cmi on ScheduledEvent = cmi.MedicalItemTitle and MedicalItemCode <= 13) codeduebydates on codeduebydates.interval = datediff(M,dateadd(dd,-30.44,HVCase.TCDOB),@rdate)
 								inner join dbo.SplitString(@programfk,',') on caseprogram.programfk = listitem
+								INNER JOIN dbo.udfCaseFilters(@CaseFiltersPositive,'',@ProgramFK) cf ON cf.HVCaseFK = HVCasePK
 							where (hvcase.tcdob is not null
 								 and hvcase.tcdob <= dateadd(dd,-30.44,@rdate)) -- caseprogress >= 11
 								 and intakedate <= @rdate
@@ -95,18 +100,22 @@ as
 									   ,TCItemDate
 									   ,IsDelayed
 									   ,TCMedical.TCMedicalPK
+										,CurrentFSWFK
 							from caseprogram
 								inner join hvcase on hvcasepk = caseprogram.hvcasefk
 								inner join tcid on tcid.hvcasefk = hvcasepk and tcid.programfk = caseprogram.programfk
 								inner join TCMedical on TCMedical.hvcasefk = hvcasepk and TCMedical.programfk = caseprogram.programfk and TCMedical.TCIDFK = TCID.TCIDPK and @rdate >= TCItemDate
 								inner join codeMedicalItem cmi on MedicalItemCode = TCMedical.TCMedicalItem and MedicalItemCode <= 13
 								inner join dbo.SplitString(@programfk,',') on caseprogram.programfk = listitem
+								INNER JOIN dbo.udfCaseFilters(@CaseFiltersPositive,'',@ProgramFK) cf ON cf.HVCaseFK = HVCasePK
 							where (hvcase.tcdob is not null
 								 and hvcase.tcdob <= dateadd(dd,-30.44,@rdate)) -- caseprogress >= 11
 								 and intakedate <= @rdate
 								 and (dischargedate is null)
 								 and (TCID.NoImmunization = 0
 								 or TCID.NoImmunization is null)) immunizations
+					  INNER join worker fsw on CurrentFSWFK = fsw.workerpk
+					  INNER join workerprogram on workerprogram.workerfk = fsw.workerpk
 					  inner join (select hvcasefk
 										,programfk
 										,max(HVlevelpk) HVlevelpk
@@ -117,6 +126,7 @@ as
 											  ,programfk) hl2 on hvcasepk = hl2.hvcasefk and immunizations.programfk = hl2.programfk
 					  inner join HVLevelDetail hl1 on hl2.HVlevelpk = hl1.HVlevelpk
 				  where levelname <> 'Level X'
+					AND (CASE WHEN @SiteFK = 0 THEN 1 WHEN workerprogram.SiteFK = @SiteFK THEN 1 ELSE 0 END = 1)
 				  group by PC1ID
 						  ,HVCasePK
 						  ,immunizations.ProgramFK
