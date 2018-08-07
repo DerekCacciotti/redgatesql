0a96a0156c4330ce3d6d37afb5652ce5915ba5a0
Jay Robohn <jrobohn@albany.edu>
2018-08-07 12:38:28 -0400

    Updated ASQ-SE for version 2 and fixes

--- hfny.main/Stored Procedures/dbo.spGetOptionItem.sql
+++ hfny.main/Stored Procedures/dbo.spGetOptionItem.sql
@@ -2,28 +2,32 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-
 -- =============================================
 -- Author:		<Dorothy Baum>
 -- Create date: <June 19,2009>
 -- Description:	<Return the OptionValue for OptionItem, ProgramCode, for specify date>
+-- mod: <Jay Robohn> <Aug 06,2018> <Make ProgramFK optional>
 -- =============================================
-create PROCEDURE [dbo].[spGetOptionItem]
-	(@OptionItem varchar(50),@ProgramFK int,@CompareDate datetime, @OptionValue varchar(200) output)
-	
-AS
-BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
+CREATE procedure [dbo].[spGetOptionItem] (@OptionItem varchar(50)
+								, @ProgramFK int
+								, @CompareDate datetime
+								, @OptionValue varchar(200) output)
+as
+	begin
+		-- SET NOCOUNT ON added to prevent extra result sets from
+		-- interfering with SELECT statements.
+		set noCount on ;
 
-    -- Insert statements for procedure here
-	SELECT @OptionValue = OptionValue FROM appOptions 
-    WHERE OptionItem  = @OptionItem and 
-		  ProgramFK = @ProgramFK and 
-		  @CompareDate between 
-		  OptionStart and isnull(OptionEnd,getdate())
+		-- Insert statements for procedure here
+		select	@OptionValue = OptionValue
+		from	AppOptions
+		where	OptionItem = @OptionItem 
+				and case when @ProgramFK is null then 1
+						when ProgramFK = @ProgramFK then 1
+						else 0
+					end = 1
+				and @CompareDate between OptionStart and isnull(OptionEnd, getdate())
 
-END
+	end ;
 
 GO
--- hfny.main/Tables/dbo.scoreASQSE.sql
+++ hfny.main/Tables/dbo.scoreASQSE.sql
@@ -3,7 +3,7 @@ CREATE TABLE [dbo].[scoreASQSE]
 [scoreASQSEPK] [int] NOT NULL IDENTITY(1, 1),
 [ASQSEVersion] [varchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [MaximumASQSEScore] [numeric] (3, 0) NOT NULL,
-[SocialEmotionalScore] [numeric] (6, 2) NOT NULL,
+[SocialEmotionalScore] [numeric] (3, 0) NOT NULL,
 [TCAge] [char] (4) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 ) ON [PRIMARY]
 GO
