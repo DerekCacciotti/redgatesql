cb305d8f02128f1e082e655b20ea24a97aa19fe6
Jay Robohn <jrobohn@albany.edu>
2018-09-05 10:54:33 -0400

    Added indices and changed proc to improve RecentlyAccessedCaseList

--- hfny.main/Tables/dbo.CaseView.sql
+++ hfny.main/Tables/dbo.CaseView.sql
@@ -2,9 +2,13 @@ CREATE TABLE [dbo].[CaseView]
 (
 [CaseViewPK] [int] NOT NULL IDENTITY(1, 1),
 [PC1ID] [nchar] (13) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[Username] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Username] [char] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [ViewDate] [datetime] NOT NULL
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[CaseView] ADD CONSTRAINT [PK_CaseView] PRIMARY KEY CLUSTERED  ([CaseViewPK]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IX_FK_CaseView_PC1ID] ON [dbo].[CaseView] ([PC1ID]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IX_FK_CaseView_Username] ON [dbo].[CaseView] ([Username]) ON [PRIMARY]
 GO
