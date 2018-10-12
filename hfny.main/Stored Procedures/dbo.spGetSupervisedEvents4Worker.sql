9b56aebef578c77dbfc2bff4bcd35799ecc35834
Jay Robohn <jayrobot@gmail.com>
2018-10-12 11:19:30 -0400

    Updates to supervision-related procs

--- hfny.main/Stored Procedures/dbo.spGetSupervisedEvents4Worker.sql
+++ hfny.main/Stored Procedures/dbo.spGetSupervisedEvents4Worker.sql
@@ -1,9 +1,7 @@
-
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-
 --[spGetSupervisedEvents4Worker] 115
 
 -- =============================================
@@ -17,33 +15,53 @@ GO
 -- =============================================
 
 CREATE PROCEDURE [dbo].[spGetSupervisedEvents4Worker]
-(@WorkerFK int,@ProgramFK AS int)
-
+(
+    @WorkerFK INT,
+    @ProgramFK AS INT
+)
 AS
 --
 SET NOCOUNT ON;
 
-SELECT 
-IsApproved,
-RowNumber = ROW_NUMBER() OVER(Order by s.WorkerFK), 
-rtrim(w.LastName) + ' ' + rtrim(w.FirstName) as SupervisorName, s.SupervisionDate,s.SupervisionPK,
-
-CASE
-	WHEN s.TakePlace = 1 THEN 'Yes' ELSE 'No' END
-as TakePlace,
-
-CASE -- convert into to string
-	WHEN s.SupervisionHours > 0 AND s.SupervisionMinutes > 0 THEN CONVERT(varchar(10),s.SupervisionHours) + ':' + CONVERT(varchar(10),s.SupervisionMinutes)
-	WHEN s.SupervisionHours > 0 AND (s.SupervisionMinutes = 0 OR s.SupervisionMinutes IS NULL) THEN CONVERT(varchar(10),s.SupervisionHours) + ':00'
-	WHEN (s.SupervisionHours = 0 OR s.SupervisionHours  IS NULL) AND s.SupervisionMinutes > 0 THEN '00:' + CONVERT(varchar(10),s.SupervisionMinutes)
-	--WHEN (s.SupervisionHours = 0 OR s.SupervisionHours  IS NULL) AND (s.SupervisionMinutes = 0 OR s.SupervisionMinutes IS NULL) THEN '00:00'
-	ELSE ' ' END
-
-as HoursMinutes,
-
-s.WorkerFK,s.SupervisorFK FROM Worker w
-INNER JOIN Supervision s ON s.SupervisorFK = w.WorkerPK
-INNER JOIN FormReviewedTableList('SU', @ProgramFK)ON formfk = s.SupervisionPK
+SELECT IsApproved,
+       RowNumber = ROW_NUMBER() OVER (ORDER BY s.WorkerFK),
+       RTRIM(w.LastName) + ', ' + RTRIM(w.FirstName) AS SupervisorName,
+       CONVERT(VARCHAR(10), s.SupervisionDate, 111) AS SupervisionDate,
+       s.SupervisionPK,
+       CASE
+           WHEN s.TakePlace = 1 THEN
+               'Yes'
+           ELSE
+               'No'
+       END AS TakePlace,
+       CASE -- convert into to string
+           WHEN s.SupervisionHours > 0
+                AND s.SupervisionMinutes > 0 THEN
+               CONVERT(VARCHAR(10), s.SupervisionHours) + ':' + CONVERT(VARCHAR(10), s.SupervisionMinutes)
+           WHEN s.SupervisionHours > 0
+                AND
+                (
+                    s.SupervisionMinutes = 0
+                    OR s.SupervisionMinutes IS NULL
+                ) THEN
+               CONVERT(VARCHAR(10), s.SupervisionHours) + ':00'
+           WHEN (
+                    s.SupervisionHours = 0
+                    OR s.SupervisionHours IS NULL
+                )
+                AND s.SupervisionMinutes > 0 THEN
+               '00:' + CONVERT(VARCHAR(10), s.SupervisionMinutes)
+           --WHEN (s.SupervisionHours = 0 OR s.SupervisionHours  IS NULL) AND (s.SupervisionMinutes = 0 OR s.SupervisionMinutes IS NULL) THEN '00:00'
+           ELSE
+               ' '
+       END AS HoursMinutes,
+       s.WorkerFK,
+       s.SupervisorFK
+FROM Worker w
+    INNER JOIN Supervision s
+        ON s.SupervisorFK = w.WorkerPK
+    INNER JOIN FormReviewedTableList('SU', @ProgramFK)
+        ON FormFK = s.SupervisionPK
 WHERE s.WorkerFK = @WorkerFK
-ORDER BY SupervisionDate DESC
+ORDER BY SupervisionDate DESC;
 GO
