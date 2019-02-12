9e2757467c2a1f351dc82b8e842d7aed171c9822
cpapas <cpapas@albany.edu>
2019-02-12 08:04:21 -0500

    Need 2 month optional ASQSE scores

--- hfny.main/Data/dbo.scoreASQSE_Data.sql
+++ hfny.main/Data/dbo.scoreASQSE_Data.sql
@@ -1,12 +1,6 @@
 SET IDENTITY_INSERT [dbo].[scoreASQSE] ON
-INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (34, '2', 345, 45, '06  ')
-INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (35, '2', 405, 50, '12  ')
-INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (36, '2', 465, 65, '18  ')
-INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (37, '2', 465, 65, '24  ')
-INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (38, '2', 495, 85, '30  ')
-INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (39, '2', 525, 105, '36  ')
-INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (40, '2', 540, 85, '48  ')
-INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (41, '2', 540, 95, '60  ')
+INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (44, '1', 240, 35, '02  ')
+INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (45, '2', 240, 35, '02  ')
 SET IDENTITY_INSERT [dbo].[scoreASQSE] OFF
 SET IDENTITY_INSERT [dbo].[scoreASQSE] ON
 INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (18, '1', 285, 45, '06  ')
@@ -17,4 +11,12 @@ INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSESco
 INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (23, '1', 465, 59, '36  ')
 INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (24, '1', 495, 70, '48  ')
 INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (25, '1', 495, 70, '60  ')
+INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (34, '2', 345, 45, '06  ')
+INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (35, '2', 405, 50, '12  ')
+INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (36, '2', 465, 65, '18  ')
+INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (37, '2', 465, 65, '24  ')
+INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (38, '2', 495, 85, '30  ')
+INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (39, '2', 525, 105, '36  ')
+INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (40, '2', 540, 85, '48  ')
+INSERT INTO [dbo].[scoreASQSE] ([scoreASQSEPK], [ASQSEVersion], [MaximumASQSEScore], [SocialEmotionalScore], [TCAge]) VALUES (41, '2', 540, 95, '60  ')
 SET IDENTITY_INSERT [dbo].[scoreASQSE] OFF
