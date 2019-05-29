d600364c0caa4b786b93d32e3a29cb785d81d41a
Derek Cacciotti <dcacciotti@albany.edu>
2019-05-29 14:53:32 -0400

    Revert "edit proc for the count of certain tcmedical items"
    This reverts commit c2336740216711451ced5923b470554580125116.

--- hfny.main/Stored Procedures/dbo.spGetLeadScreeningsCountByTCID.sql
+++ hfny.main/Stored Procedures/dbo.spGetLeadScreeningsCountByTCID.sql
@@ -2,9 +2,10 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-CREATE PROC [dbo].[spGetLeadScreeningsCountByTCID] @TCIDFK INT, @ProgramFK int AS 
+CREATE PROC [dbo].[spGetLeadScreeningsCountByTCID] @TCIDFK INT AS 
 
 SELECT COUNT(*) AS results FROM dbo.TCMedical 
 INNER JOIN dbo.codeMedicalItem cmi 
-ON TCMedicalItem = cmi.MedicalItemCode WHERE TCIDFK = @TCIDFK AND cmi.MedicalItemText = 'Lead Screening' AND ProgramFK = @ProgramFK
+ON TCMedicalItem = cmi.MedicalItemCode WHERE TCIDFK = @TCIDFK AND cmi.MedicalItemText = 'Lead Screening'
+
 GO
