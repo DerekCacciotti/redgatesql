c2336740216711451ced5923b470554580125116
Derek Cacciotti <dcacciotti@albany.edu>
2019-05-29 14:52:37 -0400

    edit proc for the count of certain tcmedical items

--- hfny.main/Stored Procedures/dbo.spGetWellBabyVisitCountByTCID.sql
+++ hfny.main/Stored Procedures/dbo.spGetWellBabyVisitCountByTCID.sql
@@ -2,10 +2,10 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-CREATE PROC [dbo].[spGetWellBabyVisitCountByTCID] @TCIDFK INT AS 
+CREATE PROC [dbo].[spGetWellBabyVisitCountByTCID] @TCIDFK INT, @ProgramFK int AS 
 
 SELECT COUNT(*) AS results FROM dbo.TCMedical
 INNER JOIN dbo.codeMedicalItem cmi 
-ON TCMedicalItem = cmi.MedicalItemCode WHERE TCIDFK = @TCIDFK AND cmi.MedicalItemText = 'Well Baby Visit'
-
+ON TCMedicalItem = cmi.MedicalItemCode WHERE TCIDFK = @TCIDFK AND cmi.MedicalItemText = 'Well Baby Visit' AND 
+ProgramFK = @ProgramFK
 GO
