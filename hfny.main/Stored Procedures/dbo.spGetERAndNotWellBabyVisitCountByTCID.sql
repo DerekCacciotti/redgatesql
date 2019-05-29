25cb518a262c64ef315b197f603047dea38748c8
Derek Cacciotti <dcacciotti@albany.edu>
2019-05-29 15:01:39 -0400

    Revert "Revert "edit proc for the count of certain tcmedical items""
    This reverts commit d600364c0caa4b786b93d32e3a29cb785d81d41a.

--- hfny.main/Stored Procedures/dbo.spGetERAndNotWellBabyVisitCountByTCID.sql
+++ hfny.main/Stored Procedures/dbo.spGetERAndNotWellBabyVisitCountByTCID.sql
@@ -2,9 +2,10 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-CREATE PROC [dbo].[spGetERAndNotWellBabyVisitCountByTCID] @TCIDFK INT AS 
+CREATE PROC [dbo].[spGetERAndNotWellBabyVisitCountByTCID] @TCIDFK INT, @ProgramFK int AS 
 
 SELECT COUNT(*) AS results FROM dbo.TCMedical
 INNER JOIN dbo.codeMedicalItem cmi 
 ON TCMedicalItem = cmi.MedicalItemCode WHERE TCIDFK = @TCIDFK AND cmi.MedicalItemText LIKE '%TC%' AND cmi.MedicalItemUsedWhere = 'TM'
+AND ProgramFK = @ProgramFK
 GO
