57e782e4efde28d58a774b4360f1d458dc49e55f
jayrobot <jayrobot@gmail.com>
2019-09-03 10:45:26 -0400

    Fixed name of PK column

--- hfny.main/Tables/dbo.AutoSaveLog.sql
+++ hfny.main/Tables/dbo.AutoSaveLog.sql
@@ -1,11 +1,11 @@
 CREATE TABLE [dbo].[AutoSaveLog]
 (
-[AutoSavePK] [int] NOT NULL IDENTITY(1, 1),
+[AutoSaveLogPK] [int] NOT NULL IDENTITY(1, 1),
 [AutoSaveDate] [datetime] NOT NULL,
 [AutoSaveCreator] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [FormFK] [int] NOT NULL,
 [FormType] [char] (2) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 ) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[AutoSaveLog] ADD CONSTRAINT [PK_AutoSaveLog] PRIMARY KEY CLUSTERED  ([AutoSavePK]) ON [PRIMARY]
+ALTER TABLE [dbo].[AutoSaveLog] ADD CONSTRAINT [PK_AutoSaveLog] PRIMARY KEY CLUSTERED  ([AutoSaveLogPK]) ON [PRIMARY]
 GO
