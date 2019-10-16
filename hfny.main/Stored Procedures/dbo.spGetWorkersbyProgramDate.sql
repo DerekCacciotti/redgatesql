33ef9caae29d990d81d6ea59e6c8ec1130c98abd
Bill O'Brien <wobrien2@albany.edu>
2019-10-16 12:49:09 -0400

    modified procs for worker dropdowns

--- hfny.main/Stored Procedures/dbo.spGetWorkersbyProgramDate.sql
+++ hfny.main/Stored Procedures/dbo.spGetWorkersbyProgramDate.sql
@@ -1,4 +1,3 @@
-
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -11,6 +10,10 @@ GO
 -- Edit date: 03/19/2013
 -- Edited by: Chris Papas
 -- Edit Reason: Needed = next to @thedate because workers who started on the date aren't appearing
+
+-- Edit date: 10/10/2019
+-- Edited by: Bill O'Brien
+-- Edit Reason: With Worker Redesign, need to account for role end dates and worker leave periods.
 -- =============================================
 
 CREATE PROCEDURE [dbo].[spGetWorkersbyProgramDate]
@@ -27,13 +30,20 @@ SET NOCOUNT ON;
     -- Insert statements for procedure here
 
 SELECT WorkerPK, rtrim(FirstName) + ' ' + rtrim(LastName) AS WorkerName FROM Worker w
-INNER JOIN WorkerProgram wp ON w.WorkerPK=wp.WorkerFK
-WHERE (TerminationDate IS NULL OR TerminationDate >@thedate)
-AND ProgramFK=@progfk
-AND (
-wp.FAWStartDate <=dateadd(mm, 36, @thedate) OR wp.FSWStartDate<=dateadd(mm, 36, @thedate) 
-OR wp.SupervisorStartDate<=dateadd(mm, 36, @thedate)
-OR wp.ProgramManagerStartDate<=dateadd(mm, 36, @thedate)
+INNER JOIN WorkerProgram wp ON w.WorkerPK = wp.WorkerFK
+LEFT JOIN fnWorkersOnLeave(@theDate, @progFK) wol on wol.WorkerFK = WorkerPK
+WHERE (TerminationDate IS NULL OR TerminationDate > @theDate)
+AND ProgramFK = @progfk
+AND wol.WorkerFK is null
+AND 
+(
+	(wp.FAWStartDate <= dateadd(mm, 36, @theDate) AND (wp.FAWEndDate is null or wp.FAWEndDate > @theDate))
+	OR 
+	(wp.FSWStartDate<= dateadd(mm, 36, @theDate) AND (wp.FSWEndDate is null or wp.FSWEndDate > @theDate))
+	OR 
+	(wp.SupervisorStartDate<= dateadd(mm, 36, @theDate) AND (wp.SupervisorEndDate is null or wp.SupervisorEndDate > @theDate))
+	OR 
+	(wp.ProgramManagerStartDate<= dateadd(mm, 36, @theDate) AND (wp.ProgramManagerEndDate is null or wp.ProgramManagerEndDate > @theDate))
 )
 END
 
