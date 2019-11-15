5e4fdb2996d05a937c5c6527f34201a67f01eafd
Derek Cacciotti <dcacciotti@albany.edu>
2019-11-15 10:50:02 -0500

    changed the gettcmedicals for approval proc to display a list of tcmedical pk per kid

--- hfny.main/Stored Procedures/dbo.spGetTCMedicalsForApporval.sql
+++ hfny.main/Stored Procedures/dbo.spGetTCMedicalsForApporval.sql
@@ -2,8 +2,10 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-CREATE PROC [dbo].[spGetTCMedicalsForApporval] @date VARCHAR(120), @HVCaseFK int AS
+CREATE PROC [dbo].[spGetTCMedicalsForApporval] @date VARCHAR(120), @TCIDFK INT AS 
 
-SELECT * FROM dbo.FormReview WHERE FormDate = @date AND HVCaseFK = @HVCaseFK AND FormType = 'TM'
+--SELECT * FROM dbo.FormReview WHERE FormDate = @date AND HVCaseFK = @HVCaseFK AND FormType = 'TM'
 
+
+SELECT TCMedicalPK FROM TCMedical tm WHERE tm.TCItemDate = @date AND tm.TCIDFK = @TCIDFK
 GO
