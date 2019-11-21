7aaf1666afd4530607b1cbc7b06b73e589fa1d57
Derek Cacciotti <dcacciotti@albany.edu>
2019-11-21 14:58:50 -0500

    replaced hard coded value with parameters

--- hfny.main/Stored Procedures/dbo.spGetCaseNotesForDashboard.sql
+++ hfny.main/Stored Procedures/dbo.spGetCaseNotesForDashboard.sql
@@ -9,7 +9,7 @@ INNER JOIN HVCase hc ON hc.HVCasePK = cp.HVCaseFK
 INNER JOIN CaseNote cn ON cn.HVCaseFK = cp.HVCaseFK
 INNER JOIN Worker w ON w.WorkerPK = cp.CurrentFSWFK
 INNER JOIN WorkerProgram wp	ON wp.WorkerFK = w.WorkerPK
-WHERE cn.ProgramFK = 1 AND wp.SupervisorFK = 155 AND cn.CaseNoteDate BETWEEN CAST(GETDATE() AS DATE) 
+WHERE cn.ProgramFK = @programfk AND wp.SupervisorFK = @supervisorfk AND cn.CaseNoteDate BETWEEN CAST(GETDATE() AS DATE) 
 AND CAST(DATEADD(DAY, 7, GETDATE()) AS DATE)
 
 --SELECT DATEADD(DAY,7,GETDATE())
