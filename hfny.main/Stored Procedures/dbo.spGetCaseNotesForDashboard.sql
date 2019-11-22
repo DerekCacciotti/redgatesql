0ec1fbd2af11286d847c46d592ee2dc28cf0a5de
Derek Cacciotti <dcacciotti@albany.edu>
2019-11-22 15:17:21 -0500

    changed where clause

--- hfny.main/Stored Procedures/dbo.spGetCaseNotesForDashboard.sql
+++ hfny.main/Stored Procedures/dbo.spGetCaseNotesForDashboard.sql
@@ -9,9 +9,8 @@ INNER JOIN HVCase hc ON hc.HVCasePK = cp.HVCaseFK
 INNER JOIN CaseNote cn ON cn.HVCaseFK = cp.HVCaseFK
 INNER JOIN Worker w ON w.WorkerPK = cp.CurrentFSWFK
 INNER JOIN WorkerProgram wp	ON wp.WorkerFK = w.WorkerPK
-WHERE cn.ProgramFK = @programfk AND wp.SupervisorFK = @supervisorfk AND cn.CaseNoteDate BETWEEN CAST(GETDATE() AS DATE) 
-AND CAST(DATEADD(DAY, 7, GETDATE()) AS DATE)
-
+WHERE cn.ProgramFK = @programfk AND wp.SupervisorFK = @supervisorfk AND cn.CaseNoteDate BETWEEN CAST(DATEADD(DAY, -7, GETDATE()) AS DATE)
+AND CAST(GETDATE() AS DATE)
 --SELECT DATEADD(DAY,7,GETDATE())
 
 
