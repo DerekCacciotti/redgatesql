01295ee0a41a4db4a3266d2c13ea59473fe3313f
Benjamin Simmons <brsimmons@albany.edu>
2019-12-09 11:25:49 -0500

    codeReportAccess - Added ReportFK field and foreign key relationships

--- hfny.main/Tables/dbo.codeReportAccess.sql
+++ hfny.main/Tables/dbo.codeReportAccess.sql
@@ -3,8 +3,13 @@ CREATE TABLE [dbo].[codeReportAccess]
 [codeReportAccessPK] [int] NOT NULL IDENTITY(1, 1),
 [StartDate] [datetime] NOT NULL,
 [EndDate] [datetime] NULL,
+[ReportFK] [int] NOT NULL,
 [StateFK] [int] NOT NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[codeReportAccess] ADD CONSTRAINT [PK_codeReportAccess] PRIMARY KEY CLUSTERED  ([codeReportAccessPK]) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[codeReportAccess] ADD CONSTRAINT [FK_codeReportAccess_codeReportCatalog] FOREIGN KEY ([ReportFK]) REFERENCES [dbo].[codeReportCatalog] ([codeReportCatalogPK])
+GO
+ALTER TABLE [dbo].[codeReportAccess] ADD CONSTRAINT [FK_codeReportAccess_State] FOREIGN KEY ([StateFK]) REFERENCES [dbo].[State] ([StatePK])
 GO
