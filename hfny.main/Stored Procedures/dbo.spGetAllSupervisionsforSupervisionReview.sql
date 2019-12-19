f11b3680418399b1e137a344f1559709e9550dfa
Derek Cacciotti <dcacciotti@albany.edu>
2019-12-19 15:48:24 -0500

    new proc for the partial supervision review

---
+++ hfny.main/Stored Procedures/dbo.spGetAllSupervisionsforSupervisionReview.sql
@@ -0,0 +1,17 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROC [dbo].[spGetAllSupervisionsforSupervisionReview] @ProgramFK INT AS 
+--DECLARE @ProgramFK INT = 1 
+
+SELECT s.SupervisionDate, s.SupervisionStartTime, s.SupervisionSessionType, 
+LTRIM(RTRIM(w.LastName)) + ', ' + LTRIM(RTRIM(w.FirstName)) AS WorkerName,
+LTRIM(RTRIM(super.LastName)) + ', ' + LTRIM(RTRIM(w.FirstName)) AS SupervisorName,
+ CASE WHEN s.SupervisionSessionType = '1' THEN 'Scheduled Session' WHEN s.SupervisionSessionType = '2' THEN 'Pre-Suspervision Planing' 
+		WHEN s.SupervisionSessionType = '3' THEN 'Group Session' ELSE 'Missed Session' END as SupervisionType
+ FROM Supervision s 
+INNER JOIN Worker w ON w.WorkerPK = s.WorkerFK
+INNER JOIN Worker super ON super.WorkerPK = s.SupervisorFK
+WHERE s.ProgramFK = @ProgramFK AND s.FormComplete = 0
+GO
