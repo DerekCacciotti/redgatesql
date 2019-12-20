24f51cb07ef16f3b9bb9c9b642cab5882e9215dd
Derek Cacciotti <dcacciotti@albany.edu>
2019-12-20 10:48:47 -0500

    remove time from supervision date and added supervisionpk for redirect

--- hfny.main/Stored Procedures/dbo.spGetAllSupervisionsforSupervisionReview.sql
+++ hfny.main/Stored Procedures/dbo.spGetAllSupervisionsforSupervisionReview.sql
@@ -5,11 +5,11 @@ GO
 CREATE PROC [dbo].[spGetAllSupervisionsforSupervisionReview] @ProgramFK INT AS 
 --DECLARE @ProgramFK INT = 1 
 
-SELECT s.SupervisionDate, s.SupervisionStartTime, s.SupervisionSessionType, 
+SELECT FORMAT(s.SupervisionDate,'MM/dd/yy')  AS SupervisionDate, s.SupervisionStartTime, s.SupervisionSessionType, 
 LTRIM(RTRIM(w.LastName)) + ', ' + LTRIM(RTRIM(w.FirstName)) AS WorkerName,
 LTRIM(RTRIM(super.LastName)) + ', ' + LTRIM(RTRIM(w.FirstName)) AS SupervisorName,
  CASE WHEN s.SupervisionSessionType = '1' THEN 'Scheduled Session' WHEN s.SupervisionSessionType = '2' THEN 'Pre-Suspervision Planing' 
-		WHEN s.SupervisionSessionType = '3' THEN 'Group Session' ELSE 'Missed Session' END as SupervisionType
+		WHEN s.SupervisionSessionType = '3' THEN 'Group Session' ELSE 'Missed Session' END as SupervisionType,s.SupervisionPK
  FROM Supervision s 
 INNER JOIN Worker w ON w.WorkerPK = s.WorkerFK
 INNER JOIN Worker super ON super.WorkerPK = s.SupervisorFK
