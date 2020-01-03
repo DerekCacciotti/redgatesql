99c76a6547eb98b6ba7b2d000d4f4434b2028192
Derek Cacciotti <dcacciotti@albany.edu>
2020-01-03 08:51:15 -0500

    fixed spelling error

--- hfny.main/Stored Procedures/dbo.spGetAllSupervisionsforSupervisionReview.sql
+++ hfny.main/Stored Procedures/dbo.spGetAllSupervisionsforSupervisionReview.sql
@@ -8,7 +8,7 @@ CREATE PROC [dbo].[spGetAllSupervisionsforSupervisionReview] @ProgramFK INT AS
 SELECT FORMAT(s.SupervisionDate,'MM/dd/yy')  AS SupervisionDate, s.SupervisionStartTime, s.SupervisionSessionType, 
 LTRIM(RTRIM(w.LastName)) + ', ' + LTRIM(RTRIM(w.FirstName)) AS WorkerName,
 LTRIM(RTRIM(super.LastName)) + ', ' + LTRIM(RTRIM(w.FirstName)) AS SupervisorName,
- CASE WHEN s.SupervisionSessionType = '1' THEN 'Scheduled Session' WHEN s.SupervisionSessionType = '2' THEN 'Pre-Suspervision Planing' 
+ CASE WHEN s.SupervisionSessionType = '1' THEN 'Scheduled Session' WHEN s.SupervisionSessionType = '2' THEN 'Pre-Supervision Planning' 
 		WHEN s.SupervisionSessionType = '3' THEN 'Group Session' ELSE 'Missed Session' END as SupervisionType,s.SupervisionPK
  FROM Supervision s 
 INNER JOIN Worker w ON w.WorkerPK = s.WorkerFK
