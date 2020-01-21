e4587dee9f8a2783bb3e54147ad16b8c5a08fbb2
Derek Cacciotti <dcacciotti@albany.edu>
2020-01-21 12:09:49 -0500

    fixed issue wher tcdob was not displaying correctly

--- hfny.main/Stored Procedures/dbo.rspGetSinglePreassessmentForCaseHomePage.sql
+++ hfny.main/Stored Procedures/dbo.rspGetSinglePreassessmentForCaseHomePage.sql
@@ -3,7 +3,7 @@ GO
 SET ANSI_NULLS ON
 GO
 CREATE PROC [dbo].[rspGetSinglePreassessmentForCaseHomePage] @papk INT AS
-SELECT TOP 1 cp.PC1ID, hvs.ScreenDate, pa.PADate, pa.PAParentLetter, ISNULL(tcid.TCDOB, '') AS TCDOB,
+SELECT TOP 1 cp.PC1ID, hvs.ScreenDate, pa.PADate, pa.PAParentLetter, ISNULL(hc.TCDOB, '') AS TCDOB,
 --tcid.TCDOB, w.FirstName, w.LastName, pa.PAParentLetter,
 pa.PACall2Parent,pa.PACallFromParent,pa.PAVisitAttempt,pa.PAVisitMade,pa.PAOtherHVProgram, pa.PAParent2Office,
 pa.PAProgramMaterial,pa.PAGift,pa.PACaseReview, pa.PAOtherActivity, pa.CaseStatus, pa.KempeDate, pa.PAFSWFK,
@@ -11,6 +11,7 @@ w.FirstName AS 'Current Worker First Name', w.LastName AS 'Current Worker Last N
 pa.FSWAssignDate, cp.DischargeDate, cp.DischargeReason, cd.DischargeReason AS DischareReasonText
 FROM preassessment pa 
 INNER JOIN dbo.CaseProgram cp ON cp.HVCaseFK = pa.HVCaseFK 
+LEFT JOIN  HVCase hc ON hc.HVCasePK = cp.HVCaseFK
 inner JOIN dbo.HVScreen hvs ON hvs.HVCaseFK = pa.HVCaseFK
 left JOIN dbo.TCID tcid ON tcid.HVCaseFK = pa.HVCaseFK 
 INNER  JOIN dbo.Worker w ON w.WorkerPK = pa.PAFAWFK
