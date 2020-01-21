88db5e570fbd5a77ce23697179b83774f14eac15
Derek Cacciotti <dcacciotti@albany.edu>
2020-01-21 11:53:32 -0500

    Added discharge date and reason to report

--- hfny.main/Stored Procedures/dbo.rspGetSinglePreassessmentForCaseHomePage.sql
+++ hfny.main/Stored Procedures/dbo.rspGetSinglePreassessmentForCaseHomePage.sql
@@ -8,13 +8,14 @@ SELECT TOP 1 cp.PC1ID, hvs.ScreenDate, pa.PADate, pa.PAParentLetter, ISNULL(tcid
 pa.PACall2Parent,pa.PACallFromParent,pa.PAVisitAttempt,pa.PAVisitMade,pa.PAOtherHVProgram, pa.PAParent2Office,
 pa.PAProgramMaterial,pa.PAGift,pa.PACaseReview, pa.PAOtherActivity, pa.CaseStatus, pa.KempeDate, pa.PAFSWFK,
 w.FirstName AS 'Current Worker First Name', w.LastName AS 'Current Worker Last Name',
-pa.FSWAssignDate
+pa.FSWAssignDate, cp.DischargeDate, cp.DischargeReason, cd.DischargeReason AS DischareReasonText
 FROM preassessment pa 
 INNER JOIN dbo.CaseProgram cp ON cp.HVCaseFK = pa.HVCaseFK 
 inner JOIN dbo.HVScreen hvs ON hvs.HVCaseFK = pa.HVCaseFK
 left JOIN dbo.TCID tcid ON tcid.HVCaseFK = pa.HVCaseFK 
 INNER  JOIN dbo.Worker w ON w.WorkerPK = pa.PAFAWFK
 left JOIN dbo.Worker currentworker ON currentworker.WorkerPK = cp.CurrentFSWFK
+LEFT JOIN codeDischarge cd ON cd.DischargeCode = cp.DischargeReason
 WHERE pa.PreassessmentPK = @papk
 ORDER BY padate DESC
 GO
