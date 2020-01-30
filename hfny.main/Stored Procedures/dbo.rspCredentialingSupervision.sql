55a738ec617f7d10408c4249649076c6c9ed366c
Benjamin Simmons <brsimmons@albany.edu>
2020-01-30 09:30:01 -0500

    rspCredentialingSupervision - Don't allow planning sessions to reduce the number of expected sessions

--- hfny.main/Stored Procedures/dbo.rspCredentialingSupervision.sql
+++ hfny.main/Stored Procedures/dbo.rspCredentialingSupervision.sql
@@ -21,7 +21,7 @@ GO
 -- max of 2 supervisions per week ... khalsa 1/29/2014
 
 -- =============================================
-CREATE procedure [dbo].[rspCredentialingSupervision] @ProgramFK int = null
+CREATE PROC [dbo].[rspCredentialingSupervision] @ProgramFK int = null
 										, @sDate datetime = null
 										, @eDate datetime = null
 										, @supervisorfk int = null
@@ -1027,7 +1027,7 @@ set fmtOnly off ;
 	as (
 
 		select		WorkerName
-				, sum(	case when DaysInTheCurrentWeek = 7 and	MeetsStandard1 <> ' ' then 1
+				, sum(	case when DaysInTheCurrentWeek = 7 then 1
 						else 0 end
 					) as NumOfExpectedSessions
 				, sum(case when MeetsStandard1 = 'E' then 1 else 0 end) as NumOfAllowedExecuses
