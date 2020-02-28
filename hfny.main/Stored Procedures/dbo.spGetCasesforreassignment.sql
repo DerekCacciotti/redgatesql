0a65e7c5be0dda2c3b6fc442ea83b439f7af1f62
Derek Cacciotti <dcacciotti@albany.edu>
2020-02-28 09:33:54 -0500

    edited where clause to prevent cases from appearing twice for both FRS and FSS roles

--- hfny.main/Stored Procedures/dbo.spGetCasesforreassignment.sql
+++ hfny.main/Stored Procedures/dbo.spGetCasesforreassignment.sql
@@ -8,7 +8,7 @@ SELECT cp.PC1ID, CONCAT(p.PCFirstName, ' ', p.PCLastName) AS PCName, 'FSS' AS Wo
 INNER JOIN HVCase hc ON hc.HVCasePK = cp.HVCaseFK
 INNER JOIN PC p ON p.PCPK = hc.PC1FK
 INNER JOIN (SELECT MAX(workerassignmentdate) AS MaxDate, hvcaseFK FROM WorkerAssignment wa GROUP BY wa.HVCaseFK) AS sub ON sub.HVCaseFK = hc.HVCasePK
-WHERE cp.CurrentFSWFK = @workerfk AND cp.DischargeDate IS NULL AND cp.ProgramFK = @programfk
+WHERE cp.CurrentFSWFK = @workerfk AND cp.DischargeDate IS NULL AND cp.ProgramFK = @programfk 
 
 UNION
  -- FRS Cases 
@@ -16,5 +16,5 @@ UNION
 INNER JOIN HVCase hc ON hc.HVCasePK = cp.HVCaseFK
 INNER JOIN PC p ON p.PCPK = hc.PC1FK
 LEFT JOIN (SELECT MAX(workerassignmentdate) AS MaxDate, hvcaseFK FROM WorkerAssignment wa GROUP BY wa.HVCaseFK) AS sub ON sub.HVCaseFK = hc.HVCasePK
-WHERE cp.CurrentFAWFK = @workerfk  AND cp.DischargeDate IS NULL AND cp.ProgramFK = @programfk
+WHERE cp.CurrentFAWFK = @workerfk  AND cp.DischargeDate IS NULL AND cp.ProgramFK = @programfk  AND cp.CurrentFSWFK IS NULL
 GO
